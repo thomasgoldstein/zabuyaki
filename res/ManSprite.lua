437833d4f910b048b6abe36f8ccf08d4fd97505c
Don Miguel <mikhail.bratus@gmail.com>
2016-02-15 21:33:20 +0500

    new added sprites coords form the new sheet

--- res/ManSprite.lua
+++ res/ManSprite.lua
@@ -1,7 +1,7 @@
 print("ManSprite.lua loaded")
 
-local image_w = 640 --This info can be accessed with a Love2D call
-local image_h = 130 --      after the image has been loaded. I'm creating these for readability.
+local image_w = 240 --This info can be accessed with a Love2D call
+local image_h = 393 --after the image has been loaded
 
 return {
 	serialization_version = 0.2, -- The version of this serialization process
@@ -19,27 +19,14 @@ return {
 	--      of animations in in here.
 	animations_names = {
 		"idle",
-		"walk" --[[,
+		"center",
+		"walk",
         "duck",
         "jump",
-        "center",
-        "run"
---]]
+        "run",
+        "kick",
+        "punch"
 	},
-
---[[
-RAW 323 0 35 55 duck1
-RAW 359 0 31 51 duck2
-RAW 391 0 44 67 jump1
-RAW 436 0 44 66 jump2
-RAW 481 0 41 62 center
-RAW 0 67 33 63 run1
-RAW 34 67 48 61 run2
-RAW 83 67 51 55 run3
-RAW 135 67 45 58 run4
-RAW 181 67 51 55 run5
---]]
-
 
 	--The list with all the frames mapped to their respective animations
 	--  each one can be accessed like this:
@@ -47,37 +34,111 @@ RAW 181 67 51 55 run5
 	animations = {
 		idle = {
 			--  love.graphics.newQuad( X, Y, Width, Height, Image_W, Image_H)
-			love.graphics.newQuad( 0, 0, 49, 62, image_w, image_h ),
-			love.graphics.newQuad( 50, 0, 49, 61, image_w, image_h ),
-			love.graphics.newQuad( 100, 0, 49, 60, image_w, image_h ),
-			love.graphics.newQuad( 50, 0, 49, 61, image_w, image_h )
+			love.graphics.newQuad(2, 2, 49, 62, image_w, image_h),
+			love.graphics.newQuad(53, 3, 49, 61, image_w, image_h),
+			love.graphics.newQuad(104, 4, 49, 60, image_w, image_h),
+			love.graphics.newQuad(53, 3, 49, 61, image_w, image_h)
+		},
+		center = {
+			love.graphics.newQuad(155, 2, 41, 62, image_w, image_h),
 		},
 		walk = {
-			--  love.graphics.newQuad( X, Y, Width, Height, Image_W, Image_H)
-			love.graphics.newQuad( 150, 0, 31, 63, image_w, image_h ),
-			love.graphics.newQuad( 182, 0, 32, 63, image_w, image_h ),
-			love.graphics.newQuad( 215, 0, 37, 62, image_w, image_h ),
-			love.graphics.newQuad( 253, 0, 31, 63, image_w, image_h ),
-			love.graphics.newQuad( 285, 0, 37, 62, image_w, image_h )
-		}        
+			love.graphics.newQuad(2, 66, 31, 63, image_w, image_h),
+			love.graphics.newQuad(35, 66, 32, 63, image_w, image_h),
+			love.graphics.newQuad(69, 66, 37, 62, image_w, image_h),
+			love.graphics.newQuad(108, 66, 31, 63, image_w, image_h),
+			love.graphics.newQuad(141, 66, 37, 62, image_w, image_h)
+		},
+		duck = {
+			love.graphics.newQuad(2, 143, 35, 55, image_w, image_h),
+			love.graphics.newQuad(39, 147, 31, 51, image_w, image_h)
+		},
+		jump = {
+			love.graphics.newQuad(72, 132, 44, 66, image_w, image_h),
+			love.graphics.newQuad(118, 131, 44, 67, image_w, image_h)
+		},
+		run = {
+			love.graphics.newQuad(2, 200, 33, 63, image_w, image_h),
+			love.graphics.newQuad(37, 201, 48, 61, image_w, image_h),
+			love.graphics.newQuad(87, 202, 51, 55, image_w, image_h),
+			love.graphics.newQuad(140, 201, 45, 58, image_w, image_h),
+			love.graphics.newQuad(187, 202, 51, 55, image_w, image_h)
+		},
+		punch = {
+			love.graphics.newQuad(2, 266, 56, 61, image_w, image_h),
+			love.graphics.newQuad(60, 265, 51, 62, image_w, image_h),
+			love.graphics.newQuad(113, 265, 42, 62, image_w, image_h),
+			love.graphics.newQuad(157, 265, 51, 62, image_w, image_h),
+			love.graphics.newQuad(113, 265, 42, 62, image_w, image_h),
+		},
+		kick = {
+			love.graphics.newQuad(2, 329, 33, 62, image_w, image_h),
+			love.graphics.newQuad(37, 329, 52, 62, image_w, image_h),
+			love.graphics.newQuad(91, 330, 46, 61, image_w, image_h),
+			love.graphics.newQuad(139, 331, 60, 60, image_w, image_h),
+			love.graphics.newQuad(91, 330, 46, 61, image_w, image_h),
+		},
+		hurt = {
+			love.graphics.newQuad(118, 131, 44, 67, image_w, image_h)
+		},
+		dead = {
+			love.graphics.newQuad(2, 143, 35, 55, image_w, image_h)
+		}
 	},
 	--pivots offsets
 	offsets = {
 		idle = {
 			--  { xo, yo},
-			{ 20, 61},
-			{ 20, 60},
-			{ 20, 59},
-			{ 20, 60},
+			{ 20, 61 },
+			{ 20, 60 },
+			{ 20, 59 },
+			{ 20, 60 }
+		},
+		center = {
+			{ 20, 60 },
 		},
 		walk = {
-			--  { xo, yo},
-			{ 20, 61},
-			{ 20, 60},
-			{ 20, 59},
-			{ 20, 59},
-			{ 20, 60},
-		}        
+			{ 20, 61 },
+			{ 20, 60 },
+			{ 20, 59 },
+			{ 20, 59 },
+			{ 20, 60 }
+		},
+		duck = {
+			{ 20, 59 },
+			{ 20, 60 },
+		},
+		jump = {
+			{ 20, 59 },
+			{ 20, 60 },
+		},
+		run = {
+			{ 20, 61 },
+			{ 20, 60 },
+			{ 20, 59 },
+			{ 20, 59 },
+			{ 20, 60 },
+		},
+		punch = {
+			{ 20, 61 },
+			{ 20, 61 },
+			{ 20, 60 },
+			{ 20, 59 },
+			{ 20, 60 },
+		},
+		kick = {
+			{ 20, 61 },
+			{ 20, 59 },
+			{ 20, 59 },
+			{ 20, 59 },
+			{ 20, 60 },
+		},
+		hurt = {
+			{ 20, 60 },
+		},
+		dead = {
+			{ 20, 60 },
+		}
 	} --offsets
 
 } --return (end of file)
