039d57989f240a5cd58c73195c1f32812d5e425e
Don Miguel <mikhail.bratus@gmail.com>
2016-03-18 22:51:51 +0500

    update lib

--- lib/fancy.lua
+++ lib/fancy.lua
@@ -16,6 +16,7 @@ fancy.x = 8
 fancy.y = gr.getHeight() - fancy.maxRows*fancy.rowHeight - 8
 fancy.alpha = 140
 fancy.alphaScroll = 0
+fancy.visible = true
 
 function fancy.setAlpha(alpha)
 	colors = {
@@ -25,6 +26,11 @@ function fancy.setAlpha(alpha)
 	}
 end
 fancy.setAlpha(fancy.alpha)
+
+function fancy.setFont(font)
+	fancy.font = font
+	fancy.rowHeight = fancy.font:getHeight() + fancy.padding * 2
+end
 
 local function overQuad(mx, my)
 	return not (
@@ -44,84 +50,102 @@ function fancy.getIndex(tagId)
 end
 
 function fancy.watch(tagId, value, priority)
-	if fancy.getIndex(tagId) == -1 then
-		table.insert(fancy.items, {
-			label = tagId,
-			value = value,
-			priority = priority or 3
-		})
-		fancy.h = fancy.maxRows * fancy.font:getHeight()
-	else
-		fancy.items[fancy.getIndex(tagId)].value = value
+	if fancy.visible then
+		if fancy.getIndex(tagId) == -1 then
+			table.insert(fancy.items, {
+				label = tagId,
+				value = value,
+				priority = priority or 3
+			})
+			fancy.h = fancy.maxRows * fancy.font:getHeight()
+		else
+			fancy.items[fancy.getIndex(tagId)].value = value
+		end
 	end
 end
 
 function fancy.draw()
-	local prevFont = gr.getFont()
-	gr.setFont(fancy.font)
-	gr.setLineStyle("rough")
-	local maxWidth = 0
-	local rowStep = 0
-	for i = fancy.indexRowDisplacement, fancy.indexRowDisplacement + fancy.maxRows - 1 do
-		local item = fancy.items[i]
-		if item then
-			local t = item.label..": "..item.value
-			gr.setColor(colors[item.priority])
-			if fancy.pressed and mo.isDown(3) then -- Middle btn.
-				fancy.x = mo.getX() - fancy.dx
-				fancy.y = mo.getY() - fancy.dy
+	if fancy.visible then
+		local prevFont = gr.getFont()
+		gr.setFont(fancy.font)
+		gr.setLineStyle("rough")
+		local maxWidth = 0
+		local rowStep = 0
+		for i = fancy.indexRowDisplacement, fancy.indexRowDisplacement + fancy.maxRows - 1 do
+			local item = fancy.items[i]
+			if item then
+				local t = item.label..": "..item.value
+				gr.setColor(colors[item.priority])
+				if fancy.pressed and mo.isDown(3) then -- Middle btn.
+					fancy.x = mo.getX() - fancy.dx
+					fancy.y = mo.getY() - fancy.dy
+				end
+				gr.rectangle("fill", fancy.x, fancy.y + rowStep, fancy.w, fancy.rowHeight)
+				gr.setColor(255, 255, 255)
+				if item.priority == 2 then gr.setColor(0, 0, 0) end
+				gr.print(t,
+					math.floor(fancy.x + fancy.padding),
+					math.floor(fancy.y + rowStep + fancy.padding))
+				rowStep = rowStep + fancy.rowHeight
 			end
-			gr.rectangle("fill", fancy.x, fancy.y + rowStep, fancy.w, fancy.rowHeight)
-			gr.setColor(255, 255, 255)
-			if item.priority == 2 then gr.setColor(0, 0, 0) end
-			gr.print(t,
-				math.floor(fancy.x + fancy.padding),
-				math.floor(fancy.y + rowStep + fancy.padding))
-			rowStep = rowStep + fancy.rowHeight
+			fancy.h = rowStep
 		end
-		fancy.h = rowStep
-	end
-	gr.setFont(prevFont)
-	if #fancy.items > fancy.maxRows then
-		gr.setColor(255, 255, 255, fancy.alphaScroll)
-		local fancyWidth = fancy.maxRows * fancy.rowHeight
-		local barHeight = (fancy.maxRows / #fancy.items) * fancyWidth
-		local barWidth = 7
-		if barHeight < 1 then barHeight = 1 end
-		local barX = fancy.x + fancy.w - barWidth
-		local barY = fancy.y + ((fancy.indexRowDisplacement - 1) / #fancy.items) * fancyWidth
-		if fancy.x + fancy.w > gr.getWidth() then barX = fancy.x - barWidth end
-		gr.rectangle("fill", barX, barY, barWidth, barHeight)
-	end
-	gr.setColor(255, 255, 255)
-	--gr.rectangle("line", fancy.x, fancy.y, fancy.w, fancy.h)
-	gr.setLineStyle("smooth")
-	fancy.alphaScroll = fancy.alphaScroll - love.timer.getDelta() * 200
-	if fancy.alphaScroll <= 0 then
-		fancy.alphaScroll = 0
+		
+		if #fancy.items > fancy.maxRows then
+			gr.setColor(255, 255, 255, fancy.alphaScroll)
+			local fancyWidth = fancy.maxRows * fancy.rowHeight
+			local barHeight = (fancy.maxRows / #fancy.items) * fancyWidth
+			local barWidth = 7
+			if barHeight < 1 then barHeight = 1 end
+			local barX = fancy.x + fancy.w - barWidth
+			local barY = fancy.y + ((fancy.indexRowDisplacement - 1) / #fancy.items) * fancyWidth
+			if fancy.x + fancy.w > gr.getWidth() then barX = fancy.x - barWidth end
+			gr.rectangle("fill", barX, barY, barWidth, barHeight)
+		end
+		gr.setColor(255, 255, 255)
+		--gr.rectangle("line", fancy.x, fancy.y, fancy.w, fancy.h)
+		gr.setLineStyle("smooth")
+		fancy.alphaScroll = fancy.alphaScroll - love.timer.getDelta() * 200
+		if fancy.alphaScroll <= 0 then
+			fancy.alphaScroll = 0
+		end
+		gr.setFont(prevFont)
 	end
 end
 
-function fancy.wheel(y)
-	if y < 0 then -- Up:
-		if kb.isDown("lctrl") or kb.isDown("rctrl") then
-			if fancy.maxRows < #fancy.items then
-				fancy.maxRows = fancy.maxRows + 1
-				fancy.alphaScroll = 0
+function fancy.key(key)
+	-- Hide/show fancy window:
+	if kb.isDown("lctrl") or kb.isDown("rctrl") then
+		if kb.isDown("lshift") or kb.isDown("rshift") then
+			if key == "f" then
+				fancy.visible = not fancy.visible
 			end
-		elseif (fancy.indexRowDisplacement - 1) + fancy.maxRows < #fancy.items then
-			fancy.indexRowDisplacement = fancy.indexRowDisplacement + 1
-			fancy.alphaScroll = 255
 		end
-	elseif y > 0 then -- Down:
-		if kb.isDown("lctrl") or kb.isDown("rctrl") then
-			if fancy.maxRows > 1 then
-				fancy.maxRows = fancy.maxRows - 1
-				fancy.alphaScroll = 0
+	end 
+end
+
+function fancy.wheel(y)
+	if fancy.visible and overQuad(mo.getX(), mo.getY()) then
+		if y < 0 then -- Up:
+			if kb.isDown("lctrl") or kb.isDown("rctrl") then
+				if fancy.maxRows < #fancy.items then
+					fancy.maxRows = fancy.maxRows + 1
+					fancy.alphaScroll = 0
+				end
+			elseif (fancy.indexRowDisplacement - 1) + fancy.maxRows < #fancy.items then
+				fancy.indexRowDisplacement = fancy.indexRowDisplacement + 1
+				fancy.alphaScroll = 255
 			end
-		elseif fancy.indexRowDisplacement > 1 then
-			fancy.indexRowDisplacement = fancy.indexRowDisplacement - 1
-			fancy.alphaScroll = 255
+		elseif y > 0 then -- Down:
+			if kb.isDown("lctrl") or kb.isDown("rctrl") then
+				if fancy.maxRows > 1 then
+					fancy.maxRows = fancy.maxRows - 1
+					fancy.alphaScroll = 0
+				end
+			elseif fancy.indexRowDisplacement > 1 then
+				fancy.indexRowDisplacement = fancy.indexRowDisplacement - 1
+				fancy.alphaScroll = 255
+			end
 		end
 	end
 end
