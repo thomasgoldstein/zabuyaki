c99524962cabd6948814753a0442c4ebb8ab023b
Thomas Goldstein <stifu@free.fr>
2016-03-25 20:37:27 +0100

    Redraw Rick's head during his standing animation

--- res/rick.lua
+++ res/rick.lua
@@ -1,7 +1,7 @@
 print("rick.lua loaded")
 
 local image_w = 224 --This info can be accessed with a Love2D call
-local image_h = 133 --after the image has been loaded
+local image_h = 134 --after the image has been loaded
 
 local function q(x,y,w,h)
 	return love.graphics.newQuad(x, y, w, h, image_w, image_h)
@@ -24,124 +24,124 @@ return {
 			-- q = Love.graphics.newQuad( X, Y, Width, Height, Image_W, Image_H),
 			-- ox,oy pivots offsets from the top left corner of the quad
 			-- duration = 0.1, func = fun
-			{ q = q(2, 2, 47, 63), ox = 24, oy = 62 }, --stand 1
-			{ q = q(51,3,48,62), ox = 24, oy = 61 }, --stand 2
-			{ q = q(101,4,49,61), ox = 24, oy = 60 }, --stand 3
-			{ q = q(51,3,48,62), ox = 24, oy = 61 }, --stand 2
+			{ q = q(2,2,44,64), ox = 22, oy = 63 }, --stand 1
+			{ q = q(48,3,45,63), ox = 22, oy = 62 }, --stand 2
+			{ q = q(95,4,46,62), ox = 22, oy = 61 }, --stand 3
+			{ q = q(48,3,45,63), ox = 22, oy = 62 }, --stand 2
 			frame_duration = 0.167
 		},
 		walk = { -- 1 2 3 4 5 6
-			{ q = q(2,67,35,64), ox = 17, oy = 63 }, --walk 1
-			{ q = q(39,67,35,64), ox = 17, oy = 63 }, --walk 2
-			{ q = q(76,68,35,63), ox = 17, oy = 62, duration = 0.25 }, --walk 3
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4
-			{ q = q(150,67,35,64), ox = 17, oy = 63 }, --walk 5
-			{ q = q(187,68,35,63), ox = 17, oy = 62, duration = 0.25 }, --walk 6
+			{ q = q(2,68,35,64), ox = 17, oy = 63 }, --walk 1
+			{ q = q(39,68,35,64), ox = 17, oy = 63 }, --walk 2
+			{ q = q(76,69,35,63), ox = 17, oy = 62, duration = 0.25 }, --walk 3
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4
+			{ q = q(150,68,35,64), ox = 17, oy = 63 }, --walk 5
+			{ q = q(187,69,35,63), ox = 17, oy = 62, duration = 0.25 }, --walk 6
 			frame_duration = 0.167
 		},
 		run = { -- 1 2 3 4 5 6 temp ruu
-			{ q = q(2,67,35,64), ox = 17, oy = 63 }, --walk 1
-			{ q = q(39,67,35,64), ox = 17, oy = 63 }, --walk 2
-			{ q = q(76,68,35,63), ox = 17, oy = 62, duration = 0.15 }, --walk 3
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4
-			{ q = q(150,67,35,64), ox = 17, oy = 63 }, --walk 5
-			{ q = q(187,68,35,63), ox = 17, oy = 62, duration = 0.15 }, --walk 6
+			{ q = q(2,68,35,64), ox = 17, oy = 63 }, --walk 1
+			{ q = q(39,68,35,64), ox = 17, oy = 63 }, --walk 2
+			{ q = q(76,69,35,63), ox = 17, oy = 62, duration = 0.15 }, --walk 3
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4
+			{ q = q(150,68,35,64), ox = 17, oy = 63 }, --walk 5
+			{ q = q(187,69,35,63), ox = 17, oy = 62, duration = 0.15 }, --walk 6
 			frame_duration = 0.1
 		},
 		jumpUp = {
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4			frame_duration = 5
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4			frame_duration = 5
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4			frame_duration = 5
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4			frame_duration = 5
 		},
 		jumpDown = {
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4			frame_duration = 5
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4			frame_duration = 5
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4			frame_duration = 5
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4			frame_duration = 5
 		},
 		duck = {
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4			frame_duration = 0.5
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4			frame_duration = 0.5
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4			frame_duration = 0.5
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4			frame_duration = 0.5
 		},
 		dash = {
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4
 			frame_duration = 0.2
 		},
 		combo12 = {
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4
-			{ q = q(113,67,35,64), ox = 17, oy = 62 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 62 }, --walk 4
 			frame_duration = 0.1
 		},
 		combo3 = {
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4
-			{ q = q(113,67,35,64), ox = 17, oy = 62 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 62 }, --walk 4
 			frame_duration = 0.1
 		},
 		combo4 = {
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4
-			{ q = q(113,67,35,64), ox = 17, oy = 62 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 62 }, --walk 4
 			frame_duration = 0.1
 		},
 		combo5 = {
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4
-			{ q = q(113,67,35,64), ox = 17, oy = 62 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 62 }, --walk 4
 			frame_duration = 0.1
 		},
 		fall = {
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4
 			frame_duration = 0.2
 		},
 		dead = {
-			{ q = q(113,67,35,54), ox = 17, oy = 53 }, --head
-			{ q = q(113,67,35,44), ox = 17, oy = 43 }, --head
-			{ q = q(113,67,35,33), ox = 17, oy = 32 }, --head
-			{ q = q(113,67,35,23), ox = 17, oy = 22,duration = 60 }, --head
+			{ q = q(113,68,35,54), ox = 17, oy = 53 }, --head
+			{ q = q(113,68,35,44), ox = 17, oy = 43 }, --head
+			{ q = q(113,68,35,33), ox = 17, oy = 32 }, --head
+			{ q = q(113,68,35,23), ox = 17, oy = 22,duration = 60 }, --head
 			frame_duration = 0.3
 		},
 		hurtFace = {
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4
 			frame_duration = 0.2
 		},
 		hurtStomach = {
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4
 			frame_duration = 0.2
 		},
 		jumpAttackForwardUp = {
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4
 			frame_duration = 5
 		},
 		jumpAttackForwardDown = {
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4
 			frame_duration = 5
 		},
 		jumpAttackWeakUp = {
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4
 			frame_duration = 5
 		},
 		jumpAttackWeakDown = {
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4
 			frame_duration = 5
 		},
 		jumpAttackStillUp = {
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4
 			frame_duration = 5
 		},
 		jumpAttackStillDown = {
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4
 			frame_duration = 0.4
 		},
 		sideStepUp = {
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4
 		},
 		sideStepDown = {
-			{ q = q(113,67,35,64), ox = 17, oy = 63 }, --walk 4
+			{ q = q(113,68,35,64), ox = 17, oy = 63 }, --walk 4
 		},
 	} --offsets
 
