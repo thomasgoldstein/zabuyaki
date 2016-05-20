1298056d9afe4d63da328e7a6d367aa5015ad26b
Don Miguel <mikhail.bratus@gmail.com>
2016-05-21 00:03:58 +0500

    new sfx preload module

---
+++ res/preload_sfx.lua
@@ -0,0 +1,39 @@
+--
+-- Date: 19.05.2016
+-- Time: 11:44
+--
+
+local SFX = {}
+SFX.play = function(alias, func)
+    local s = SFX[alias]
+    TEsound.play(s.src, s.pitch, s.volume, func)
+end
+
+SFX.load = function(alias, s, pitch, volume)
+    local src = love.audio.newSource(s, "static")
+    src:setVolume(0)
+    src:play()
+    src:stop()
+    src:setVolume(volume or 1)
+    assert(SFX[alias] == nil, "Sound FX alias '"..alias.."' not found")
+    SFX[alias] = {src = s, pitch = pitch, volume = volume}
+--    return src
+end
+
+SFX.load("step","res/sfx/step.wav", nil, 0.5)
+SFX.load("air","res/sfx/attack1.wav", nil, 0.5)
+SFX.load("pickup1","res/sfx/pickup1.wav", nil, 1)
+SFX.load("pickup2","res/sfx/pickup2.wav", nil, 1)
+
+SFX.load("hit","res/sfx/hit3.wav", nil, 0.5)
+SFX.load("jump","res/sfx/jump.wav", nil, 1)
+SFX.load("land","res/sfx/land.wav", nil, 1)
+SFX.load("fall","res/sfx/fall.wav", nil, 1)
+SFX.load("grunt1","res/sfx/grunt1.wav", nil, 1)
+
+--[[local f2 = function() SFX.play("hit2",f3) end
+local f3 = function() SFX.play("hit3") end]]
+
+--SFX.play("hit")
+
+return SFX
