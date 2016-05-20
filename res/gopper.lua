c74683d1f9e533ad29cace92e4868adabbf20f77
Thomas Goldstein <stifu@free.fr>
2016-05-20 17:23:25 +0200

    Add Gopper's idle (squatting) frame

--- res/gopper.lua
+++ res/gopper.lua
@@ -1,5 +1,5 @@
 local image_w = 196 --This info can be accessed with a Love2D call
-local image_h = 308 --after the image has been loaded
+local image_h = 246 --after the image has been loaded
 
 local function q(x,y,w,h)
     return love.graphics.newQuad(x, y, w, h, image_w, image_h)
@@ -38,9 +38,9 @@ return {
             delay = 0.167
         },
         walk = {
-			{ q = q(2,66,36,62), ox = 18, oy = 61 }, --walk 1
+			{ q = q(116,2,36,62), ox = 18, oy = 61 }, --walk 1
             { q = q(2,2,36,62), ox = 18, oy = 61 }, --stand 1
-			{ q = q(40,67,38,61), ox = 18, oy = 60 }, --walk 2
+			{ q = q(154,3,38,61), ox = 18, oy = 60 }, --walk 2
             { q = q(2,2,36,62), ox = 18, oy = 61 }, --stand 1
             loop = true,
             delay = 0.167
@@ -67,35 +67,36 @@ return {
             delay = 0.16
         },
         combo1 = {
-            { q = q(2,130,62,61), ox = 18, oy = 60, func = combo_attack }, --punch
+            { q = q(2,66,62,61), ox = 18, oy = 60, func = combo_attack }, --punch
             delay = 0.2
         },
         fall = {
-			{ q = q(2,263,67,43), ox = 33, oy = 42, delay = 0.8 }, --falling
-            { q = q(71,264,65,42), ox = 32, oy = 29, delay = 3 }, --lying down
-			{ q = q(138,257,56,49), ox = 25, oy = 48 }, --getting up
+			{ q = q(2,200,67,43), ox = 33, oy = 42, delay = 0.8 }, --falling
+			{ q = q(71,201,65,42), ox = 32, oy = 29, delay = 3 }, --lying down
+			{ q = q(138,194,56,49), ox = 25, oy = 48 }, --getting up
             delay = 0.2
         },
         getup = {
-            { q = q(71,264,65,42), ox = 32, oy = 29, delay = 1 }, --lying down
-			{ q = q(138,257,56,49), ox = 25, oy = 48 }, --getting up
+            { q = q(71,201,65,42), ox = 32, oy = 29, delay = 1 }, --lying down
+			{ q = q(138,194,56,49), ox = 25, oy = 48 }, --getting up
+			{ q = q(66,72,38,56), ox = 19, oy = 55 }, --idle
             delay = 0.2
         },
         dead = {
-			{ q = q(138,257,56,49), ox = 25, oy = 48, delay = 1 }, --getting up
+			{ q = q(138,194,56,49), ox = 25, oy = 48, delay = 1 }, --getting up
             { q = q(71,264,65,42), ox = 32, oy = 29 }, --lying down
             delay = 65
         },
         hurtHigh = {
-            { q = q(2,193,39,62), ox = 21, oy = 61, delay = 0.03 }, --hh1
-			{ q = q(43,193,44,62), ox = 26, oy = 61 }, --hh2
-            { q = q(2,193,39,62), ox = 21, oy = 61, delay = 0.1 }, --hh1
+            { q = q(2,130,39,62), ox = 21, oy = 61, delay = 0.03 }, --hh1
+			{ q = q(43,130,44,62), ox = 26, oy = 61 }, --hh2
+            { q = q(2,130,39,62), ox = 21, oy = 61, delay = 0.1 }, --hh1
             delay = 0.3
         },
         hurtLow = {
-            { q = q(89,194,37,61), ox = 19, oy = 60 , delay = 0.03 }, --hl1
-			{ q = q(128,196,42,59), ox = 20, oy = 58 }, --hl2
-            { q = q(89,194,37,61), ox = 19, oy = 60 , delay = 0.1 }, --hl1
+            { q = q(89,131,37,61), ox = 19, oy = 60, delay = 0.03 }, --hl1
+			{ q = q(128,133,42,59), ox = 20, oy = 58 }, --hl2
+            { q = q(89,131,37,61), ox = 19, oy = 60, delay = 0.1 }, --hl1
             delay = 0.3
         },
         jumpAttackForward = {
@@ -138,7 +139,7 @@ return {
             { q = q(2,2,36,62), ox = 18, oy = 61 }, --stand 1
         },
         grabbed = {
-            { q = q(43,193,44,62), ox = 26, oy = 61 }, --hh2
+            { q = q(43,130,44,62), ox = 26, oy = 61 }, --hh2
             delay = 0.1
         },
 
