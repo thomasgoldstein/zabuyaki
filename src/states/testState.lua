ac896b05776e1e52bb6359177cb9d014e533d407
Don Miguel <mikhail.bratus@gmail.com>
2016-05-23 16:28:47 +0500

    update use setToughness to vary max HP of the test levelenemy

--- src/states/testState.lua
+++ src/states/testState.lua
@@ -44,29 +44,29 @@ function testState:enter()
     gopper1 = Gopper:new("GOPNIK", GetInstance("res/gopper.lua"), button3, 270, 204, {255,255,255, 255})
  	gopper2 = Gopper:new("GOPNIK 2", GetInstance("res/gopper.lua"), button3, 360, 184, {255,255,255, 255})
     gopper2.shader = sh_gopper2
-    gopper2.toughness = 1
+    gopper2:setToughness(1)
  	gopper3 = Gopper:new("GOPNIK 3", GetInstance("res/gopper.lua"), button3, 470, 190, {255,255,255, 255})
     gopper3.shader = sh_gopper3
-    gopper3.toughness = 2
+    gopper3:setToughness(2)
     gopper4 = Gopper:new("GOPNIK 4", GetInstance("res/gopper.lua"), button3, 320, 200-24, {255,255,255, 255})
     gopper4.shader = sh_gopper4
-    gopper4.toughness = 3
+    gopper4:setToughness(3)
     gopper5 = Gopper:new("UNNIE", GetInstance("res/gopper.lua"), button3, 600, 204, {255,255,255, 255})
-    gopper5.toughness = 4
+    gopper5:setToughness(4)
     gopper6 = Gopper:new("DORMY", GetInstance("res/gopper.lua"), button3, 500, 200-4, {255,255,255, 255})
-    gopper6.toughness = 5
+    gopper6:setToughness(5)
 
     dummy3 = Gopper:new("RICKER 3", GetInstance("res/rick.lua"), button3, 400, 200-30, {255,255,255, 255})
     dummy3.shader = sh_rick3
     dummy3.horizontal = -1
     dummy3.face = -1
-    dummy3.toughness = 5
+    dummy3:setToughness(5)
 
     dummy4 = Rick:new("Dummie RICK 4", GetInstance("res/rick.lua"), button3, 220, 150, {255,255,255, 255})
     dummy4.shader = sh_rick4
     dummy4.horizontal = -1
     dummy4.face = -1
-    dummy4.toughness = 5
+    dummy4:setToughness(5)
 
     dummy5 = Temper:new("LOCKY", GetInstance("res/rick.lua"), button3, 120, 200-24, {255,255,255, 255})
     dummy5.shader = sh_rick5
