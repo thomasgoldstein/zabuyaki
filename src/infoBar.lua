2809f91705f2934922a855f505035224e7f9f59b
Don Miguel <mikhail.bratus@gmail.com>
2016-05-26 17:34:57 +0500

    new infoBars items compatibility

--- src/infoBar.lua
+++ src/infoBar.lua
@@ -46,18 +46,27 @@ end
 
 function InfoBar:initialize(source)
     self.source = source
-    self.icon_sprite = source.sprite.def.sprite_sheet
-    self.icon_q = source.sprite.def.animations["icon"][1].q  --quad
-    self.icon_color = source.color
     self.name = source.name or "Unknown"
     self.extra_text = "EXTRA TEXT"
-    self.hp = 1
-    self.old_hp = 1
     self.color = {155,110,20}
-    self.max_hp = source.max_hp
     self.cool_down = 0
     self.id = self.source.id
-    self.x, self.y, self.face = bars_coords[self.id].x, bars_coords[self.id].y, bars_coords[self.id].face
+    if source.type == "item" then
+        self.icon_sprite = source.sprite
+        self.icon_q = source.sprite.icon_q  --quad
+        self.icon_color = { r= 255, g = 255, b = 255, a = 255 }
+        self.hp = 1
+        self.old_hp = 1
+        self.x, self.y, self.face = 0, 0, 1
+    else --Player / enemy / object
+        self.icon_sprite = source.sprite.def.sprite_sheet
+        self.icon_q = source.sprite.def.animations["icon"][1].q  --quad
+        self.icon_color = source.color
+        self.max_hp = source.max_hp
+        self.hp = 1
+        self.old_hp = 1
+        self.x, self.y, self.face = bars_coords[self.id].x, bars_coords[self.id].y, bars_coords[self.id].face
+    end
 end
 
 function InfoBar:setAttacker(attacker_source)
@@ -68,10 +77,63 @@ function InfoBar:setAttacker(attacker_source)
         id = attacker_source.id
     end
     if id <= MAX_PLAYERS and self.id > MAX_PLAYERS then
-        --TODO we might have 4 players
         self.x, self.y, self.face = bars_coords[id].x, bars_coords[id].y + v_g, bars_coords[id].face
     end
     return self
+end
+
+function InfoBar:setPicker(picker_source)
+    id = picker_source.id
+    if id <= MAX_PLAYERS then
+        self.x, self.y, self.face = bars_coords[id].x, bars_coords[id].y + v_g, bars_coords[id].face
+    end
+    self.cool_down = 3
+    return self
+end
+function InfoBar:draw_enemy_bar(l,t,w,h)
+    love.graphics.setColor(0, 50, 50, transp_bg)
+    love.graphics.rectangle("fill", l + self.x, t + self.y - icon_height - 1, icon_width + 2, icon_height + 2 )
+    love.graphics.rectangle("fill", l + self.x, t + self.y , calcBarWidth(self), bar_height )
+    if self.old_hp > 0 then
+        if self.source.hp > self.hp then
+            love.graphics.setColor(got_color[1], got_color[2], got_color[3], transp_got)
+        else
+            love.graphics.setColor(lost_color[1], lost_color[2], lost_color[3], transp_lost)
+        end
+        love.graphics.rectangle("fill", l + self.x + 2, t + self.y + 2, (calcBarWidth(self) - 2) * self.old_hp / self.max_hp, (bar_height - 4) )
+    end
+    if self.hp > 0 then
+        love.graphics.setColor(self.color[1], self.color[2], self.color[3], transp_bar)
+        love.graphics.rectangle("fill", l + self.x + 2, t + self.y + 2, (calcBarWidth(self) - 2) * self.hp / self.max_hp, (bar_height - 4) )
+    end
+
+    love.graphics.setColor(self.icon_color.r, self.icon_color.g, self.icon_color.b, transp_icon)
+    love.graphics.draw (
+        image_bank[self.icon_sprite],
+        self.icon_q, --Current frame of the current animation
+        l + self.x + 1, t + self.y - icon_height
+    )
+    love.graphics.setColor(255, 255, 255, transp_name)
+    love.graphics.print(self.name, l + self.x + icon_width + 4, t + self.y-13)
+end
+
+function InfoBar:draw_item_bar(l,t,w,h)
+    love.graphics.setColor(0, 50, 50, transp_bg)
+    love.graphics.rectangle("fill", l + self.x, t + self.y - icon_height - 1, icon_width + 2, icon_height + 2 )
+    love.graphics.rectangle("fill", l + self.x, t + self.y , calcBarWidth(self), bar_height )
+    if self.hp > 0 then
+        love.graphics.setColor(self.color[1], self.color[2], self.color[3], transp_bar)
+        love.graphics.rectangle("fill", l + self.x + 2, t + self.y + 2, (calcBarWidth(self) - 2) * self.hp / self.max_hp, (bar_height - 4) )
+    end
+
+    love.graphics.setColor(self.icon_color.r, self.icon_color.g, self.icon_color.b, transp_icon)
+    love.graphics.draw (
+        image_bank[self.icon_sprite],
+        self.icon_q, --Current frame of the current animation
+        l + self.x + 1, t + self.y - icon_height
+    )
+    love.graphics.setColor(255, 255, 255, transp_name)
+    love.graphics.print(self.name, l + self.x + icon_width + 4, t + self.y-13)
 end
 
 function InfoBar:draw(l,t,w,h)
@@ -80,56 +142,10 @@ function InfoBar:draw(l,t,w,h)
     end
     if self.face == 1 then
         --left side
-        love.graphics.setColor(0, 50, 50, transp_bg)
-        love.graphics.rectangle("fill", l + self.x, t + self.y - icon_height - 1, icon_width + 2, icon_height + 2 )
-        love.graphics.rectangle("fill", l + self.x, t + self.y , calcBarWidth(self), bar_height )
-        if self.old_hp > 0 then
-            if self.source.hp > self.hp then
-                love.graphics.setColor(got_color[1], got_color[2], got_color[3], transp_got)
-            else
-                love.graphics.setColor(lost_color[1], lost_color[2], lost_color[3], transp_lost)
-            end
-            love.graphics.rectangle("fill", l + self.x + 2, t + self.y + 2, (calcBarWidth(self) - 2) * self.old_hp / self.max_hp, (bar_height - 4) )
-        end
-        if self.hp > 0 then
-            love.graphics.setColor(self.color[1], self.color[2], self.color[3], transp_bar)
-            love.graphics.rectangle("fill", l + self.x + 2, t + self.y + 2, (calcBarWidth(self) - 2) * self.hp / self.max_hp, (bar_height - 4) )
-        end
-
-        love.graphics.setColor(self.icon_color.r, self.icon_color.g, self.icon_color.b, transp_icon)
-        love.graphics.draw (
-            image_bank[self.icon_sprite],
-            self.icon_q, --Current frame of the current animation
-            l + self.x + 1, t + self.y - icon_height
-        )
-        love.graphics.setColor(255, 255, 255, transp_name)
-        love.graphics.print(self.name, l + self.x + icon_width + 4, t + self.y-13)
+        self:draw_enemy_bar(l,t,w,h)
     else
         --right side
-        love.graphics.setColor(0, 50, 50, transp_bg)
-        love.graphics.rectangle("fill", l + self.x, t + self.y - icon_height - 1, icon_width + 2, icon_height + 2 )
-        love.graphics.rectangle("fill", l + self.x, t + self.y, calcBarWidth(self), bar_height )
-        if self.old_hp > 0 then
-            if self.source.hp > self.hp then
-                love.graphics.setColor(got_color[1], got_color[2], got_color[3], transp_got)
-            else
-                love.graphics.setColor(lost_color[1], lost_color[2], lost_color[3], transp_lost)
-            end
-            love.graphics.rectangle("fill", l + self.x + 2, t + self.y + 2, (calcBarWidth(self) -2) * self.old_hp / self.max_hp, (bar_height - 4) )
-        end
-        if self.hp > 0 then
-            love.graphics.setColor(self.color[1], self.color[2], self.color[3], transp_bar)
-            love.graphics.rectangle("fill", l + self.x + 2, t + self.y + 2, (calcBarWidth(self) -2) * self.hp / self.max_hp, (bar_height - 4) )
-        end
-
-        love.graphics.setColor(self.icon_color.r, self.icon_color.g, self.icon_color.b, transp_icon)
-        love.graphics.draw (
-            image_bank[self.icon_sprite],
-            self.icon_q, --Current frame of the current animation
-            l + self.x + 1, t + self.y - icon_height
-        )
-        love.graphics.setColor(255, 255, 255, transp_name)
-        love.graphics.print(self.name, l + self.x + icon_width + 4, t + self.y-13)
+        self:draw_enemy_bar(l,t,w,h)
     end
 end
 
