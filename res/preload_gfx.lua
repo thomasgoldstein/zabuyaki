ba9aec8aa97ed64e579d82cf6867857740353daa
Don Miguel <mikhail.bratus@gmail.com>
2016-05-27 14:13:47 +0500

    new fonts show case

--- res/preload_gfx.lua
+++ res/preload_gfx.lua
@@ -18,9 +18,9 @@ gfx.items.apple = {sprite = image, q = q(2,2,18,17), ox = 9, oy = 16, icon_q = q
 gfx.items.chicken = {sprite = image, q = q(2,21,30,19), ox = 12, oy = 18, icon_q = q(15, 21, icon_width, icon_height) }
 gfx.items.beef = {sprite = image, q = q(2,42,30,19), ox = 15, oy = 18, icon_q = q(2, 44, icon_width, icon_height) }
 
-gfx.font.clock = love.graphics.newFont( "res/font/alarm clock.ttf", 48 )
-gfx.font.pixel = love.graphics.newFont( "res/font/pixeldart.ttf", 16 )
-gfx.font.arcade = love.graphics.newFont( "res/font/karmatic_arcade.ttf", 16 )
+gfx.font.clock = love.graphics.newFont( "res/font/alarm clock.ttf", 36 )
+gfx.font.pixel = love.graphics.newFont( "res/font/pixeldart.ttf", 36 )
+gfx.font.arcade = love.graphics.newFont( "res/font/karmatic_arcade.ttf", 36 )
 gfx.font.arcade2 = love.graphics.newFont( "res/font/arcade_i.ttf", 16 )
 gfx.font.arcade3 = love.graphics.newFont( "res/font/arcade_n.ttf", 16 )
 gfx.font.arcade4 = love.graphics.newFont( "res/font/arcade_r.ttf", 16 )
--- src/states/menuState.lua
+++ src/states/menuState.lua
@@ -8,11 +8,20 @@ function menuState:update(dt)
 
 end
 
+local fonts_set = {
+	gfx.font.arcade, gfx.font.arcade2, gfx.font.arcade3,
+	gfx.font.arcade4, gfx.font.clock, gfx.font.pixel
+}
+
 function menuState:draw()
+
 	local text = "Press SPACE to play"
 	local font = gfx.font.arcade3
-	love.graphics.setFont(font)
-	love.graphics.print(text, (640 - font:getWidth(text)) / 2, 400)
+	for i = 1,6 do
+		font = fonts_set[i]
+		love.graphics.setFont(font)
+		love.graphics.print(text, (640 - font:getWidth(text)) / 2, 40+ 50 * i)
+	end
 end
 
 function menuState:keypressed(key, unicode)
