9fd2218982e269da2d40e77786f4ed740eb902b7
Don Miguel <mikhail.bratus@gmail.com>
2016-06-01 11:22:11 +0500

    new Niko enemy class

--- main.lua
+++ main.lua
@@ -42,6 +42,7 @@ function love.load(arg)
 	Rick = require "src/units/rick_unit"
 	Item = require "src/units/item"
 	Gopper = require "src/units/gopper_enemy"
+	Niko = require "src/units/niko_enemy"
 	Temper = require "src/units/temper_enemy"
 	InfoBar = require "src/infoBar"
 
---
+++ src/units/niko_enemy.lua
@@ -0,0 +1,29 @@
+local class = require "lib/middleclass"
+
+local Niko = class('Niko', Gopper)
+
+local function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
+    return x1 < x2+w2 and
+            x2 < x1+w1 and
+            y1 < y2+h2 and
+            y2 < y1+h1
+end
+local function dist(x1,y1, x2,y2) return ((x2-x1)^2+(y2-y1)^2)^0.5 end
+local function rand1()
+    if love.math.random() < 0.5 then
+        return -1
+    else
+        return 1
+    end
+end
+
+local function nop() --[[print "nop"]] end
+
+function Niko:initialize(name, sprite, input, x, y, color)
+    self.tx, self.ty = x, y
+    self.move = tween.new(0.01, self, {tx = x, ty = y})
+    Gopper.initialize(self, name, sprite, input, x, y, color)
+    self:setState(self.intro)
+end
+
+return Niko
