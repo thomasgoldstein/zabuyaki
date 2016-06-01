4081446a06b4478044374ff6ec15c665c5086879
Don Miguel <mikhail.bratus@gmail.com>
2016-06-02 00:02:27 +0500

    new menu with mouse support

--- src/states/pauseState.lua
+++ src/states/pauseState.lua
@@ -4,6 +4,10 @@
 pauseState = {}
 
 local time = 0
+local screen_width = 640
+local screen_height = 480
+local menu_item_h = 40
+local menu_y_offset = 200 - menu_item_h
 
 local txt_paused = love.graphics.newText( gfx.font.arcade2, "PAUSED" )
 local txt_continue = love.graphics.newText( gfx.font.arcade4, "Continue" )
@@ -14,17 +18,27 @@ local txt_quit_hint = love.graphics.newText( gfx.font.arcade4, "Are you sure you
 local txt_quick_save_hint = love.graphics.newText( gfx.font.arcade4, "quick save doesn't let you choose\na save, there is only one at most" )
 local txt_press_action_hint = love.graphics.newText( gfx.font.arcade4, "Return to the game" )
 
+local txt_items = {txt_continue, txt_quick_save, txt_quit}
 local txt_hints = {txt_press_action_hint, txt_quick_save_hint, txt_quit_hint}
 
-local rick_spr = GetInstance("res/rick.lua")
-SetSpriteAnim(rick_spr,"fallen")
-rick_spr.size_scale = 4
+--local rick_spr = GetInstance("res/rick.lua")
+--SetSpriteAnim(rick_spr,"fallen")
+--rick_spr.size_scale = 4
 
-local menu_state = 0
+local menu_state = 1
+local mouse_x, mouse_y = 0,0
+
+local function CheckPointCollision(x,y, x1,y1,w1,h1)
+    return x < x1+w1 and
+            x >= x1 and
+            y < y1+h1 and
+            y >= y1
+end
 
 function pauseState:enter()
     TEsound.volume("music", 0.4)
-    menu_state = 0
+    menu_state = 1
+    mouse_x, mouse_y = 0,0
 end
 
 function pauseState:leave()
@@ -33,10 +47,10 @@ end
 
 function pauseState:update(dt)
     time = time + dt
-    UpdateInstance(rick_spr, dt)
-    if rick_spr.cur_anim ~= "stand" and rick_spr.isFinished then
-        SetSpriteAnim(rick_spr,"stand")
-    end
+--    UpdateInstance(rick_spr, dt)
+--    if rick_spr.cur_anim ~= "stand" and rick_spr.isFinished then
+--        SetSpriteAnim(rick_spr,"stand")
+--    end
 end
 
 function pauseState:draw()
@@ -46,18 +60,31 @@ function pauseState:draw()
     end
 
     love.graphics.setColor(255, 255, 255, 255)
-    DrawInstance(rick_spr, 200, 370)
-
-    love.graphics.setColor(255, 255, 255, 255)
-    love.graphics.draw(txt_continue, 400, 200)
-    love.graphics.draw(txt_quick_save, 400, 240)
-    love.graphics.draw(txt_quit, 400, 280)
-    love.graphics.rectangle("line", 380, 200 - 8 + menu_state * 40, 230, 32 )
+    for i = 1,#txt_items do
+        local x = screen_width / 2 - txt_items[i]:getWidth()/2
+        local y = menu_y_offset + i * menu_item_h
+        local w, h = txt_items[i]:getDimensions()
+        if i == menu_state then
+            love.graphics.rectangle("line", x - 6, y - 6, w + 12, h + 10 )
+        end
+        if CheckPointCollision(mouse_x, mouse_y, x - 6,y -6 , w + 12, h + 10) then
+            menu_state = i
+            return pauseState:keypressed("x")
+        end
+        love.graphics.draw(txt_items[i], x, y )
+    end
 
     love.graphics.setColor(255, 255, 255, 200 + math.sin(time)*55)
     love.graphics.draw(txt_paused, (640 - txt_paused:getWidth()) / 2, 40)
-    love.graphics.setColor(255, 255, 255, math.abs(math.sin(time*2))*255)
-    love.graphics.draw(txt_hints[menu_state + 1], (640 - txt_hints[menu_state + 1]:getWidth()) / 2, 480 - 80)
+
+    love.graphics.setColor(255, 255, 255, 200 - math.sin(time)*55)
+    love.graphics.draw(txt_hints[menu_state], (640 - txt_hints[menu_state]:getWidth()) / 2, screen_height - 80)
+end
+
+function pauseState:mousepressed( x, y, button, istouch )
+    if button == 1 then
+        mouse_x, mouse_y = x, y
+    end
 end
 
 function pauseState:keypressed(key, unicode)
@@ -69,20 +96,20 @@ function pauseState:keypressed(key, unicode)
     elseif key == 'down' then
         menu_state = menu_state + 1
     elseif key == "x" then
-        if menu_state == 0 then
---            Gamestate.pop(testState)
+        if menu_state == 1 then
             return Gamestate.pop()
-        elseif menu_state == 2 then
---            Gamestate.switch(
+        elseif menu_state == 3 then
             return Gamestate.switch(titleState)
         end
     elseif key == 'c' or key == "escape" then
         return Gamestate.pop()
     end
 
-    if menu_state < 0 then
-        menu_state = 0
-    elseif menu_state > 2 then
-        menu_state = 2
+    if menu_state < 1 then
+        menu_state = 1
+    elseif menu_state > 3 then
+        menu_state = 3
     end
+
+    mouse_x, mouse_y = 0, 0
 end
