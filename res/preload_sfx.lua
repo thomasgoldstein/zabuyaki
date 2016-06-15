537dba24b3c13c8120b4fd161051ba6e01ab018b
Don Miguel <mikhail.bratus@gmail.com>
2016-06-15 15:52:21 +0500

    fix wrong sfx function arguments order

--- res/preload_sfx.lua
+++ res/preload_sfx.lua
@@ -1,15 +1,14 @@
 --
 -- Date: 19.05.2016
--- Time: 11:44
 --
 
 local SFX = {}
 SFX.play = function(alias, func)
     local s = SFX[alias]
-    TEsound.play(s.src, s.pitch, s.volume, func)
+    TEsound.play(s.src, "sfx", s.volume, s.pitch, func)
 end
 
-SFX.load = function(alias, s, pitch, volume)
+SFX.load = function(alias, s, volume, pitch)
     local src = love.audio.newSource(s, "static")
     src:setVolume(0)
     src:play()
@@ -19,21 +18,21 @@ SFX.load = function(alias, s, pitch, volume)
     SFX[alias] = {src = s, pitch = pitch, volume = volume}
 --    return src
 end
-SFX.load("menu_select","res/sfx/menu_select.wav", nil, 0.5)
-SFX.load("menu_cancel","res/sfx/menu_cancel.wav", nil, 0.5)
-SFX.load("menu_move","res/sfx/menu_move.wav", nil, 0.5)
-SFX.load("menu_gamestart","res/sfx/menu_gamestart.wav", nil, 0.2)
+SFX.load("menu_select","res/sfx/menu_select.wav", 0.5)
+SFX.load("menu_cancel","res/sfx/menu_cancel.wav", 0.5)
+SFX.load("menu_move","res/sfx/menu_move.wav", 0.5)
+SFX.load("menu_gamestart","res/sfx/menu_gamestart.wav", 0.2)
 
-SFX.load("step","res/sfx/step.wav", nil, 0.5)
-SFX.load("air","res/sfx/attack1.wav", nil, 0.5)
-SFX.load("pickup1","res/sfx/pickup1.wav", nil, 1)
-SFX.load("pickup2","res/sfx/pickup2.wav", nil, 1)
+SFX.load("step","res/sfx/step.wav", 0.5)
+SFX.load("air","res/sfx/attack1.wav", 0.5)
+SFX.load("pickup1","res/sfx/pickup1.wav", 1)
+SFX.load("pickup2","res/sfx/pickup2.wav", 1)
 
-SFX.load("hit","res/sfx/hit3.wav", nil, 0.5)
-SFX.load("jump","res/sfx/jump.wav", nil, 1)
-SFX.load("land","res/sfx/land.wav", nil, 1)
-SFX.load("fall","res/sfx/fall.wav", nil, 1)
-SFX.load("grunt1","res/sfx/grunt1.wav", nil, 1)
+SFX.load("hit","res/sfx/hit3.wav", 0.5)
+SFX.load("jump","res/sfx/jump.wav", 1)
+SFX.load("land","res/sfx/land.wav", 1)
+SFX.load("fall","res/sfx/fall.wav", 1)
+SFX.load("grunt1","res/sfx/grunt1.wav", 1)
 
 --[[local f2 = function() SFX.play("hit2",f3) end
 local f3 = function() SFX.play("hit3") end]]
