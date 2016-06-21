a67adf1f96d2a80faae6b7b52bab39ae627c826c
Don Miguel <mikhail.bratus@gmail.com>
2016-06-21 11:13:46 +0500

    new fonts sizes for hero select

--- res/preload_gfx.lua
+++ res/preload_gfx.lua
@@ -23,9 +23,11 @@ gfx.font.pixel = love.graphics.newFont( "res/font/pixeldart.ttf", 36 )
 gfx.font.arcade = love.graphics.newFont( "res/font/karmatic_arcade.ttf", 12 )
 --title Zabu logo
 gfx.font.arcade2 = love.graphics.newFont( "res/font/arcade_i.ttf", 64 )
+gfx.font.arcade2x15 = love.graphics.newFont( "res/font/arcade_i.ttf", 44 )
 --info bars
 gfx.font.arcade3 = love.graphics.newFont( "res/font/arcade_n.ttf", 8 )
 gfx.font.arcade3x2 = love.graphics.newFont( "res/font/arcade_n.ttf", 16 )
+gfx.font.arcade3x3 = love.graphics.newFont( "res/font/arcade_n.ttf", 24 )
 --title - press space
 gfx.font.arcade4 = love.graphics.newFont( "res/font/arcade_n.ttf", 16 )
 gfx.font.arcade5 = love.graphics.newFont( "res/font/arcade_r.ttf", 16 )
