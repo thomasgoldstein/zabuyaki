bb5e3fae65da324290050b46960e7d31440fde37
Don Miguel <mikhail.bratus@gmail.com>
2016-06-27 23:29:55 +0500

    fix double tap detection
    now you can read the detected double tap
    during delta/2 time

--- src/keyTrace.lua
+++ src/keyTrace.lua
@@ -12,25 +12,39 @@ function KeyTrace:initialize(name, input, dir, delta)
     self.dir = dir
     self.delta = delta or 0.2
     self.time_left = self.delta
+    self.time_to_show = self.delta / 2
     self.double_tap = false
 end
 
 function KeyTrace:clear()
     self.time_left = self.delta
+    self.time_to_show = 0
     self.double_tap = false
 end
 
 function KeyTrace:update(dt)
+    if self.time_to_show > 0 then
+        --show true for some time to let it read
+        self.time_to_show = self.time_to_show - dt
+    else
+        self.double_tap = false
+    end
+    --double tap detect
     if self.time_left > 0 then
         self.time_left = self.time_left - dt
     end
+    if self.input:released(self.dir) then
+        self.double_tap = false
+    end
     if self.input:pressed(self.dir) then
         if self.time_left > 0 then
+            self.time_to_show = self.delta
             self.double_tap = true
         else
             self.double_tap = false
+            self.time_left = self.delta
         end
-        self.time_left = self.delta
+
     end
 end
 
