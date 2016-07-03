4d98ce748d54f854346c4d326d9c92e770c9cf68
Thomas Goldstein <stifu@free.fr>
2016-07-03 18:55:47 +0200

    Add +2 to the ox config of all of Chai's frames, to make up for his hair. This makes him horizontally centered the same way as the other characters.

--- src/def/char/chai.lua
+++ src/def/char/chai.lua
@@ -70,40 +70,40 @@ return {
 			-- q = Love.graphics.newQuad( X, Y, Width, Height, Image_W, Image_H),
 			-- ox,oy pivots offsets from the top left corner of the quad
 			-- delay = 0.1, func = func1, funcCont = func2
-			{ q = q(2,2,44,64), ox = 23, oy = 63 }, --stand 1
-			{ q = q(48,2,46,64), ox = 23, oy = 63 }, --stand 2
-			{ q = q(96,3,45,63), ox = 22, oy = 62 }, --stand 3
-			{ q = q(48,2,46,64), ox = 23, oy = 63 }, --stand 2
+			{ q = q(2,2,44,64), ox = 25, oy = 63 }, --stand 1
+			{ q = q(48,2,46,64), ox = 25, oy = 63 }, --stand 2
+			{ q = q(96,3,45,63), ox = 24, oy = 62 }, --stand 3
+			{ q = q(48,2,46,64), ox = 25, oy = 63 }, --stand 2
             loop = true,
 			delay = 0.175
 		},
 		walk = { -- 1 2 3 4 5 6
-			{ q = q(2,68,39,64), ox = 19, oy = 63 }, --walk 1
-			{ q = q(43,68,39,64), ox = 19, oy = 63 }, --walk 2
-			{ q = q(84,68,38,64), ox = 19, oy = 63, func = step_sfx, delay = 0.25 }, --walk 3
-			{ q = q(124,68,38,64), ox = 19, oy = 63 }, --walk 4
-			{ q = q(164,68,39,64), ox = 19, oy = 63 }, --walk 5
-			{ q = q(205,68,38,64), ox = 18, oy = 63, func = step_sfx, delay = 0.25 }, --walk 6
+			{ q = q(2,68,39,64), ox = 21, oy = 63 }, --walk 1
+			{ q = q(43,68,39,64), ox = 21, oy = 63 }, --walk 2
+			{ q = q(84,68,38,64), ox = 21, oy = 63, func = step_sfx, delay = 0.25 }, --walk 3
+			{ q = q(124,68,38,64), ox = 21, oy = 63 }, --walk 4
+			{ q = q(164,68,39,64), ox = 21, oy = 63 }, --walk 5
+			{ q = q(205,68,38,64), ox = 20, oy = 63, func = step_sfx, delay = 0.25 }, --walk 6
             loop = true,
             delay = 0.167
 		},
 		run = { -- 1 2 3 4 5 6
-			{ q = q(2,135,36,63), ox = 14, oy = 62 }, --run 1
-			{ q = q(40,134,45,63), ox = 23, oy = 63 }, --run 2
-			{ q = q(87,135,44,63), ox = 23, oy = 62, func = step_sfx2 }, --run 3
-			{ q = q(2,201,36,63), ox = 14, oy = 62 }, --run 4
-			{ q = q(40,200,46,64), ox = 22, oy = 63 }, --run 5
-			{ q = q(88,201,45,62), ox = 24, oy = 62, func = step_sfx2 }, --run 6
+			{ q = q(2,135,36,63), ox = 16, oy = 62 }, --run 1
+			{ q = q(40,134,45,63), ox = 25, oy = 63 }, --run 2
+			{ q = q(87,135,44,63), ox = 25, oy = 62, func = step_sfx2 }, --run 3
+			{ q = q(2,201,36,63), ox = 16, oy = 62 }, --run 4
+			{ q = q(40,200,46,64), ox = 24, oy = 63 }, --run 5
+			{ q = q(88,201,45,62), ox = 26, oy = 62, func = step_sfx2 }, --run 6
             loop = true,
             delay = 0.117
 		},
 		jump = {
-			{ q = q(44,266,39,67), ox = 23, oy = 66, delay = 0.4 }, --ju
-			{ q = q(85,266,43,63), ox = 21, oy = 67 }, --jd
+			{ q = q(44,266,39,67), ox = 25, oy = 66, delay = 0.4 }, --ju
+			{ q = q(85,266,43,63), ox = 23, oy = 67 }, --jd
 			delay = 5
 		},
 		duck = {
-			{ q = q(2,274,40,59), ox = 20, oy = 58 }, --duck
+			{ q = q(2,274,40,59), ox = 22, oy = 58 }, --duck
 			delay = 0.15
 		},
 		pickup = {
@@ -168,15 +168,15 @@ return {
 			delay = 65
 		},
 		hurtHigh = {
-			{ q = q(2,335,48,64), ox = 26, oy = 63 }, --hh1
-			{ q = q(52,335,50,64), ox = 29, oy = 63, delay = 0.2 }, --hh2
-			{ q = q(2,335,48,64), ox = 26, oy = 63 }, --hh1
+			{ q = q(2,335,48,64), ox = 28, oy = 63 }, --hh1
+			{ q = q(52,335,50,64), ox = 31, oy = 63, delay = 0.2 }, --hh2
+			{ q = q(2,335,48,64), ox = 28, oy = 63 }, --hh1
 			delay = 0.05
 		},
 		hurtLow = {
-			{ q = q(104,336,44,63), ox = 21, oy = 62 }, --hl1
-			{ q = q(150,337,42,62), ox = 19, oy = 61, delay = 0.2 }, --hl2
-			{ q = q(104,336,44,63), ox = 21, oy = 62 }, --hl1
+			{ q = q(104,336,44,63), ox = 23, oy = 62 }, --hl1
+			{ q = q(150,337,42,62), ox = 21, oy = 61, delay = 0.2 }, --hl2
+			{ q = q(104,336,44,63), ox = 23, oy = 62 }, --hl1
 			delay = 0.05
 		},
 		jumpAttackForward = {
@@ -234,8 +234,8 @@ return {
 			{ q = q(48,134,50,62), ox = 18, oy = 61 }, --run 2
 		},
 		grabbed = {
-			{ q = q(2,335,48,64), ox = 26, oy = 63 }, --hh1
-			{ q = q(52,335,50,64), ox = 29, oy = 63 }, --hh2
+			{ q = q(2,335,48,64), ox = 28, oy = 63 }, --hh1
+			{ q = q(52,335,50,64), ox = 31, oy = 63 }, --hh2
 			delay = 0.1
 		},
 
