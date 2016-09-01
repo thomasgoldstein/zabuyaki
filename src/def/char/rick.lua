b8232b1646ae4bb88c0a4f9dc3cf6647dc6137a7
Thomas Goldstein <stifu@free.fr>
2016-09-01 22:18:30 +0200

    Add Rick's grab swap animation

--- src/def/char/rick.lua
+++ src/def/char/rick.lua
@@ -240,7 +240,9 @@ return {
 			delay = 0.1
 		},
 		grabSwap = {
-			{ q = q(48,134,50,62), ox = 18, oy = 61 }, --run 2
+			{ q = q(137,1109,44,62), ox = 22, oy = 62 }, --grab swap 1.1
+			{ q = q(183,1109,37,62), ox = 18, oy = 62 }, --grab swap 1.2
+			delay = 0.1
 		},
 		grabbed = {
 			{ q = q(2,330,45,63), ox = 24, oy = 62 }, --hh1
