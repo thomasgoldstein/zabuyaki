a699e7745ab1a64b3d7a93461eabc69b28398f98
Don Miguel <mikhail.bratus@gmail.com>
2016-09-05 16:36:02 +0500

    new show debug info on the currently pressed P1 P2 P3 controls
    Record video and do frame by frame debugging

--- src/states/arcadeState.lua
+++ src/states/arcadeState.lua
@@ -184,8 +184,8 @@ function arcadeState:draw()
         background:draw(l, t, w, h)
         level_objects:draw(l,t,w,h)
 
-        -- debug draw bump boxes
         if GLOBAL_SETTING.DEBUG then
+            -- debug draw bump boxes
             local obj, _ = world:getItems()
             love.graphics.setColor(255, 0, 0, 50)
             for i = 1, #obj do
@@ -226,6 +226,45 @@ function arcadeState:draw()
         player3.infoBar:draw(0,0)
         if player3.victim_infoBar then
             player3.victim_infoBar:draw(0,0)
+        end
+    end
+
+    if GLOBAL_SETTING.DEBUG then
+        --debug draw P1 / P2 pressed buttons
+        local players = {player1, player2, player3}
+        for i = 1, #players do
+            local p = players[i]
+            local c = GLOBAL_SETTING.PLAYERS_COLORS[p.id]
+            local x = p.infoBar.x + 70
+            local y = p.infoBar.y + 18
+            love.graphics.setColor(0, 0, 0, 150)
+            love.graphics.rectangle("fill", x-2, y, 61, 9 )
+            love.graphics.setColor(c[1], c[2], c[3])
+
+            if p.b.fire:isDown()  then
+                love.graphics.print("F", x, y)
+            end
+            x = x + 10
+            if p.b.jump:isDown()  then
+                love.graphics.print("J", x, y)
+            end
+            x = x + 10
+            if p.b.horizontal:isDown(-1)  then
+                love.graphics.print("<", x, y)
+            end
+            x = x + 10
+            if p.b.horizontal:isDown(1)  then
+                love.graphics.print(">", x, y)
+            end
+            x = x + 10
+            if p.b.vertical:isDown(-1)  then
+                love.graphics.print("A", x, y)
+            end
+            x = x + 10
+            if p.b.vertical:isDown(1)  then
+                love.graphics.print("V", x, y)
+            end
+            x = x + 10
         end
     end
 end
