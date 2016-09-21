c5347ea0629e14a1b3545227e5da23d4d54166cb
Don Miguel <mikhail.bratus@gmail.com>
2016-09-21 15:56:12 +0500

    remove excessive sprites filtering

--- src/units/unit.lua
+++ src/units/unit.lua
@@ -212,11 +212,6 @@ function Unit:default_draw(l,t,w,h)
         else
             love.graphics.setColor(self.color.r, self.color.g, self.color.b, self.color.a)
 		end
-		if mainCamera:getScale() < 2 then
-			image_bank[self.sprite.def.sprite_sheet]:setFilter("linear", "linear", 2)
-		else
-			image_bank[self.sprite.def.sprite_sheet]:setFilter("nearest", "nearest")
-		end
 		if self.shader then
 			love.graphics.setShader(self.shader)
 		end
