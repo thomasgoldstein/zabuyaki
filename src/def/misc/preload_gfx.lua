748c0030660e5a69ea0640531fdf036e911f847c
Don Miguel <mikhail.bratus@gmail.com>
2016-10-02 23:25:42 +0500

    update quad function's constants to work with UI png

--- src/def/misc/preload_gfx.lua
+++ src/def/misc/preload_gfx.lua
@@ -1,26 +1,23 @@
 -- Date: 28.09.2016
 -- Preload common gfx and fonts
-local gfx = {items = {}, font = {}}
+local gfx = {items = {}, ui = {}, font = {}}
 
 local image_w = 34 --This info can be accessed with a Love2D call
 local image_h = 63 --after the image has been loaded
-
-local icon_width = 32
-local icon_height = 24
-
 local function q(x,y,w,h)
     return love.graphics.newQuad(x, y, w, h, image_w, image_h)
 end
-
 local image = love.graphics.newImage("res/img/misc/items.png")
 gfx.items.apple = {sprite = image, q = q(2,2,18,17), ox = 9, oy = 16, icon_q = q(2, 2, 18, 17)}
 gfx.items.chicken = {sprite = image, q = q(2,21,30,19), ox = 12, oy = 18, icon_q = q(2, 21, 30, 19) }
 gfx.items.beef = {sprite = image, q = q(2,42,30,19), ox = 15, oy = 18, icon_q = q(2, 42, 30, 19) }
 gfx.items.image = image --for items particles
 
+image_w = 32
+image_h = 24
 local ui = love.graphics.newImage("res/img/misc/ui.png")
-gfx.ui = {image = ui}
-gfx.ui.dead_icon = {sprite = ui, q = q(2,2,32,24), ox = 16, oy = 12, icon_q = q(2,2,32,24)}
+gfx.ui.dead_icon = {sprite = ui, q = q(2,2,32,24), ox = 16, oy = 23, icon_q = q(2,2,32,24)}
+gfx.ui.image = ui
 
 gfx.font.clock = love.graphics.newFont( "res/font/alarm clock.ttf", 36 )
 gfx.font.pixel = love.graphics.newFont( "res/font/pixeldart.ttf", 36 )
