50c071313d9d66170d8dded0217df3febadcbda1
Don Miguel <mikhail.bratus@gmail.com>
2016-10-06 14:40:17 +0500

    remove  facing option

--- src/infoBar.lua
+++ src/infoBar.lua
@@ -27,9 +27,9 @@ local MAX_PLAYERS = GLOBAL_SETTING.MAX_PLAYERS
 local max_bar_width = bar_width + icon_width + icon_height/2 + bar_height/2
 
 local bars_coords = {   --for players only 1..MAX_PLAYERS
-    { x = h_m, y = v_m + 0 * v_g, face = 1 },
-    { x = screen_width / 2 - max_bar_width/2 + icon_width/2, y = v_m + 0 * v_g, face = 1 },
-    { x = screen_width - max_bar_width - h_m/3, y = v_m + 0 * v_g, face = 11 }
+    { x = h_m, y = v_m + 0 * v_g },
+    { x = screen_width / 2 - max_bar_width/2 + icon_width/2, y = v_m + 0 * v_g },
+    { x = screen_width - max_bar_width - h_m/3, y = v_m + 0 * v_g }
 }
 
 local function calcBarWidth(self)
@@ -74,7 +74,7 @@ function InfoBar:initialize(source)
         self.max_hp = 20
         self.hp = 1
         self.old_hp = 1
-        self.x, self.y, self.face = 0, 0, 1
+        self.x, self.y = 0, 0
     else --Player / enemy / object
         if source.type == "player" then
             self.score = -1
@@ -87,9 +87,9 @@ function InfoBar:initialize(source)
         self.hp = 1
         self.old_hp = 1
         if self.id <= MAX_PLAYERS then
-            self.x, self.y, self.face = bars_coords[self.id].x, bars_coords[self.id].y, bars_coords[self.id].face
+            self.x, self.y = bars_coords[self.id].x, bars_coords[self.id].y
         else
-            self.x, self.y, self.face = 0, 0, 1
+            self.x, self.y = 0, 0
         end
     end
 end
@@ -103,7 +103,7 @@ function InfoBar:setAttacker(attacker_source)
     end
     self.cool_down = 3
     if id <= MAX_PLAYERS and self.id > MAX_PLAYERS then
-        self.x, self.y, self.face = bars_coords[id].x, bars_coords[id].y + v_g, bars_coords[id].face
+        self.x, self.y = bars_coords[id].x, bars_coords[id].y + v_g
         return self
     end
     return nil
@@ -112,33 +112,30 @@ end
 function InfoBar:setPicker(picker_source)
     local id = picker_source.id
     if id <= MAX_PLAYERS then
-        self.x, self.y, self.face = bars_coords[id].x, bars_coords[id].y + v_g, bars_coords[id].face
+        self.x, self.y = bars_coords[id].x, bars_coords[id].y + v_g
     end
     self.cool_down = 3
     return self
 end
+
 function InfoBar:draw_enemy_bar(l,t,w,h)
     local cool_down_transparency = 1
     if self.source.id > GLOBAL_SETTING.MAX_PLAYERS then
         cool_down_transparency = calcTransparency(self.cool_down)
     end
     transp_bg = 255 * cool_down_transparency
-
     love.graphics.setColor(bar_gray_color[1], bar_gray_color[2], bar_gray_color[3], transp_bg)
     slantedRectangle2( l + self.x + 6, t + self.y + icon_height + 3, calcBarWidth(self) - 8, bar_height )
     love.graphics.setColor(255, 255, 255, transp_bg)
     slantedRectangle2( l + self.x + 5, t + self.y + icon_height + 4, calcBarWidth(self) - 7, bar_height - 2 )
     love.graphics.setColor(lost_color[1], lost_color[2], lost_color[3], transp_bg)
     slantedRectangle2( l + self.x + 5, t + self.y + icon_height + 6, calcBarWidth(self) - 7 , bar_height - 6 )
-
     love.graphics.setColor(255, 255, 255, transp_bg)
-
     love.graphics.draw (
         image_bank[self.icon_sprite],
         self.icon_q, --Current frame of the current animation
         l + self.x + self.source.shake.x, t + self.y
     )
-
     if self.old_hp > 0 then
         if self.source.hp > self.hp then
             love.graphics.setColor(got_color[1], got_color[2], got_color[3], transp_bg)
@@ -164,13 +161,11 @@ function InfoBar:draw_enemy_bar(l,t,w,h)
         gfx.ui.left_slant.q,
         l + self.x - 2, t + self.y + icon_height + 3
     )
-
     love.graphics.draw (
         gfx.ui.right_slant.sprite,
         gfx.ui.right_slant.q,
         l + self.x - 3 - 6 + calcBarWidth(self), t + self.y + icon_height + 3
     )
-
     if self.score ~= self.source.score then
         self.score = self.source.score
         self.displayed_score = string.format("%06d", self.score)
@@ -217,21 +212,10 @@ function InfoBar:draw(l,t,w,h)
     if self.cool_down <= 0 and self.source.id > GLOBAL_SETTING.MAX_PLAYERS then
         return
     end
-    love.graphics.setFont(gfx.font.arcade3)
-    if self.face == 1 then
-        --left side
-        if self.source.type == "item" then
-            self:draw_item_bar(l,t,w,h)
-        else
-            self:draw_enemy_bar(l,t,w,h)
-        end
+    if self.source.type == "item" then
+        self:draw_item_bar(l,t,w,h)
     else
-        --right side
-        if self.source.type == "item" then
-            self:draw_item_bar(l,t,w,h)
-        else
-            self:draw_enemy_bar(l,t,w,h)
-        end
+        self:draw_enemy_bar(l,t,w,h)
     end
 end
 
