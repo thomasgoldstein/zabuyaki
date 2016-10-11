9200c95614f825c3c0608509f064ae7c289e7683
Don Miguel <mikhail.bratus@gmail.com>
2016-10-11 16:31:14 +0500

    update rearranged conditions order in the Zoom / Scroll

--- src/units/level.lua
+++ src/units/level.lua
@@ -11,23 +11,23 @@ function Level:initialize(name)
     self.worldHeight = 800
     self.background = nil
     self.foreground = nil
-    self.scrolling = {commonY = 430, chunksX = {} }
---    self.scrolling.chunks = {
---        {startX = 0, endX = 320, startY = 430, endY = 430},
---        {startX = 321, endX = 321+320, startY = 430, endY = 430-100}
---    }
+    self.scrolling = { commonY = 430, chunksX = {} }
+    --    self.scrolling.chunks = {
+    --        {startX = 0, endX = 320, startY = 430, endY = 430},
+    --        {startX = 321, endX = 321+320, startY = 430, endY = 430-100}
+    --    }
 
     self.world = bump.newWorld(64)
-    self.world:add({type = "wall"}, -20, 0, 40, self.worldHeight) --left
-    self.world:add({type = "wall"}, self.worldWidth - 20, 0, 40, self.worldHeight) --right
-    self.world:add({type = "wall"}, 0, 410, self.worldWidth, 40)  --top
-    self.world:add({type = "wall"}, 0, 546, self.worldWidth, 40) --bottom
+    self.world:add({ type = "wall" }, -20, 0, 40, self.worldHeight) --left
+    self.world:add({ type = "wall" }, self.worldWidth - 20, 0, 40, self.worldHeight) --right
+    self.world:add({ type = "wall" }, 0, 410, self.worldWidth, 40) --top
+    self.world:add({ type = "wall" }, 0, 546, self.worldWidth, 40) --bottom
 
     --adding BLOCKING left-right walls
-    self.left_block_wall = {type = "wall"}
-    self.right_block_wall = {type = "wall" }
+    self.left_block_wall = { type = "wall" }
+    self.right_block_wall = { type = "wall" }
     self.world:add(self.left_block_wall, -10, 0, 40, self.worldHeight) --left
-    self.world:add(self.right_block_wall, self.worldWidth+20, 0, 40, self.worldHeight) --right
+    self.world:add(self.right_block_wall, self.worldWidth + 20, 0, 40, self.worldHeight) --right
 end
 
 function Level:update(dt)
@@ -42,14 +42,14 @@ function Level:update(dt)
         self.foreground:update(dt)
     end
 
-    self:calcScrolling(dt)
+    self:setCamera(dt)
 end
 
 function Level:draw(l, t, w, h)
     if self.background then
         self.background:draw(l, t, w, h)
     end
-    self.objects:draw(l,t,w,h)  -- units
+    self.objects:draw(l, t, w, h) -- units
     if self.foreground then
         self.foreground:draw(l, t, w, h)
     end
@@ -59,12 +59,10 @@ function Level:draw(l, t, w, h)
     love.graphics.rectangle("fill", self.world:getRect(self.right_block_wall))
 end
 
-function Level:calcScrolling(dt)
-    --center camera over all players
-    --local pc = 0
-    --local mx = 0
+function Level:setCamera(dt)
+    -- center camera over all players
     local coord_y = 430 -- const vertical Y (no scroll)
-    local coord_x = 1
+    local coord_x
     local x1, x2, x3
     if player1 then
         x1 = player1.x
@@ -75,8 +73,8 @@ function Level:calcScrolling(dt)
     if player3 then
         x3 = player3.x
     end
-    -- Stage Scale
-    local max_distance = 320+160 - 50
+    -- Camera Zoom
+    local max_distance = 320 + 160 - 50
     local min_distance = 320 - 50
     local min_zoom = 1.5
     local max_zoom = 2
@@ -88,19 +86,6 @@ function Level:calcScrolling(dt)
     local maxx = math.max(x1, x2, x3)
     local dist = maxx - minx
     local scale = max_zoom
-
-    if dist > max_distance - 60 then
-        -- move block walls
-        local actualX, actualY, cols, len = self.world:move(self.left_block_wall, maxx - max_distance - 40, 0, function() return "cross" end)
-        local actualX2, actualY2, cols2, len2 = self.world:move(self.right_block_wall, minx + max_distance +1, 0, function() return "cross" end)
-        --dp(actualX, actualX2, player1.x, player2.x)
-    else
-        -- move block walls
-        local actualX, actualY, cols, len = self.world:move(self.left_block_wall, -100, 0, function() return "cross" end)
-        local actualX2, actualY2, cols2, len2 = self.world:move(self.right_block_wall, 4400, 0, function() return "cross" end)
-        --dp(actualX, actualX2, player1.x, player2.x)
-    end
-
     if dist > min_distance then
         if dist > max_distance then
             scale = min_zoom
@@ -109,18 +94,18 @@ function Level:calcScrolling(dt)
         end
     end
     if mainCamera:getScale() ~= scale then
-        mainCamera:setScale( 2 * math.max(scale, min_zoom) )
+        mainCamera:setScale(2 * math.max(scale, min_zoom))
         if math.max(scale, min_zoom) < max_zoom then
             canvas:setFilter("linear", "linear", 2)
         else
             canvas:setFilter("nearest", "nearest")
         end
     end
-    -- Vertical scroll
-    coord_y = self.scrolling.commonY
+    -- Camera position
     coord_x = (minx + maxx) / 2
+    coord_y = self.scrolling.commonY or coord_y
     local ty, tx, cx = 0, 0, 0
-    for i=1,#self.scrolling.chunks do
+    for i = 1, #self.scrolling.chunks do
         local c = self.scrolling.chunks[i]
         if coord_x >= c.startX and coord_x <= c.endX then
             ty = c.endY - c.startY
@@ -130,7 +115,18 @@ function Level:calcScrolling(dt)
             break
         end
     end
-    mainCamera:update(dt,math.floor(coord_x), math.floor(coord_y))
+    mainCamera:update(dt, math.floor(coord_x), math.floor(coord_y))
+
+    -- Move block walls
+    if dist > max_distance - 60 then
+        local actualX, actualY, cols, len = self.world:move(self.left_block_wall, maxx - max_distance - 40, 0, function() return "cross" end)
+        local actualX2, actualY2, cols2, len2 = self.world:move(self.right_block_wall, minx + max_distance + 1, 0, function() return "cross" end)
+        --dp(actualX, actualX2, player1.x, player2.x)
+    else
+        local actualX, actualY, cols, len = self.world:move(self.left_block_wall, -100, 0, function() return "cross" end)
+        local actualX2, actualY2, cols2, len2 = self.world:move(self.right_block_wall, 4400, 0, function() return "cross" end)
+        --dp(actualX, actualX2, player1.x, player2.x)
+    end
 end
 
 return Level
