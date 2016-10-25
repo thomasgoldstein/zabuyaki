5cabbdf82fef6fc4beb83be71d2e5ffb9f421b4c
Don Miguel <mikhail.bratus@gmail.com>
2016-10-25 21:21:27 +0500

    remove a movie flick effect

--- src/movie.lua
+++ src/movie.lua
@@ -5,26 +5,21 @@ local class = require "lib/middleclass"
 local function r(x) return math.floor(x) end
 
 local screen_gap = 12
-local square_gap = 16
-local square_size = 8
-local square_sx = 0
-local square_speed = 6
 local slide_text_gap = 10
 
 local Movie = class('Movie')
 
---local time = 0
-
 --[[ table = {
-        slide = picture_src,
-           q = picture_quad,
-        text = "some text\nand more",
-        delay = 2
- },
+            {
+                slide = slide1,
+                q = love.graphics.newQuad(120, 130, 240, 80, slide1:getDimensions()),
+                text = "and here GOES other text\nand more of it\n...",
+                delay = 2
+            },
+            }
 --]]
 
 function Movie:initialize(t)
-    --self.tx, self.ty = x, y
     self.type = "movie"
     self.font = gfx.font.arcade3
     self.frame = 1
@@ -39,8 +34,6 @@ function Movie:update(dt)
         dp("Movie is empty")
         return true
     end
-    local f = self.frames[self.frame]
-
     if self.time <= 0 then
         self.frame = self.frame + 1
         if not self.frames[self.frame] then
@@ -48,12 +41,6 @@ function Movie:update(dt)
             return true
         end
         self.time = self.frames[self.frame].delay
-    end
-
-    -- Move perforation
-    square_sx = square_sx + square_speed * dt
-    if square_sx >= square_gap + square_size then
-        square_sx = 0
     end
     -- Movie is in process
     return false
@@ -63,14 +50,6 @@ function Movie:draw(l, t, w, h)
     love.graphics.clear(0, 0, 0, 255)
     -- Flick Perforations
     love.graphics.setColor(255, 255, 255, 255)
-    love.graphics.rectangle("fill", 40, 40, 10, 10)
-    for x = 0, 320 + square_gap + square_size, square_gap + square_size do
-        love.graphics.rectangle("fill", r(l + x - square_sx),
-            r(t + screen_gap), square_size, square_size)
-        love.graphics.rectangle("fill", r(l + x - square_sx),
-            r(t + 240 - screen_gap - square_size), square_size, square_size)
-    end
-
     local f = self.frames[self.frame]
     -- Show Picture
     local _, _, w, h = f.q:getViewport( )
