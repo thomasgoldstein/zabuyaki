a4f133d8ae3fae898f835f58a7e241a670ceaf42
Don Miguel <mikhail.bratus@gmail.com>
2016-11-11 23:05:51 +0500

    fix D193	Make it so objects instantly stop being obstacles when they reach 0 HP, even if they're still visible.

--- src/units/unit.lua
+++ src/units/unit.lua
@@ -290,7 +290,7 @@ function Unit:checkCollisionAndMove(dt)
 	for other, separating_vector in pairs(stage.world:collisions(self.shape)) do
 		local o = other.obj
 		if o.type == "wall"
-		or (o.type == "obstacle" and o.z <= 0)
+		or (o.type == "obstacle" and o.z <= 0 and o.hp > 0)
         then
 			self.shape:move(separating_vector.x, separating_vector.y)
 			--other:move( separating_vector.x/2,  separating_vector.y/2)
