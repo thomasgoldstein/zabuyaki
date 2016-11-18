5c0f5d6e5d3b75244ef650f84a0ad0d306dbe520
Don Miguel <mikhail.bratus@gmail.com>
2016-11-18 22:41:34 +0500

    add copyright / file info on registering

--- src/def/misc/preload_bgm.lua
+++ src/def/misc/preload_bgm.lua
@@ -1,10 +1,17 @@
 local BGM = {}
 
-BGM.intro = "res/bgm/rockdrive.xm"
-BGM.test = "res/bgm/zabutest.xm"
-BGM.title = "res/bgm/theme.xm"
-BGM.level01 = "res/bgm/testtrck.xm"
+BGM.load = function(alias, file, copyright)
+    BGM[alias] = file
+    local _, fileName, _ = string.match(file, "(.-)([^\\/]-%.?([^%.\\/]*))$")
+    BGM[#BGM + 1] = {file = file, alias = alias, fileName = fileName, copyright = copyright or "SubspaceAudio" }
+end
+
 --BGM.level01 = BGM.test
 --BGM.level00 = {"res/bgm/theme.xm","res/bgm/testtrck.xm"}
+
+BGM.load("intro","res/bgm/rockdrive.xm","J.J")
+BGM.load("test","res/bgm/zabutest.xm","Stifu")
+BGM.load("title","res/bgm/theme.xm","Don Miguel")
+BGM.load("level01","res/bgm/testtrck.xm")
 
 return BGM
