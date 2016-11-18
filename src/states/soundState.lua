4ae42f34683f581733d973f4e9648c3dfb3aa48e
Don Miguel <mikhail.bratus@gmail.com>
2016-11-18 21:16:56 +0500

    update play SFX by attack/start buttons

--- src/states/soundState.lua
+++ src/states/soundState.lua
@@ -17,7 +17,41 @@ local item_width_margin = left_item_offset * 2
 local item_height_margin = top_item_offset * 2 - 2
 
 local txt_options_logo = love.graphics.newText( gfx.font.kimberley, "SOUND TEST" )
-local txt_quit = love.graphics.newText( gfx.font.arcade4, "BACK" )
+
+local txt_items = {"SFX N 000", "MUSIC TRACK N 000", "BACK"}
+local txt_hints = {"<, >, ATTACK", "<, >, ATTACK", ""}
+
+local function fillMenu(txt_items, txt_hints)
+    local m = {}
+    local max_item_width, max_item_x = 8, 0
+    for i = 1, #txt_items do
+        local w = gfx.font.arcade4:getWidth(txt_items[i])
+        if w > max_item_width then
+            max_item_x = menu_x_offset + screen_width / 2 - w / 2
+            max_item_width = w
+        end
+    end
+    for i = 1, #txt_items do
+        local w = gfx.font.arcade4:getWidth(txt_items[i])
+        local h = gfx.font.arcade4:getHeight(txt_items[i])
+        local x = menu_x_offset + screen_width / 2 - w / 2
+        local y = menu_y_offset + i * menu_item_h
+
+        m[#m + 1] = {
+            item = txt_items[i],
+            hint = txt_hints[i],
+            x = x,
+            y = y,
+            rect_x = max_item_x,
+            w = max_item_width,
+            h = h,
+            n = 1
+        }
+    end
+    return m
+end
+
+local menu = fillMenu(txt_items, txt_hints)
 
 local menu_state, old_menu_state = 1, 1
 local mouse_x, mouse_y = 0,0
@@ -46,17 +80,40 @@ local function player_input(controls)
         sfx.play("sfx","menu_cancel")
         return Gamestate.pop()
     elseif controls.attack:pressed() or controls.start:pressed() then
+        sfx.play("sfx", menu[menu_state].n)
         return nil
     end
-    if controls.horizontal:pressed(-1) or controls.vertical:pressed(-1) then
+    if controls.horizontal:pressed(-1)then
+        menu[menu_state].n = menu[menu_state].n - 1
+        if menu_state == 1 then
+            if menu[menu_state].n < 1 then
+                menu[menu_state].n = #sfx
+            end
+        elseif menu_state == 2 then
+            if menu[menu_state].n < 1 then
+                menu[menu_state].n = 5  --TODO add Music track names table
+            end
+        end
+    elseif controls.horizontal:pressed(1) then
+        menu[menu_state].n = menu[menu_state].n + 1
+        if menu_state == 1 then
+            if menu[menu_state].n > #sfx then
+                menu[menu_state].n = 1
+            end
+        elseif menu_state == 2 then
+            if menu[menu_state].n > 5 then   --TODO add Music track names table
+                menu[menu_state].n = 1
+            end
+        end
+    elseif controls.vertical:pressed(-1) then
         menu_state = menu_state - 1
-    elseif controls.horizontal:pressed(1) or controls.vertical:pressed(1) then
+    elseif controls.vertical:pressed(1) then
         menu_state = menu_state + 1
     end
     if menu_state < 1 then
-        menu_state = #sfx
+        menu_state = #menu
     end
-    if menu_state > #sfx then
+    if menu_state > #menu then
         menu_state = 1
     end
 end
@@ -73,10 +130,49 @@ end
 function soundState:draw()
     push:apply("start")
     love.graphics.setColor(255, 255, 255, 255)
+    love.graphics.setFont(gfx.font.arcade4)
+    for i = 1,#menu do
+        local m = menu[i]
+        if i == 1 then
+            m.item = "SFX #"..m.n.." "..sfx[m.n].alias
+            m.hint = "by "..sfx[m.n].copyright
+        elseif i == 2 then
+            m.item = "MUSIC #"..m.n
+            m.hint = "by SubspaceAudio"
+        end
+        local w = gfx.font.arcade4:getWidth(m.item)
+        local wb = w + item_width_margin
+        local h = gfx.font.arcade4:getHeight(m.item)
 
+        if i == old_menu_state then
+            love.graphics.setColor(110, 244, 0, 80)
+            love.graphics.rectangle("fill",
+                (screen_width - wb) / 2, m.y - top_item_offset,
+                wb, h + item_height_margin, 4,4,1)
+            love.graphics.setColor(255,200,40, 255)
+            love.graphics.rectangle("line",
+                (screen_width - wb) / 2, m.y - top_item_offset,
+                wb, h + item_height_margin, 4,4,1)
+
+            love.graphics.setColor(255, 255, 255, 255)
+            local w = gfx.font.arcade4:getWidth( m.hint )
+            love.graphics.print(m.hint, (screen_width - w) / 2, screen_height - hint_y_offset)
+
+        end
+        love.graphics.setColor(255, 255, 255, 255)
+        love.graphics.print(m.item, (screen_width - w) / 2, m.y )
+    end
     --header
     love.graphics.setColor(255, 255, 255, 255)
     love.graphics.draw(txt_options_logo, (screen_width - txt_options_logo:getWidth()) / 2, title_y_offset)
     show_debug_indicator()
     push:apply("end")
+end
+
+function soundState:mousepressed( x, y, button, istouch )
+    if not GLOBAL_SETTING.MOUSE_ENABLED then
+        return
+    end
+    sfx.play("sfx","menu_cancel")
+    return Gamestate.pop()
 end
