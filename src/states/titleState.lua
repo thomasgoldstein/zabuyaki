6a02fbf936de0660df269f7d3f6ab6bb30f9cac6
Don Miguel <mikhail.bratus@gmail.com>
2016-11-20 15:53:37 +0500

    tweak menu item select with mouse uses item rect size (was text width)

--- src/states/titleState.lua
+++ src/states/titleState.lua
@@ -163,7 +163,7 @@ function titleState:draw()
         love.graphics.setColor(255, 255, 255, 255)
         love.graphics.draw(m.item, m.x, m.y )
         if GLOBAL_SETTING.MOUSE_ENABLED and
-                CheckPointCollision(mouse_x, mouse_y, m.x - left_item_offset, m.y - top_item_offset, m.w + item_width_margin, m.h + item_height_margin ) then
+                CheckPointCollision(mouse_x, mouse_y, m.rect_x - left_item_offset, m.y - top_item_offset, m.w + item_width_margin, m.h + item_height_margin ) then
             menu_state = i
         end
     end
