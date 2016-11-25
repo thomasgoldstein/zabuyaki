63f99495fd06a73ef5143895c59e75be1b59061b
Thomas Goldstein <stifu@free.fr>
2016-11-25 22:03:35 +0100

    Make all characters more colorful

--- src/def/misc/shaders.lua
+++ src/def/misc/shaders.lua
@@ -162,70 +162,70 @@ return resultCol;
 
 --Players
 local rick_colors_default = {
-    { 181, 81, 23, 255 }, { 122, 54, 15, 255 }, { 56, 27, 28, 255 }, -- orange hoodie
-    { 53, 53, 53, 255 }, { 30, 30, 30, 255 }, { 15, 15, 15, 255 } } -- black pants
+    { 196, 81, 15, 255 }, { 133, 55, 10, 255 }, { 60, 27, 28, 255 }, -- orange hoodie
+    { 55, 55, 55, 255 }, { 31, 31, 31, 255 }, { 15, 15, 15, 255 } } -- black pants
 local rick_colors_2 = {
-    { 188, 188, 188, 255 }, { 130, 130, 130, 255 }, { 73, 73, 73, 255 }, -- white hoodie
-    { 39, 85, 135, 255 }, { 24, 53, 84, 255 }, { 11, 24, 38, 255 } } -- blue pants
+    { 196, 196, 196, 255 }, { 135, 135, 135, 255 }, { 76, 76, 76, 255 }, -- white hoodie
+    { 37, 90, 147, 255 }, { 23, 56, 92, 255 }, { 10, 25, 41, 255 } } -- blue pants
 local rick_colors_3 = {
-    { 86, 135, 97, 255 }, { 47, 91, 63, 255 }, { 24, 53, 35, 255 }, -- green hoodie
-    { 84, 75, 68, 255 }, { 51, 45, 41, 255 }, { 25, 22, 20, 255 } } -- gray pants
+    { 86, 142, 99, 255 }, { 46, 97, 65, 255 }, { 22, 55, 35, 255 }, -- green hoodie
+    { 89, 78, 70, 255 }, { 53, 46, 41, 255 }, { 26, 22, 20, 255 } } -- gray pants
 shaders.rick[2] = swapColors(rick_colors_default, rick_colors_2)
 shaders.rick[3] = swapColors(rick_colors_default, rick_colors_3)
 
 local chai_colors_default = {
-    { 220, 206, 234, 255 }, { 145, 137, 153, 255 }, { 87, 82, 91, 255 }, -- gray bandages
-    { 224, 208, 62, 255 }, { 158, 145, 34, 255 }, { 96, 71, 19, 255 }, -- yellow shirt
-    { 126, 54, 130, 255 }, { 86, 11, 86, 255 }, { 33, 4, 33, 255 }, -- purple shorts
-    { 51, 22, 27, 255 } } -- brown hair
+    { 236, 217, 50, 255 }, { 166, 151, 23, 255 }, { 103, 74, 14, 255 }, -- yellow shirt
+    { 135, 52, 140, 255 }, { 94, 8, 94, 255 }, { 36, 3, 36, 255 }, -- purple shorts
+    { 230, 214, 246, 255 }, { 150, 141, 160, 255 }, { 90, 85, 95, 255 }, -- gray bandages
+    { 55, 21, 27, 255 } } -- brown hair
 local chai_colors_2 = {
-    { 224, 208, 62, 255 }, { 158, 145, 34, 255 }, { 96, 71, 19, 255 }, -- yellow bandages
-    { 193, 207, 244, 255 }, { 125, 142, 167, 255 }, { 65, 73, 86, 255 }, -- light blue shirt
-    { 54, 104, 130, 255 }, { 11, 56, 86, 255 }, { 4, 21, 33, 255 }, -- teal shorts
-    { 34, 29, 57, 255 } } -- purple hair
+    { 199, 216, 255, 255 }, { 128, 148, 176, 255 }, { 67, 76, 91, 255 }, -- light blue shirt
+    { 52, 109, 139, 255 }, { 7, 59, 94, 255 }, { 2, 21, 35, 255 }, -- teal shorts
+    { 236, 217, 50, 255 }, { 166, 151, 23, 255 }, { 103, 74, 14, 255 }, -- yellow bandages
+    { 35, 30, 62, 255 } } -- purple hair
 local chai_colors_3 = {
-    { 226, 113, 113, 255 }, { 193, 44, 44, 255 }, { 112, 19, 19, 255 }, -- red bandages
-    { 206, 196, 185, 255 }, { 154, 136, 119, 255 }, { 92, 72, 55, 255 }, -- light sepia shirt
-    { 53, 53, 53, 255 }, { 30, 30, 30, 255 }, { 15, 15, 15, 255 }, -- black shorts
-    { 51, 35, 22, 255 } } -- sand hair
+    { 217, 205, 192, 255 }, { 161, 140, 121, 255 }, { 97, 74, 54, 255 }, -- light sepia shirt
+    { 53, 53, 53, 255 }, { 31, 31, 31, 255 }, { 15, 15, 15, 255 }, -- black shorts
+    { 244, 114, 114, 255 }, { 213, 41, 41, 255 }, { 123, 16, 16, 255 }, -- red bandages
+    { 54, 35, 20, 255 } } -- sand hair
 shaders.chai[2] = swapColors(chai_colors_default, chai_colors_2)
 shaders.chai[3] = swapColors(chai_colors_default, chai_colors_3)
 
 local kisa_colors_default = {
-    { 69, 145, 134, 255 }, { 45, 96, 92, 255 }, { 21, 45, 43, 255 }, -- teal hat
-    { 133, 62, 65, 255 }, { 89, 39, 42, 255 }, { 41, 14, 16, 255 } } -- maroon shoes
+    { 66, 153, 140, 255 }, { 43, 101, 97, 255 }, { 20, 47, 45, 255 }, -- teal hat
+    { 144, 62, 65, 255 }, { 96, 39, 42, 255 }, { 44, 13, 15, 255 } } -- maroon shoes
 local kisa_colors_2 = {
-    { 76, 145, 55, 255 }, { 49, 91, 34, 255 }, { 19, 45, 24, 255 }, -- green hat
-    { 127, 80, 53, 255 }, { 74, 52, 39, 255 }, { 35, 25, 19, 255 } } -- brown shoes
+    { 75, 154, 51, 255 }, { 49, 97, 32, 255 }, { 18, 48, 24, 255 }, -- green hat
+    { 137, 83, 52, 255 }, { 78, 53, 38, 255 }, { 36, 25, 18, 255 } } -- brown shoes
 local kisa_colors_3 = {
-    { 150, 90, 196, 255 }, { 92, 56, 122, 255 }, { 39, 23, 51, 255 }, -- lavander hat
-    { 173, 100, 17, 255 }, { 99, 59, 9, 255 }, { 45, 26, 4, 255 } } -- orange-brown shoes
+    { 159, 90, 212, 255 }, { 97, 56, 132, 255 }, { 42, 23, 56, 255 }, -- lavander hat
+    { 187, 103, 8, 255 }, { 106, 60, 2, 255 }, { 48, 26, 1, 255 } } -- orange-brown shoes
 shaders.kisa[2] = swapColors(kisa_colors_default, kisa_colors_2)
 shaders.kisa[3] = swapColors(kisa_colors_default, kisa_colors_3)
 
 -- Enemy
 local gopper_colors_default = {
-    { 223, 221, 235, 255 }, { 167, 156, 188, 255 }, { 102, 96, 114, 255 }, -- white top
-    { 51, 63, 105, 255 }, { 31, 41, 76, 255 }, { 19, 25, 40, 255 }, -- blue pants
-    { 105, 108, 112, 255 }, { 81, 83, 86, 255 }, -- top gray stripes
-    { 222, 230, 239, 255 }, { 161, 167, 173, 255 }, -- bottom white stripes
-    { 53, 53, 53, 255 }, { 30, 30, 30, 255 }, { 15, 15, 15, 255 } } -- black shoes
+    { 233, 230, 246, 255 }, { 175, 163, 199, 255 }, { 107, 100, 121, 255 }, -- white top
+    { 51, 65, 113, 255 }, { 31, 42, 83, 255 }, { 19, 26, 43, 255 }, -- blue pants
+    { 110, 113, 118, 255 }, { 84, 86, 90, 255 }, -- top gray stripes
+    { 231, 240, 251, 255 }, { 168, 175, 182, 255 }, -- bottom white stripes
+    { 55, 55, 55, 255 }, { 31, 31, 31, 255 }, { 15, 15, 15, 255 } } -- black shoes
 local gopper_colors_2 = {
-    { 51, 63, 105, 255 }, { 31, 41, 76, 255 }, { 19, 25, 40, 255 }, -- blue top
-    { 51, 63, 105, 255 }, { 31, 41, 76, 255 }, { 19, 25, 40, 255 }, -- blue pants
-    { 222, 230, 239, 255 }, { 161, 167, 173, 255 } } -- top white stripes
+    { 51, 65, 113, 255 }, { 31, 42, 83, 255 }, { 19, 26, 43, 255 }, -- blue top
+    { 51, 65, 113, 255 }, { 31, 42, 83, 255 }, { 19, 26, 43, 255 }, -- blue pants
+    { 231, 240, 251, 255 }, { 168, 175, 182, 255 } } -- top white stripes
 local gopper_colors_3 = {
-    { 56, 84, 57, 255 }, { 35, 53, 36, 255 }, { 20, 30, 20, 255 }, -- green top
-    { 56, 84, 57, 255 }, { 35, 53, 36, 255 }, { 20, 30, 20, 255 }, -- green pants
-    { 222, 230, 239, 255 }, { 161, 167, 173, 255 } } -- top white stripes
+    { 57, 89, 58, 255 }, { 35, 55, 36, 255 }, { 20, 31, 20, 255 }, -- green top
+    { 57, 89, 58, 255 }, { 35, 55, 36, 255 }, { 20, 31, 20, 255 }, -- green pants
+    { 231, 240, 251, 255 }, { 168, 175, 182, 255 } } -- top white stripes
 local gopper_colors_4 = {
-    { 53, 53, 53, 255 }, { 30, 30, 30, 255 }, { 15, 15, 15, 255 }, -- black top
-    { 53, 53, 53, 255 }, { 30, 30, 30, 255 }, { 15, 15, 15, 255 }, -- black pants
-    { 222, 230, 239, 255 }, { 161, 167, 173, 255 } } -- top white stripes
+    { 55, 55, 55, 255 }, { 31, 31, 31, 255 }, { 15, 15, 15, 255 }, -- black top
+    { 55, 55, 55, 255 }, { 31, 31, 31, 255 }, { 15, 15, 15, 255 }, -- black pants
+    { 231, 240, 251, 255 }, { 168, 175, 182, 255 } } -- top white stripes
 local gopper_colors_5 = {
-    { 112, 48, 61, 255 }, { 73, 31, 40, 255 }, { 40, 17, 22, 255 }, -- red top
-    { 112, 48, 61, 255 }, { 73, 31, 40, 255 }, { 40, 17, 22, 255 }, -- red pants
-    { 222, 230, 239, 255 }, { 161, 167, 173, 255 } } -- top white stripes
+    { 122, 48, 63, 255 }, { 78, 30, 40, 255 }, { 42, 16, 22, 255 }, -- red top
+    { 122, 48, 63, 255 }, { 78, 30, 40, 255 }, { 42, 16, 22, 255 }, -- red pants
+    { 231, 240, 251, 255 }, { 168, 175, 182, 255 } } -- top white stripes
 shaders.gopper[2] = swapColors(gopper_colors_default, gopper_colors_2)
 shaders.gopper[3] = swapColors(gopper_colors_default, gopper_colors_3)
 shaders.gopper[4] = swapColors(gopper_colors_default, gopper_colors_4)
@@ -234,34 +234,34 @@ shaders.gopper[5] = swapColors(gopper_colors_default, gopper_colors_5)
 local niko_colors_default = gopper_colors_default
 local niko_colors_2 = gopper_colors_2
 local niko_colors_3 = {
-    { 51, 63, 105, 255 }, { 31, 41, 76, 255 }, { 19, 25, 40, 255 }, -- blue top
-    { 51, 63, 105, 255 }, { 31, 41, 76, 255 }, { 19, 25, 40, 255 }, -- blue pants
-    { 30, 30, 30, 255 }, { 15, 15, 15, 255 }, -- top black stripes
-    { 30, 30, 30, 255 }, { 15, 15, 15, 255 }, -- bottom black stripes
-    { 198, 198, 198, 255 }, { 137, 137, 137, 255 }, { 84, 84, 84, 255 } } -- white shoes
+    { 51, 65, 113, 255 }, { 31, 42, 83, 255 }, { 19, 26, 43, 255 }, -- blue top
+    { 51, 65, 113, 255 }, { 31, 42, 83, 255 }, { 19, 26, 43, 255 }, -- blue pants
+    { 31, 31, 31, 255 }, { 15, 15, 15, 255 }, -- top black stripes
+    { 31, 31, 31, 255 }, { 15, 15, 15, 255 }, -- bottom black stripes
+    { 207, 207, 207, 255 }, { 142, 142, 142, 255 }, { 87, 87, 87, 255 } } -- white shoes
 shaders.niko[2] = swapColors(niko_colors_default, niko_colors_2)
 shaders.niko[3] = swapColors(niko_colors_default, niko_colors_3)
 
 local satoff_colors_default = {
-    { 181, 47, 51, 255 }, { 119, 31, 34, 255 }, { 53, 20, 21, 255 }, -- red suit
-    { 95, 40, 45, 255 }, { 55, 23, 28, 255 }, { 38, 16, 19, 255 }, -- maroon shoes
-    { 51, 32, 29, 255 }, { 33, 20, 18, 255 }, { 22, 14, 12, 255 }, -- brown pants
-    { 172, 129, 113, 255 }} -- face scar
+    { 199, 45, 50, 255 }, { 130, 29, 33, 255 }, { 58, 20, 21, 255 }, -- red suit
+    { 102, 39, 45, 255 }, { 59, 22, 28, 255 }, { 41, 16, 19, 255 }, -- maroon shoes
+    { 54, 32, 29, 255 }, { 35, 20, 17, 255 }, { 23, 13, 11, 255 }, -- brown pants
+    { 181, 132, 113, 255 }} -- face scar
 local satoff_colors_2 = {
-    { 62, 97, 145, 255 }, { 39, 61, 91, 255 }, { 19, 30, 45, 255 }, -- blue suit
-    { 49, 53, 94, 255 }, { 29, 31, 56, 255 }, { 17, 18, 33, 255 }, -- midnight blue shoes
-    { 37, 37, 48, 255 }, { 23, 23, 30, 255 }, { 16, 16, 21, 255 }, -- cool gray pants
-    { 226, 173, 158, 255 }} -- no face scar
+    { 61, 101, 156, 255 }, { 39, 64, 98, 255 }, { 19, 31, 48, 255 }, -- blue suit
+    { 50, 55, 102, 255 }, { 29, 31, 60, 255 }, { 17, 18, 35, 255 }, -- midnight blue shoes
+    { 38, 38, 50, 255 }, { 23, 23, 31, 255 }, { 16, 16, 22, 255 }, -- cool gray pants
+    { 239, 179, 161, 255 }} -- no face scar
 local satoff_colors_3 = {
-    { 238, 227, 224, 255 }, { 173, 159, 150, 255 }, { 96, 88, 83, 255 }, -- white suit
-    { 67, 55, 52, 255 }, { 39, 38, 35, 255 }, { 25, 21, 20, 255 }, -- taupe shoes
-    { 111, 48, 119, 255 }, { 71, 31, 76, 255 }, { 43, 22, 45, 255 }, -- purple pants
-    { 226, 173, 158, 255 }} -- no face scar
+    { 250, 237, 234, 255 }, { 181, 165, 155, 255 }, { 101, 92, 86, 255 }, -- white suit
+    { 70, 56, 53, 255 }, { 41, 40, 36, 255 }, { 25, 20, 19, 255 }, -- taupe shoes
+    { 120, 48, 129, 255 }, { 76, 30, 82, 255 }, { 46, 22, 49, 255 }, -- purple pants
+    { 239, 179, 161, 255 }} -- no face scar
 local satoff_colors_4 = {
-    { 44, 44, 36, 255 }, { 29, 25, 22, 255 }, { 24, 17, 11, 255 }, -- black suit
-    { 44, 44, 36, 255 }, { 29, 25, 22, 255 }, { 24, 17, 11, 255 }, -- black shoes
-    { 160, 32, 62, 255 }, { 104, 20, 40, 255 }, { 56, 14, 24, 255 }, -- bordeaux pants
-    { 226, 173, 158, 255 }} -- no face scar
+    { 45, 45, 36, 255 }, { 30, 25, 22, 255 }, { 25, 17, 10, 255 }, -- black suit
+    { 45, 45, 36, 255 }, { 30, 25, 22, 255 }, { 25, 17, 10, 255 }, -- black shoes
+    { 176, 29, 64, 255 }, { 114, 18, 41, 255 }, { 62, 13, 25, 255 }, -- bordeaux pants
+    { 239, 179, 161, 255 }} -- no face scar
 shaders.satoff[2] = swapColors(satoff_colors_default, satoff_colors_2)
 shaders.satoff[3] = swapColors(satoff_colors_default, satoff_colors_3)
 shaders.satoff[4] = swapColors(satoff_colors_default, satoff_colors_4)
