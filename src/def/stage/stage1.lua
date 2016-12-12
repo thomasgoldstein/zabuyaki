ed929b31f755e854344870c25c561bc64269777d
Don Miguel <mikhail.bratus@gmail.com>
2016-12-12 16:00:43 +0500

    add the 2nd horiz bottom part of the road

--- src/def/stage/stage1.lua
+++ src/def/stage/stage1.lua
@@ -111,7 +111,9 @@ function Stage1:initialize(players)
     local wall3a = Wall:new("wall3a", { shapeType = "rectangle", shapeArgs = { 1800, 180, 1800, 100 }}) --top
     local ppx, ppy = 1800, 546
     local walld1 = Wall:new("walld1", { shapeType = "polygon", shapeArgs ={ ppx + 0, ppy + 0, ppx + 370, ppy - 190, ppx + 370, ppy + 100, ppx + 0, ppy + 100 }}) --polygon
-    local wall4 = Wall:new("wall4", { shapeType = "rectangle", shapeArgs = { 0, 546, self.worldWidth, 100 }}) --bottom
+    local wall4 = Wall:new("wall4", { shapeType = "rectangle", shapeArgs = { 0, 546, 1800
+        , 100 }}) --bottom
+    local wall4a = Wall:new("wall4a", { shapeType = "rectangle", shapeArgs = { 1800 + 370, 356, 1800, 100 }}) --bottom
 
 --[[    local wall5 = Wall:new("wall5", { shapeType = "circle", shapeArgs = { 27, 560, 40 }}) --test circle
     local wall6 = Wall:new("wall6", { shapeType = "rectangle", shapeArgs = { 90, 526, 60, 10, rotate = -0.3 }}) --rotated rectangle
@@ -281,7 +283,7 @@ function Stage1:initialize(players)
         dummy4, dummy5,
         loot1, loot2, loot3,
         can1, can2, can3, can4, no_entry_sign1,no_entry_sign2,
-        wall1,wall2,wall3,wall3a,walld1,wall4 --,wall5,wall6,wall7
+        wall1,wall2,wall3,wall3a,walld1,wall4,wall4a --,wall5,wall6,wall7
     })
 
     local a, sx  = {}, 0
