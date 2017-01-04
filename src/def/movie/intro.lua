2480aca6fe30ca496ee34b33093c1187daaefe54
Thomas Goldstein <stifu@free.fr>
2017-01-04 16:27:00 +0100

    Add new intro text

--- src/def/movie/intro.lua
+++ src/def/movie/intro.lua
@@ -5,27 +5,63 @@ local slide1 = love.graphics.newImage("res/img/stage/stage1/building1.png")
 movie_intro = {
     {
         slide = slide1, -- Picture
-        q = { 60, 70, 200, 120 }, -- Initial quad
-        text =
-        [[This city has one rule:
-fight for everything...]],
+        q = { 60, 70, 200, 120 }, -- Initial quad,
         hScroll = -10,
-        delay = 4 -- How long to type th text
+        text =
+[[Abibas City was once a nice
+place, but now it's a shithole.]]
     },
     {
         slide = slide1,
         q = { 160, 170, 240, 80 },
         vScroll = 20,
         text =
-        [[And if you have nothing
-then fight for your life...]],
-        delay = 4
+[[Crime runs rampant, and the
+streets are no longer safe.]]
     },
     {
         slide = slide1,
         q = { 230, 290, 220, 100 },
-        text = "And the last.\nDon't let down your friends!",
-        delay = 4
+        hScroll = -10,
+        text =
+[[The mayor, Hyke Magger, failed
+to clean up the city.          
+Literally.]]
+    },
+    {
+        slide = slide1,
+        q = { 160, 170, 240, 80 },
+        vScroll = 20,
+        text =
+[[Corruption suspicions grow
+toward mayor Magger, accused
+of ties to the local mafia.]]
+    },
+    {
+        slide = slide1,
+        q = { 60, 70, 200, 120 },
+        hScroll = -10,
+        text =
+[[The public waste collection
+service has even been replaced
+by a shady private company.]]
+    },
+    {
+        slide = slide1,
+        q = { 230, 290, 220, 100 },
+        vScroll = 20,
+        text =
+[[After having lost their job,
+the ex-garbage collectors saw
+their city turn into a landfill.]]
+    },
+    {
+        slide = slide1,
+        q = { 60, 70, 200, 120 },
+        text =
+[[Some of them decided
+to stand up and take out
+the trash once and for all.]]
     },
     bgColor = { 0, 0, 0 },
     autoSkip = true,
