ca635a5578dd415d4916c2d2489ba12f3cb8e1ed
Thomas Goldstein <stifu@free.fr>
2017-02-27 22:33:57 +0100

    Add Satoff's standHold and walkHold animations

--- src/def/char/satoff.lua
+++ src/def/char/satoff.lua
@@ -65,44 +65,38 @@ return {
             delay = 0.15
         },
         standHold = {
-            { q = q(2,2,68,68), ox = 34, oy = 67,
-                wx = -10, wy = -33, wRotate = -0.5, wAnimation = 'angle0_equipped' }, --stand 1
-            { q = q(72,3,68,67), ox = 34, oy = 66,
-                wx = -10, wy = -32, wRotate = -0.45, wAnimation = 'angle0_equipped' }, --stand 2
-            { q = q(142,4,67,66), ox = 34, oy = 65,
-                wx = -10, wy = -31, wRotate = -0.4, wAnimation = 'angle0_equipped' }, --stand 3
-            { q = q(72,3,68,67), ox = 34, oy = 66,
-                wx = -10, wy = -32, wRotate = -0.55, wAnimation = 'angle0_equipped' }, --stand 2
+            { q = q(2,598,66,66), ox = 31, oy = 65 }, --standHold 1
+            { q = q(70,598,66,66), ox = 31, oy = 65 }, --standHold 2
+            { q = q(138,599,66,65), ox = 31, oy = 64 }, --standHold 3
+            { q = q(70,598,66,66), ox = 31, oy = 65 }, --standHold 2
             loop = true,
             delay = 0.15
         },
         walk = {
             { q = q(2,72,74,68), ox = 34, oy = 67,
-                wx = -6, wy = -33, wRotate = 0, wAnimation = 'angle0_equipped' },
+                wx = -6, wy = -33, wRotate = 0, wAnimation = 'angle0_equipped' }, --walk 1
             { q = q(78,72,73,68), ox = 34, oy = 67,
-                wx = -9, wy = -32, wRotate = 0, wAnimation = 'angle0_equipped' },
+                wx = -9, wy = -32, wRotate = 0, wAnimation = 'angle0_equipped' }, --walk 2
             { q = q(153,73,71,67), ox = 34, oy = 66,
-                wx = -10, wy = -30, wRotate = 0, wAnimation = 'angle0_equipped' },
+                wx = -10, wy = -30, wRotate = 0, wAnimation = 'angle0_equipped' }, --walk 3
             { q = q(226,72,73,68), ox = 34, oy = 67,
-                wx = -9, wy = -32, wRotate = 0, wAnimation = 'angle0_equipped' },
+                wx = -9, wy = -32, wRotate = 0, wAnimation = 'angle0_equipped' }, --walk 4
             loop = true,
             delay = 0.15
         },
         walkHold = {
-            { q = q(2,72,74,68), ox = 34, oy = 67,
-                wx = -6, wy = -33, wRotate = 0, wAnimation = 'angle0_equipped' },
-            { q = q(78,72,73,68), ox = 34, oy = 67,
-                wx = -9, wy = -32, wRotate = 0, wAnimation = 'angle0_equipped' },
-            { q = q(153,73,71,67), ox = 34, oy = 66,
-                wx = -10, wy = -30, wRotate = 0, wAnimation = 'angle0_equipped' },
-            { q = q(226,72,73,68), ox = 34, oy = 67,
-                wx = -9, wy = -32, wRotate = 0, wAnimation = 'angle0_equipped' },
+            { q = q(2,666,66,66), ox = 31, oy = 65 }, -- walkHold 1
+            { q = q(70,666,66,66), ox = 31, oy = 65 }, -- walkHold 2
+            { q = q(138,667,66,65), ox = 31, oy = 64 }, -- walkHold 3
+            { q = q(70,666,66,66), ox = 31, oy = 65 }, -- walkHold 2
             loop = true,
             delay = 0.15
         },
         run = {
-            { q = q(2,2,68,68), ox = 34, oy = 67 }, --stand 1
+            { q = q(59,352,60,74), ox = 32, oy = 73 }, --jaf1
+            { q = q(121,359,58,58), ox = 29, oy = 57, funcCont = jump_forward_attack }, --jaf2
             loop = true,
+			loopFrom = 2,
             delay = 0.117
         },
         jump = {
