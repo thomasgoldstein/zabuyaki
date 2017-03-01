13457180678a6fcfd8720aae94700ef9517332bb
Don Miguel <mikhail.bratus@gmail.com>
2017-03-02 00:50:19 +0500

    fix D286 alter drawTextInfo methods to show "life bars left" counter for players and enemy

--- src/unit/character.lua
+++ src/unit/character.lua
@@ -106,12 +106,25 @@ function Character:drawFaceIcon(l, t)
     )
 end
 
-local calcBarTransparency = calcBarTransparency
 local printWithShadow = printWithShadow
-function Character:drawTextInfo(l, t, transp_bg, icon_width)
+local calcBarTransparency = calcBarTransparency
+function Character:drawTextInfo(l, t, transp_bg, icon_width, norm_color)
     love.graphics.setColor(255, 255, 255, transp_bg)
     printWithShadow(self.name, l + self.shake.x + icon_width + 2, t + 9,
         transp_bg)
+    if self.lives >= 1 then
+        love.graphics.setColor(255, 255, 255, transp_bg)
+        printWithShadow("x", l + self.shake.x + icon_width + 91, t + 9,
+            transp_bg)
+        love.graphics.setFont(gfx.font.arcade3x2)
+        if self.lives > 10 then
+            printWithShadow("9+", l + self.shake.x + icon_width + 100, t + 1,
+                transp_bg)
+        else
+            printWithShadow(self.lives - 1, l + self.shake.x + icon_width + 100, t + 1,
+                transp_bg)
+        end
+    end
 end
 
 function Character:drawBar(l,t,w,h, icon_width, norm_color)
