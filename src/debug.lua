244db87de4bacab03febd6d4165f730f36f70d73
Don Miguel <mikhail.bratus@gmail.com>
2017-03-09 21:51:50 +0500

    fix crash at the debug mode when selected P1 and P3 (P2 is absent)

--- src/debug.lua
+++ src/debug.lua
@@ -68,42 +68,44 @@ function show_debug_controls()
         local players = { player1, player2, player3 }
         for i = 1, #players do
             local p = players[i]
-            local x = p.infoBar.x + 76
-            local y = p.infoBar.y + 36
-            love.graphics.setColor(0, 0, 0, 150)
-            love.graphics.rectangle("fill", x - 2, y, 61, 9)
-            love.graphics.setColor( unpack( GLOBAL_SETTING.PLAYERS_COLORS[p.id] ) )
-            if p.b.attack:isDown() then
-                love.graphics.print("F", x, y)
-            end
-            x = x + 10
-            if p.b.jump:isDown() then
-                love.graphics.print("J", x, y)
-            end
-            x = x + 10
-            if p.b.horizontal:isDown(-1) then
-                love.graphics.print("<", x, y)
-            end
-            x = x + 10
-            if p.b.horizontal:isDown(1) then
-                love.graphics.print(">", x, y)
-            end
-            x = x + 10
-            if p.b.vertical:isDown(-1) then
-                love.graphics.print("A", x, y)
-            end
-            x = x + 10
-            if p.b.vertical:isDown(1) then
-                love.graphics.print("V", x, y)
-            end
-            x = p.infoBar.x + 76
-            y = y - 12
-            if p.can_attack then
-                love.graphics.print("F", x, y)
-            end
-            x = x + 10
-            if p.can_jump then
-                love.graphics.print("J", x, y)
+            if p then
+                local x = p.infoBar.x + 76
+                local y = p.infoBar.y + 36
+                love.graphics.setColor(0, 0, 0, 150)
+                love.graphics.rectangle("fill", x - 2, y, 61, 9)
+                love.graphics.setColor( unpack( GLOBAL_SETTING.PLAYERS_COLORS[p.id] ) )
+                if p.b.attack:isDown() then
+                    love.graphics.print("F", x, y)
+                end
+                x = x + 10
+                if p.b.jump:isDown() then
+                    love.graphics.print("J", x, y)
+                end
+                x = x + 10
+                if p.b.horizontal:isDown(-1) then
+                    love.graphics.print("<", x, y)
+                end
+                x = x + 10
+                if p.b.horizontal:isDown(1) then
+                    love.graphics.print(">", x, y)
+                end
+                x = x + 10
+                if p.b.vertical:isDown(-1) then
+                    love.graphics.print("A", x, y)
+                end
+                x = x + 10
+                if p.b.vertical:isDown(1) then
+                    love.graphics.print("V", x, y)
+                end
+                x = p.infoBar.x + 76
+                y = y - 12
+                if p.can_attack then
+                    love.graphics.print("F", x, y)
+                end
+                x = x + 10
+                if p.can_jump then
+                    love.graphics.print("J", x, y)
+                end
             end
         end
     end
