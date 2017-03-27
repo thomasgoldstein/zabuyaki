431b315b658b9d16cae619f7dfd8b54c7c1873c2
Don Miguel <mikhail.bratus@gmail.com>
2017-03-27 17:05:31 +0500

    tweak show nearby bath areas with 3 different colors, added some batch info

--- src/canvas2png.lua
+++ src/canvas2png.lua
@@ -1,4 +1,5 @@
 -- Debug graphics output
+local r = round
 
 function Unit:draw2()
     self.sprite.flip_h = self.face  --TODO get rid of .face
@@ -7,7 +8,7 @@ function Unit:draw2()
 --        love.graphics.setShader(self.shader)
 --    end
     if GLOBAL_SETTING.DEBUG then
-        love.graphics.setColor(255, 255, 255, 50)
+        love.graphics.setColor(255, 255, 255, 90)
     else
         love.graphics.setColor(255, 255, 255, 255)
     end
@@ -23,13 +24,34 @@ function Unit:draw2()
     draw_debug_unit_info(self)
 end
 
+local batch_colors = {
+    {255, 0, 0, 125},
+    {0, 255, 0, 125},
+    {0, 0, 255, 125 }
+}
 function Batch:draw()
+    local curr_b_color = 0
     --local b = self.batches[self.n]
     for n = 1, #self.batches do
         local b = self.batches[n]
+
+        curr_b_color = curr_b_color + 1
+        if curr_b_color > #batch_colors then curr_b_color = 1 end
+        love.graphics.setColor(unpack(batch_colors[curr_b_color]))
+--        self.left_stopper = b.left_stopper or 0
+--        self.right_stopper = b.right_stopper or 320
+        local y = (curr_b_color - 1 ) * 4
+        love.graphics.rectangle( "line", b.left_stopper, y, b.right_stopper - b.left_stopper, stage.background.height - y )
+        y = y + 4
+        love.graphics.print( "Batch N "..n.." L:"..b.left_stopper.." R:"..b.right_stopper.." Delay:"..b.delay, b.left_stopper + 4, y )
+        y = y + 10
+
         for i = 1, #b.units do
             local u = b.units[i]
-            print(n, i, u.unit.name)
+            love.graphics.setColor(unpack(batch_colors[curr_b_color]))
+            love.graphics.print( i.." "..(u.state or "n/a").."->"..u.unit.name, b.left_stopper + 4, y )
+            love.graphics.print( " "..r(u.unit.x, 0) ..","..r(u.unit.y, 0), b.left_stopper + 4, y + 9 )
+            y = y + 20
             --not u.isSpawned then
             --self.stage.objects:add(u.unit)
             u.unit:draw2()
