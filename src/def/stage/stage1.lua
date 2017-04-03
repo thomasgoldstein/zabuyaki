a458d574982dcb1d04fd92eb4236252fcc7b6b5a
D0NM <hyuBYSnONMVWgWDpCeV4>
2017-04-04 00:19:08 +0500

    tweak D328 final tweak the diagonal road walkable area to follow the same positioning as the straight one

--- src/def/stage/stage1.lua
+++ src/def/stage/stage1.lua
@@ -71,14 +71,14 @@ function Stage1:initialize(players)
     -- Walls around the level
     local wall1 = Wall:new("wall1", { shapeType = "rectangle", shapeArgs = { -80, 0, 40, self.worldHeight }}) --left
     local wall2 = Wall:new("wall2", { shapeType = "rectangle", shapeArgs = { self.worldWidth - 20, 0, 40, self.worldHeight }}) --right
-    local wall3 = Wall:new("wall3", { shapeType = "rectangle", shapeArgs = { 0, 360, 1835, 100 }}) --top road 1
-    local ppx, ppy = 1835, 546 - 86
-    local walld2 = Wall:new("walld2", { shapeType = "polygon", shapeArgs ={ ppx + 0, ppy + 0, ppx + 370, ppy - 190, ppx + 370, ppy - 290, ppx + 0, ppy - 290 }}) --top road diagonal 1
-    local wall3a = Wall:new("wall3a", { shapeType = "rectangle", shapeArgs = { 2205, 170, 1800, 100 }}) --top road 2
+    local wall3 = Wall:new("wall3", { shapeType = "rectangle", shapeArgs = { 0, 360, 1825, 100 }}) --top road 1
+    local ppx, ppy = 1825, 460
+    local walld2 = Wall:new("walld2", { shapeType = "polygon", shapeArgs ={ ppx + 0, ppy + 0, ppx + 360 , ppy - 190 + 11, ppx + 360, ppy - 290, ppx + 0, ppy - 290 }}) --top road diagonal 1
+    local wall3a = Wall:new("wall3a", { shapeType = "rectangle", shapeArgs = { 2185, 181, 1800, 100 }}) --top road 2
     local wall4 = Wall:new("wall4", { shapeType = "rectangle", shapeArgs = { 0, 546, 1835, 100 }}) --bottom road 1
     local ppx, ppy = 1835, 546
-    local walld1 = Wall:new("walld1", { shapeType = "polygon", shapeArgs ={ ppx + 0, ppy + 0, ppx + 370, ppy - 190 + 12, ppx + 370, ppy + 100, ppx + 0, ppy + 100 }}) --bottom road diagonal 1
-    local wall4a = Wall:new("wall4a", { shapeType = "rectangle", shapeArgs = { 1835 + 370, 356 + 12, 1800, 100 }}) --bottom road 2
+    local walld1 = Wall:new("walld1", { shapeType = "polygon", shapeArgs ={ ppx + 0, ppy + 0, ppx + 370, ppy - 178, ppx + 370, ppy + 100, ppx + 0, ppy + 100 }}) --bottom road diagonal 1
+    local wall4a = Wall:new("wall4a", { shapeType = "rectangle", shapeArgs = { 1835 + 370, 368, 1800, 100 }}) --bottom road 2
 
 --[[    local wall5 = Wall:new("wall5", { shapeType = "circle", shapeArgs = { 27, 560, 40 }}) --test circle
     local wall6 = Wall:new("wall6", { shapeType = "rectangle", shapeArgs = { 90, 526, 60, 10, rotate = -0.3 }}) --rotated rectangle
