b34755f17f6aae19ed8c412297db24778eb6b3d6
Don Miguel <mikhail.bratus@gmail.com>
2017-04-04 16:49:13 +0500

    new loading background images from the Tiled file

--- src/loadStage.lua
+++ src/loadStage.lua
@@ -1,10 +1,8 @@
 -- Load and correct objects from various Tiled export files
-
---stage1_data.lua
+--to load stage1_data.lua
 
 local function extractTable(tab, val)
-    for index, value in ipairs(tab) do
-        -- We grab the first index of our sub-table instead
+    for i, value in ipairs(tab) do
         if value and value.name == val then
             return value
         end
@@ -12,11 +10,8 @@ local function extractTable(tab, val)
     return nil
 end
 
-local
-
-function loadCollision(items)
+local function loadCollision(items)
     print("Load collisions...")
---        "collision"
     local t = extractTable(items.layers, "collision")
     for i, v in ipairs(t.objects) do
         if v.type == "wall" then
@@ -40,9 +35,32 @@ function loadCollision(items)
     end
 end
 
-function loadStageData(file)
+local loaded_images = {}
+local loaded_images_quads = {}
+local function cacheImage(path_to_image)
+    if not loaded_images[path_to_image] then
+        loaded_images[path_to_image] = love.graphics.newImage(path_to_image)
+        local width, height = loaded_images[path_to_image]:getDimensions()
+        loaded_images_quads[path_to_image] = love.graphics.newQuad(2, 2, width - 4, height - 4, width, height)
+    end
+    return loaded_images[path_to_image], loaded_images_quads[path_to_image]
+end
+
+local function loadImageLayer(items, background)
+    print("Load ImageLayer...")
+--    local t = extractTable(items.layers, "collision")
+    for i, v in ipairs(items.layers) do
+        if v.type == "imagelayer" then
+            if v.visible then
+                local image, quad = cacheImage(v.image)
+                background:add(image, quad, v.offsetx, v.offsety)
+            end
+        end
+    end
+end
+
+function loadStageData(file, background)
     local d = dofile(file)
-    --print(d)
-    --    print(inspect(d.layers))
     loadCollision(d)
+    loadImageLayer(d, background)
 end
