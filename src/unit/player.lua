8fb9d0179175ea0a6e7b9fcda2b69ae5cb4dbf9f
Don Miguel <mikhail.bratus@gmail.com>
2017-05-02 15:39:52 +0500

    remove drawShadow method which coinsides with Unit's

--- src/unit/player.lua
+++ src/unit/player.lua
@@ -59,29 +59,6 @@ function Player:setState(state, condition)
     end
 end
 
-function Player:drawShadow(l,t,w,h)
-    if not self.isDisabled and CheckCollision(l, t, w, h, self.x-45, self.y-10, 90, 20) then
-        if self.cool_down_death < 2 then
-            love.graphics.setColor(0, 0, 0, 255 * math.sin(self.cool_down_death)) --4th is the shadow transparency
-        else
-            love.graphics.setColor(0, 0, 0, 255) --4th is the shadow transparency
-        end
-        local spr = self.sprite
-        local sc = spr.def.animations[spr.cur_anim][spr.cur_frame]
-        local shadowAngle = -stage.shadowAngle * spr.flip_h
-        love.graphics.draw (
-            image_bank[spr.def.sprite_sheet], --The image
-            sc.q, --Current frame of the current animation
-            self.x + self.shake.x, self.y - 2 + self.z/6,
-            0,
-            spr.flip_h,
-            -stage.shadowHeight,
-            sc.ox, sc.oy,
-            shadowAngle
-        )
-    end
-end
-
 -- Start of Lifebar elements
 local printWithShadow = printWithShadow
 local calcBarTransparency = calcBarTransparency
