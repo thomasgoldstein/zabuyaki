8ccb53a02a4a41c477f8723b0c5b760ead041ec4
Thomas Goldstein <stifu@free.fr>
2017-05-17 22:10:04 +0200

    Consistency reformatting

--- src/def/misc/particles.lua
+++ src/def/misc/particles.lua
@@ -27,18 +27,18 @@ local loot_colors = {255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 55, 
 
 quads ={ triangle_small_quad = triangle_small_quad, triangle_big_quad = triangle_big_quad}
 
-psystem = love.graphics.newParticleSystem( gfx.particles, 32 )
-psystem:setPosition( 0, -2 )
+psystem = love.graphics.newParticleSystem(gfx.particles, 32)
+psystem:setPosition(0, -2)
 psystem:setEmitterLifetime(0.6)
 psystem:setParticleLifetime(0.35, 0.5) 
 psystem:setSizes(0.2, 0.7)
-psystem:setSpeed( 1, 5 )
+psystem:setSpeed(1, 5)
 psystem:setLinearAcceleration(0, 0, 0, 0) -- Random movement in all directions.
 psystem:setColors(unpack(dust_step_colors))
-psystem:setOffset( 15, 15 )
-psystem:setQuads( dust_quad )
-psystem:setLinearDamping( 7, 20 )
-psystem:setAreaSpread( "uniform", 8, 4 )
+psystem:setOffset(15, 15)
+psystem:setQuads(dust_quad)
+psystem:setLinearDamping(7, 20)
+psystem:setAreaSpread("uniform", 8, 4)
 psystem:setSpin(0, -3)
 PA_DUST_STEPS = psystem
 
@@ -50,7 +50,7 @@ psystem = PA_DUST_STEPS:clone()
 psystem:setEmitterLifetime(1)
 psystem:setParticleLifetime(0.5, 0.95) 
 psystem:setSizes(0.15, 0.53)
-psystem:setPosition( 0, 0 )
+psystem:setPosition(0, 0)
 PA_DUST_JUMP_START = psystem
 
 psystem = psystem:clone()
@@ -59,9 +59,9 @@ psystem:setSizes(0.15, 0.45)
 psystem:setColors(unpack(dust_step_colors))
 psystem:setParticleLifetime(0.5, 1.3) 
 psystem:setLinearAcceleration(-500, -20, 500, -100) -- Random movement in all directions.
-psystem:setLinearDamping( 10, 50 )
-psystem:setAreaSpread( "uniform", 30, 4 )
-psystem:setPosition( 0, -2 )
+psystem:setLinearDamping(10, 50)
+psystem:setAreaSpread("uniform", 30, 4)
+psystem:setPosition(0, -2)
 PA_DUST_FALLING = psystem
 
 psystem = psystem:clone()
@@ -70,61 +70,61 @@ psystem:setSizes(0.3, 0.6, 0.4, 0.1)
 psystem:setColors(unpack(dust_step_colors))
 psystem:setParticleLifetime(0.2, 0.7) 
 psystem:setLinearAcceleration(-400, -20, 400, -100) -- Random movement in all directions.
-psystem:setLinearDamping( 7, 20 )
-psystem:setAreaSpread( "uniform", 15, 5 )
-psystem:setPosition( 0, -4 )
+psystem:setLinearDamping(7, 20)
+psystem:setAreaSpread("uniform", 15, 5)
+psystem:setPosition(0, -4)
 PA_DUST_LANDING_UNUSED = psystem
 
-psystem = love.graphics.newParticleSystem( gfx.particles, 4 )
-psystem:setOffset( 10, 11 )
+psystem = love.graphics.newParticleSystem(gfx.particles, 4)
+psystem:setOffset(10, 11)
 psystem:setEmitterLifetime(0.2)
 psystem:setParticleLifetime(0.15)
 psystem:setColors(unpack(impact_colors))
-psystem:setQuads( imp_small_quad1, imp_small_quad2, imp_small_quad3 )
+psystem:setQuads(imp_small_quad1, imp_small_quad2, imp_small_quad3)
 PA_IMPACT_SMALL = psystem
 
-psystem = love.graphics.newParticleSystem( gfx.particles, 4 )
-psystem:setOffset( 13, 13 )
+psystem = love.graphics.newParticleSystem(gfx.particles, 4)
+psystem:setOffset(13, 13)
 psystem:setEmitterLifetime(0.2)
 psystem:setParticleLifetime(0.15)
 psystem:setColors(unpack(impact_colors))
-psystem:setQuads( imp_medium_quad1, imp_medium_quad2, imp_medium_quad3 )
+psystem:setQuads(imp_medium_quad1, imp_medium_quad2, imp_medium_quad3)
 PA_IMPACT_MEDIUM = psystem
 
-psystem = love.graphics.newParticleSystem( gfx.particles, 4 )
-psystem:setOffset( 15, 15 )
+psystem = love.graphics.newParticleSystem(gfx.particles, 4)
+psystem:setOffset(15, 15)
 psystem:setEmitterLifetime(0.2)
 psystem:setParticleLifetime(0.15)
 psystem:setColors(unpack(impact_colors))
-psystem:setQuads( imp_big_quad1, imp_big_quad2, imp_big_quad3 )
+psystem:setQuads(imp_big_quad1, imp_big_quad2, imp_big_quad3)
 PA_IMPACT_BIG = psystem
 
-psystem = love.graphics.newParticleSystem( gfx.particles, 32 )
+psystem = love.graphics.newParticleSystem(gfx.particles, 32)
 psystem:setSizes(0.3, 0.6, 0.4, 0.1)
 psystem:setColors(unpack(dust_step_colors))
 psystem:setLinearAcceleration(-400, -20, 400, -100) -- Random movement in all directions.
-psystem:setLinearDamping( 7, 20 )
-psystem:setAreaSpread( "uniform", 15, 5 )
-psystem:setPosition( 0, -4 )
+psystem:setLinearDamping(7, 20)
+psystem:setAreaSpread("uniform", 15, 5)
+psystem:setPosition(0, -4)
 psystem:setParticleLifetime(1, 4)
 psystem:setEmitterLifetime(4)
 psystem:emit(20)
 PA_DUST_PUFF_STAGE = psystem
 
-psystem = love.graphics.newParticleSystem( gfx.particles, 50 )
---psystem:setPosition( 0, -2 )
+psystem = love.graphics.newParticleSystem(gfx.particles, 50)
+--psystem:setPosition(0, -2)
 psystem:setEmitterLifetime(2.1)
 psystem:setParticleLifetime(0.3, 2)
 psystem:setSizes(0.2, 0.5, 0.1)
 --psystem:setSizeVariation(0.7)
---psystem:setSpeed( 1, 1 )
+--psystem:setSpeed(1, 1)
 psystem:setDirection(2.71)
 psystem:setLinearAcceleration(0, -10, 0, -50) -- Random movement in all directions.
 psystem:setColors(unpack(dust_step_colors))
-psystem:setOffset( 15, 15 )
-psystem:setQuads( dust_quad )
-psystem:setLinearDamping( 7, 10 )
---psystem:setAreaSpread( "uniform", 80, 40 )
+psystem:setOffset(15, 15)
+psystem:setQuads(dust_quad)
+psystem:setLinearDamping(7, 10)
+--psystem:setAreaSpread("uniform", 80, 40)
 --psystem:setSpin(0, -3)
 PA_DASH = psystem
 
@@ -158,12 +158,12 @@ PA_OBSTACLE_BREAK_BIG = psystem
 local ds_quad1 = q(2,54,31,30) -- Defensive Special Effect frame 1/3
 local ds_quad2 = q(35,54,31,30) -- Defensive Special 2/3
 local ds_quad3 = q(68,54,31,30) -- Defensive Special 3/3
-local ds_colors = {255,255,255,255, 255,255,255,255, 255,255,255,55} --R,G,B,Alphha, ...
+local ds_colors = {255,255,255,255, 255,255,255,255, 255,255,255,55} --R,G,B,Alpha, ...
 
-psystem = love.graphics.newParticleSystem( gfx.particles, 1 )
-psystem:setOffset( 15, 30 ) --center-bottom of the sprite  width/2, height
+psystem = love.graphics.newParticleSystem(gfx.particles, 1)
+psystem:setOffset(15, 30) --center-bottom of the sprite width/2, height
 psystem:setEmitterLifetime(1) --whole lengths of the anim
 psystem:setParticleLifetime(1) --should equal to setEmitterLifetime
 psystem:setColors(unpack(ds_colors))
-psystem:setQuads( ds_quad1, ds_quad2, ds_quad3, ds_quad1, ds_quad3 )
+psystem:setQuads(ds_quad1, ds_quad2, ds_quad3, ds_quad1, ds_quad3)
 PA_DEFENSIVE_SPECIAL = psystem
