1bb49f347d796d02419b269194807098dcab7a41
Thomas Goldstein <stifu@free.fr>
2017-05-27 20:37:49 +0200

    Rename defensiveSpecialEffects func to defensiveSpecialFx

--- src/def/char/rick.lua
+++ src/def/char/rick.lua
@@ -105,7 +105,7 @@ local jumpAttackStraight2 = function(slf, cont) slf:checkAndAttack(
     cont
 ) end
 local shoveNow = function(slf, cont) slf.canShoveNow = true end
-local defensiveSpecialEffects = function(slf, cont)
+local defensiveSpecialFx = function(slf, cont)
 	sfx.play("sfx","hitWeak1")
     mainCamera:onShake(0, 2, 0.03, 0.3)	--shake the screen
 	local particles = (slf.face == 1 and PA_DEFENSIVE_SPECIAL_R or PA_DEFENSIVE_SPECIAL_L):clone()
@@ -230,7 +230,7 @@ return {
 			{ q = q(49,1505,49,61), ox = 25, oy = 60, delay = 0.1 }, --defensive special 2
 			{ q = q(100,1505,45,61), ox = 18, oy = 60 }, --defensive special 3
 			{ q = q(147,1506,54,60), ox = 15, oy = 59 }, --defensive special 4
-			{ q = q(2,1568,58,57), ox = 15, oy = 54, func = defensiveSpecialEffects }, --defensive special 5a
+			{ q = q(2,1568,58,57), ox = 15, oy = 54, func = defensiveSpecialFx }, --defensive special 5a
 			{ q = q(62,1569,58,56), ox = 15, oy = 53, funcCont = defensiveSpecial }, --defensive special 5b
 			{ q = q(122,1570,58,55), ox = 15, oy = 52, funcCont = defensiveSpecial, delay = 0.233 }, --defensive special 5c
 			{ q = q(2,1630,50,60), ox = 15, oy = 59, delay = 0.067 }, --defensive special 6
