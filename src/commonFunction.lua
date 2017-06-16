8b89ea2968e92e03b2071f780215009a362a9e91
Don Miguel <mikhail.bratus@gmail.com>
2017-06-16 16:46:12 +0500

    new linear collision check func

--- src/commonFunction.lua
+++ src/commonFunction.lua
@@ -19,6 +19,11 @@ function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
             y2 < y1+h1
 end
 
+function CheckLinearCollision(y1,h1, y2,h2)
+    return y1 < y2+h2 and
+            y2 < y1+h1
+end
+
 function CheckPointCollision(x,y, x1,y1,w1,h1)
     return x < x1+w1 and
             x >= x1 and
