1b22c7463caf3beededb884e5da0cf8d3002fca0
D0NM <hyuBYSnONMVWgWDpCeV4>
2017-06-20 22:02:09 +0500

    mew loot/weapon sprite definition (possible to add animations later)

--- src/def/misc/apple.lua
+++ src/def/misc/apple.lua
@@ -9,23 +9,14 @@ end
 return {
     serializationVersion = 0.42, -- The version of this serialization process
     spriteSheet = spriteSheet, -- The path to the spritesheet
-    spriteName = "bat", -- The name of the sprite
-    delay = 5.20,	--default delay for all animations
+    spriteName = "apple", -- The name of the sprite
+    delay = 5.21,	--default delay for all animations
     animations = {
         icon  = {
-            { q = q(2,23,38,11) } -- default 38x17
+            { q = q(2,2,18,17) } -- default 38x17
         },
         stand = {
-            { q = q(2,23,55,11), ox = 27, oy = 10 } --on the ground
+            { q = q(2,2,18,17), ox = 9, oy = 16 } --on the ground
         },
-        angle0 = {
-            { q = q(2,23,55,11), ox = 12, oy = 5 }  --a0 -
-        },
-        angle22 = {
-            { q = q(2,36,54,28), ox = 13, oy = 9 } --a22 \-
-        },
-        angle45 = {
-            { q = q(2,66,44,44), ox = 12, oy = 13 } --a45 \+
-        }
     }
 } --return (end of file)
--- src/def/misc/chicken.lua
+++ src/def/misc/chicken.lua
@@ -9,23 +9,14 @@ end
 return {
     serializationVersion = 0.42, -- The version of this serialization process
     spriteSheet = spriteSheet, -- The path to the spritesheet
-    spriteName = "bat", -- The name of the sprite
-    delay = 5.20,	--default delay for all animations
+    spriteName = "chicken", -- The name of the sprite
+    delay = 5.23,	--default delay for all animations
     animations = {
         icon  = {
-            { q = q(2,23,38,11) } -- default 38x17
+            { q = q(22,2,30,19) } -- default 38x17
         },
         stand = {
-            { q = q(2,23,55,11), ox = 27, oy = 10 } --on the ground
+            { q = q(22,2,30,19), ox = 15, oy = 18 } --on the ground
         },
-        angle0 = {
-            { q = q(2,23,55,11), ox = 12, oy = 5 }  --a0 -
-        },
-        angle22 = {
-            { q = q(2,36,54,28), ox = 13, oy = 9 } --a22 \-
-        },
-        angle45 = {
-            { q = q(2,66,44,44), ox = 12, oy = 13 } --a45 \+
-        }
     }
 } --return (end of file)
