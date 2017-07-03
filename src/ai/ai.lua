f4c598e7eb4ceae40a3ddf01f927f0e9ea630c2b
Don Miguel <mikhail.bratus@gmail.com>
2017-07-03 14:15:20 +0500

    add AI for general run,walk, picking up target, facing to the player and a combo

--- src/ai/ai.lua
+++ src/ai/ai.lua
@@ -4,6 +4,7 @@ local class = require "lib/middleclass"
 local AI = class('AI')
 
 local function nop() end
+
 local sign = sign
 local clamp = clamp
 local dist = dist
@@ -25,12 +26,17 @@ local CheckCollision = CheckCollision
 function AI:initialize(unit)
     self.unit = unit
     self.conditions = {}
-    self.thinkInterval = 1
+    self.thinkInterval = 1 + math.random()
     self.currentSchedule = nil
 
-    self.SCHEDULE_INTRO = Schedule:new({self.initIntro, self.onIntro}, {"seePlayer", "random"}, unit.name)
-    self.SCHEDULE_STAND = Schedule:new({self.initStand, self.onStand}, {"bored", "random", "random2"}, unit.name)
---print(inspect(self.SCHEDULE_INTRO))
+    self.SCHEDULE_INTRO = Schedule:new({ self.initIntro, self.onIntro }, { "seePlayer", "wokeUp" }, unit.name)
+    self.SCHEDULE_STAND = Schedule:new({ self.initStand, self.onStand }, { "noTarget", "canCombo", "canDash", "faceNotToPlayer" }, unit.name)
+    self.SCHEDULE_WALK = Schedule:new({ self.initWalk, self.onWalk }, { "noTarget", "canCombo", "canDash", "faceNotToPlayer" }, unit.name)
+    self.SCHEDULE_RUN = Schedule:new({ self.initRun, self.onRun }, { "noTarget", "canCombo", "canDash", "faceNotToPlayer" }, unit.name)
+    self.SCHEDULE_PICK_TARGET = Schedule:new({ self.initPickTarget }, { "noPlayers" }, unit.name)
+    self.SCHEDULE_FACE_TO_PLAYER = Schedule:new({ self.initFaceToPlayer }, { "noTarget", "noPlayers", "tooFar" }, unit.name)
+    self.SCHEDULE_COMBO = Schedule:new({ self.initCombo, self.onCombo }, { "noTarget", "tooFar" }, unit.name)
+
     --self.currentSchedule = self.SCHEDULE_INTRO
     self:selectNewSchedule()
 end
@@ -38,12 +44,13 @@ end
 function AI:update(dt)
     self.thinkInterval = self.thinkInterval - dt
     if self.thinkInterval <= 0 then
-        dp("AI "..self.unit.name.." thinking")
+        dp("AI " .. self.unit.name .. " thinking")
         self.conditions = self:getConditions()
+        print(inspect(self.conditions))
         if not self.currentSchedule or self.currentSchedule:isDone(self.conditions) then
             self:selectNewSchedule(self.conditions)
         end
-        self.thinkInterval = 1 + math.random()
+        self.thinkInterval = 1 + math.random() / 64
     end
     -- run current schedule
     if self.currentSchedule then
@@ -53,56 +60,219 @@ end
 
 function AI:selectNewSchedule(conditions)
     --self.thinkInterval = 0
+    --print(inspect(conditions))
     if not self.currentSchedule then
         self.currentSchedule = self.SCHEDULE_INTRO
         dp("   Select NEW SCHEDULE INTRO")
         return
     end
-    if self.currentSchedule ~= self.SCHEDULE_STAND then
+    if conditions.noPlayers then
+        if self.currentSchedule ~= self.SCHEDULE_INTRO then
+            self.currentSchedule = self.SCHEDULE_INTRO
+            dp("   *2 Select NEW SCHEDULE INTRO")
+        else
+            dp("   *3 Stay with SCHEDULE INTRO")
+        end
+        return
+    end
+    if conditions.noTarget then
+        self.currentSchedule = self.SCHEDULE_PICK_TARGET
+        return
+    end
+    if conditions.faceNotToPlayer then
+        self.currentSchedule = self.SCHEDULE_FACE_TO_PLAYER
+        return
+    end
+    if conditions.canCombo then
+        self.currentSchedule = self.SCHEDULE_COMBO
+        return
+    end
+    --[[    if conditions.canDash then
+            self.currentSchedule = self.SCHEDULE_DASH
+            return
+        end]]
+    if conditions.tooFar then --and math.random() < 0.25
+        self.currentSchedule = self.SCHEDULE_RUN
+        return
+    end
+    if conditions.seePlayer then --and math.random() < 0.5
+        self.currentSchedule = self.SCHEDULE_WALK
+        return
+    end
+    --print(conditions.wokeUp , conditions.seePlayer)
+    if conditions.wokeUp or conditions.seePlayer then
         self.currentSchedule = self.SCHEDULE_STAND
-        dp("   *Select NEW SCHEDULE STAND")
+        dp("   *2 Select NEW SCHEDULE STAND")
     else
         self.currentSchedule = self.SCHEDULE_INTRO
-        dp("   *Select NEW SCHEDULE INTRO")
+        dp("   *4 Select NEW SCHEDULE INTRO")
     end
 end
 
 function AI:getConditions()
-    local conditions = {"oops_cond"}
-    if math.random() < 0.5 then
-        conditions[#conditions + 1] = "random"
-    end
+    local u = self.unit
+    local conditions = {}
     self:getVisualConditions(conditions)
-    return conditions
+    if not areThereAlivePlayers() then
+        conditions[#conditions + 1] = "noPlayers"
+    end
+    if u.target and u.target.isDisabled then
+        conditions[#conditions + 1] = "targetDead"
+    end
+    local conditionsOutput = {}
+    for _, cond in ipairs(conditions) do
+        conditionsOutput[cond] = true
+    end
+    return conditionsOutput
 end
 
 function AI:getVisualConditions(conditions)
     -- check attack range, players, units etc
-    if math.random() < 0.5 then
-        conditions[#conditions + 1] = "visualRandom"
+    local u = self.unit
+    local t
+    if not u.target then
+        conditions[#conditions + 1] = "noTarget"
+    else
+        -- facing to the player to player
+        if u.target.x < u.x then
+            if u.face < 0 then
+                conditions[#conditions + 1] = "faceToPlayer"
+            else
+                conditions[#conditions + 1] = "faceNotToPlayer"
+            end
+            if u.target.face < 0 then
+                conditions[#conditions + 1] = "playerBack"
+            else
+                conditions[#conditions + 1] = "playerSeeYou"
+            end
+        else
+            if u.face > 0 then
+                conditions[#conditions + 1] = "faceToPlayer"
+            else
+                conditions[#conditions + 1] = "faceNotToPlayer"
+            end
+            if u.target.face > 0 then
+                conditions[#conditions + 1] = "playerBack"
+            else
+                conditions[#conditions + 1] = "playerSeeYou"
+            end
+        end
+        t = dist(u.target.x, u.target.y, u.x, u.y)
+        if t < 100 and t >= 30
+                and math.floor(u.y / 4) == math.floor(u.target.y / 4) then
+            conditions[#conditions + 1] = "canDash"
+        end
+        if u.cooldown <= 0 then
+            if math.abs(u.x - u.target.x) <= 50
+                    and math.abs(u.y - u.target.y) <= 6 then
+                conditions[#conditions + 1] = "canCombo"
+            end
+        end
+        if t > 100 then
+            conditions[#conditions + 1] = "tooFar"
+        end
+    end
+    t = u:getDistanceToClosestPlayer()
+    if t < u.delayedWakeupRange and u.time > u.wakeupDelay then
+        -- ready to act
+        conditions[#conditions + 1] = "wokeUp"
+    end
+    if t < u.wakeupRange then
+        -- see near players?
+        conditions[#conditions + 1] = "seePlayer"
     end
 end
 
 function AI:initIntro()
-    dp("AI:initIntro() ".. self.unit.name)
+    dp("AI:initIntro() " .. self.unit.name)
     self.unit:setSprite("intro")
     return true
 end
+
 function AI:onIntro()
-    dp("AI:onIntro() ".. self.unit.name)
---    return false
-    return math.random() < 0.01
+    --dp("AI:onIntro() ".. self.unit.name)
+    return false
 end
 
 function AI:initStand()
-    dp("AI:initStand() ".. self.unit.name)
+    dp("AI:initStand() " .. self.unit.name)
     self.unit:setSprite("stand")
     return true
 end
+
 function AI:onStand()
-    dp("AI:onStand() ".. self.unit.name)
+    dp("AI:onStand() " .. self.unit.name)
     return false
---    return math.random() < 0.05
 end
+
+function AI:initWalk()
+    local u = self.unit
+    dp("AI:initWalk() " .. self.unit.name)
+    if u.cooldown > 0 or u.state ~= "stand" then
+        return false
+    end
+    u:setState(u.walk)
+    return true
+end
+
+function AI:onWalk()
+    local u = self.unit
+    --    dp("AI:onWalk() ".. self.unit.name)
+    if u.move then
+        return u.move:update(0)
+    else
+        return true
+    end
+    return false
+end
+
+function AI:initRun()
+    local u = self.unit
+    dp("AI:initRun() " .. self.unit.name)
+    if u.cooldown > 0 or u.state ~= "stand" then
+        return false
+    end
+    u:setState(u.run)
+    return true
+end
+
+function AI:onRun()
+    local u = self.unit
+    dp("AI:onRun() " .. self.unit.name)
+    if u.move then
+        return u.move:update(0)
+    else
+        return true
+    end
+    return false
+end
+
+function AI:initPickTarget()
+    dp("AI:initPickTarget() " .. self.unit.name)
+    --    self.unit:pickAttackTarget()
+    print("PICKED TARGET", self.unit:pickAttackTarget())
+    return true
+end
+
+function AI:initFaceToPlayer()
+    dp("AI:initFaceToPlayer() " .. self.unit.name)
+    if not self.unit.isHittable then
+        return false
+    end
+    self.unit.face = -self.unit.face
+    return true
+end
+
+function AI:initCombo()
+    dp("AI:initCombo() " .. self.unit.name)
+    self.unit:setState(self.unit.combo)
+    return true
+end
+
+function AI:onCombo()
+    --    dp("AI:onCombo() ".. self.unit.name)
+    return self.unit.state == "stand"
+end
+
 
 return AI
