9277c9baf40a33239b515a7ee18df71999d69a30
Don Miguel <mikhail.bratus@gmail.com>
2017-07-10 11:22:34 +0500

    add chanceForkToGrab var to tweak % of intentional player grabbing

--- src/unit/ai/niko_ai.lua
+++ src/unit/ai/niko_ai.lua
@@ -4,6 +4,8 @@
 local class = require "lib/middleclass"
 local eAI = class('eAI', AI)
 
+local chanceForkToGrab = 1 -- 1 == 100%, 0 == 0%
+
 local _speedReaction = {
     thinkIntervalMin = 0.02,
     thinkIntervalMax = 0.25,
@@ -35,16 +37,20 @@ function eAI:selectNewSchedule(conditions)
         return
     end
     if not conditions.cannotAct then
-        if conditions.canMove and conditions.tooCloseToPlayer then --and math.random() < 0.5
-            if love.math.random() < 1 then
+        if conditions.faceNotToPlayer then
+            self.currentSchedule = self.SCHEDULE_FACE_TO_PLAYER
+            return
+        end
+        if conditions.canMove and conditions.canGrab then
+            if love.math.random() < chanceForkToGrab then
                 self.currentSchedule = self.SCHEDULE_GRAB
             else
                 self.currentSchedule = self.SCHEDULE_BACKOFF
             end
             return
         end
-        if conditions.faceNotToPlayer then
-            self.currentSchedule = self.SCHEDULE_FACE_TO_PLAYER
+        if conditions.canMove and conditions.tooCloseToPlayer then --and math.random() < 0.5
+            self.currentSchedule = self.SCHEDULE_BACKOFF
             return
         end
         if conditions.canCombo then
@@ -52,7 +58,7 @@ function eAI:selectNewSchedule(conditions)
             return
         end
         if conditions.canMove and (conditions.seePlayer or conditions.wokeUp) or not conditions.noTarget then
-            if love.math.random() < 1 then
+            if love.math.random() < chanceForkToGrab then
                 self.currentSchedule = self.SCHEDULE_WALK_TO_GRAB
             else
                 self.currentSchedule = self.SCHEDULE_WALK_TO_ATTACK
