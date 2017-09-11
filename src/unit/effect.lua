3aa0c3bc8db8c3e1d3dfeba136b546bd649b0456
Don Miguel <mikhail.bratus@gmail.com>
2017-09-11 11:34:14 +0500

    replaced hardcoded z == 0 with an optional z parameter

--- src/unit/effect.lua
+++ src/unit/effect.lua
@@ -3,11 +3,11 @@ local Effect = class("Effect")
 
 local CheckCollision = CheckCollision
 
-function Effect:initialize(particle, x, y, shader, color)
+function Effect:initialize(particle, x, y, z, shader, color)
     self.particle = particle
     self.name = "Particle"
     self.type = "effect"
-    self.x, self.y, self.z = x, y, 0
+    self.x, self.y, self.z = x, y, z or 0
     self.color = color or { 255,255,255,255 }
     self.shader = shader
     self.isDisabled = false
@@ -24,7 +24,7 @@ end
 function Effect:draw(l,t,w,h)
     if not self.isDisabled and CheckCollision(l, t, w, h, self.x-20, self.y-40, 40, 40) then
         love.graphics.setColor( unpack( self.color ) )
-        love.graphics.draw(self.particle, self.x, self.y)
+        love.graphics.draw(self.particle, self.x, self.y - self.z)
     end
 end
 
@@ -32,17 +32,14 @@ function Effect:onHurt()
 end
 
 function Effect:onRemove()
---    dp("remove eff ", self.x, self.y)
     self.isDisabled = true
     self.y = GLOBAL_SETTING.OFFSCREEN
 end
 
 function Effect:update(dt)
     if self.isDisabled then
---        dp("eff upd HIDDEN ", self.x, self.y)
         return
     end
-    --dp("eff upd VISIBLE ", self.x, self.y)
     self.particle:update(dt)
     if self.particle:isStopped() then
         self:onRemove()
