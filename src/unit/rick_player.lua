2c3efb5a6c53be3694fdd11d413d8f968398a2f2
Don Miguel <mikhail.bratus@gmail.com>
2017-10-10 11:53:31 +0500

    rename hold.cooldown -> cooldownGrab, added max value to cooldownGrabMax

--- src/unit/rick_player.lua
+++ src/unit/rick_player.lua
@@ -93,8 +93,8 @@ function Rick:backShoveStart()
     self.isHittable = false
     local g = self.hold
     local t = g.target
-    t.cooldown = self.cooldownGrab  --prolong grabbed time
-    g.cooldown = self.cooldownGrab + 0.1
+    t.cooldownGrab = self.cooldownGrabMax  --prolong grabbed time
+    g.cooldownGrab = self.cooldownGrabMax + 0.1
     self:moveStatesInit()
     t.isHittable = false    --protect grabbed enemy from hits
     self:setSprite("backShove")
