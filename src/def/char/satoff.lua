3d75e95b261d6e3d2d40cb1a93424958ad23ada0
Ly-Jia Goldstein <hello@lyjia.net>
2017-12-21 00:29:32 +0100

    Replace Satoff's second of his grabbedFrames to use "grabbed front 2" instead of "hurt high 2" (= no spit)

--- src/def/char/satoff.lua
+++ src/def/char/satoff.lua
@@ -266,7 +266,7 @@ return {
         grabbedFrames = {
             --default order should be kept: hurtLow2, hurtHigh2, \, /, upsideDown, lying down
             { q = q(214,216,72,64), ox = 36, oy = 63 }, --grabbed back 2
-            { q = q(72,142,69,68), ox = 39, oy = 67 }, --hurt high 2
+            { q = q(72,212,69,68), ox = 39, oy = 67 }, --grabbed front 2
             { q = q(2,284,71,57), ox = 36, oy = 56 }, --falling
             { q = q(2,284,71,57), ox = 29, oy = 47, rotate = -1.57, rx = 35, ry = -56 }, --falling
             { q = q(214,146,72,64), ox = 36, oy = 63, flipV = -1 }, --hurt low 2
