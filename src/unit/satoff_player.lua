97ba2aa874f46cfd3dd2cf836225e2809e973581
D0NM <hyuBYSnONMVWgWDpCeV4>
2017-12-23 02:33:38 +0500

    refactor D487 removed unnecessary combo attack function borrowing

--- src/unit/satoff_player.lua
+++ src/unit/satoff_player.lua
@@ -13,7 +13,7 @@ function Satoff:initialize(name, sprite, input, x, y, f)
     if not f then
         f = {}
     end
-    f.shapeType = f.shapeType or "polygon"
+    f.shapeType = f.shapeType or "polygon" --Satoff has an unique base hitbox shape
     f.shapeArgs = f.shapeArgs or { 1, 0, 27, 0, 28, 3, 27, 6, 1, 6, 0, 3 }
     Player.initialize(self, name, sprite, input, x, y, f)
 end
@@ -21,7 +21,5 @@ end
 function Satoff:initAttributes()
     _Satoff.initAttributes(self)
 end
-
-Satoff.combo = {name = "combo", start = _Satoff.comboStart, exit = nop, update = _Satoff.comboUpdate, draw = Character.defaultDraw}
 
 return Satoff
