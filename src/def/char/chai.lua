bc25db04732c498574c49902bbdf7e7edc91b9ab
Thomas Goldstein <stifu@free.fr>
2018-01-14 13:07:27 +0100

    Update some of Chai's frame comments

--- src/def/char/chai.lua
+++ src/def/char/chai.lua
@@ -515,7 +515,7 @@ return {
             delay = 0.05
         },
         frontGrabAttackUp = {
-            { q = q(131,1660,42,63), ox = 20, oy = 62, flipH = -1 }, --grab attack 1.3
+            { q = q(131,1660,42,63), ox = 20, oy = 62, flipH = -1 }, --grab attack 1.3 (shifted left by 3px)
             { q = q(2,928,40,62), ox = 20, oy = 62, flipH = -1 }, --throw 1.1
             { q = q(44,928,51,63), ox = 26, oy = 62 }, --throw 1.2
             { q = q(97,928,53,63), ox = 22, oy = 62, func = frontGrabAttackUp, delay = 0.2 }, --throw 1.3
@@ -524,7 +524,7 @@ return {
             isThrow = true,
         },
         frontGrabAttackBack = {
-            { q = q(131,1660,42,63), ox = 20, oy = 62, flipH = -1 }, --grab attack 1.3
+            { q = q(131,1660,42,63), ox = 20, oy = 62, flipH = -1 }, --grab attack 1.3 (shifted left by 3px)
             { q = q(2,928,40,62), ox = 20, oy = 62, flipH = -1 }, --throw 1.1
             { q = q(44,928,51,63), ox = 26, oy = 62, func = frontGrabAttackBack }, --throw 1.2
             { q = q(97,928,53,63), ox = 22, oy = 62, delay = 0.2 }, --throw 1.3
@@ -540,9 +540,9 @@ return {
             }
         },
         frontGrabAttackForward = {
-            { q = q(131,1660,42,63), ox = 20, oy = 62, flipH = -1 }, --grab attack 1.3
-            { q = q(131,1660,42,63), ox = 20, oy = 62, flipH = -1 }, --grab attack 1.3
-            { q = q(131,1660,42,63), ox = 20, oy = 62, flipH = -1 }, --grab attack 1.3
+            { q = q(131,1660,42,63), ox = 20, oy = 62, flipH = -1 }, --grab attack 1.3 (shifted left by 3px)
+            { q = q(131,1660,42,63), ox = 20, oy = 62, flipH = -1 }, --grab attack 1.3 (shifted left by 3px)
+            { q = q(131,1660,42,63), ox = 20, oy = 62, flipH = -1 }, --grab attack 1.3 (shifted left by 3px)
             { q = q(2,928,40,62), ox = 20, oy = 62, flipH = -1 }, --throw 1.1
             { q = q(44,928,51,63), ox = 26, oy = 62, func = frontGrabAttackForward }, --throw 1.2
             { q = q(97,928,53,63), ox = 22, oy = 62 }, --throw 1.3
