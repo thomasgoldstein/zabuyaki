4d05d56f34c3451f3441d1568fd2649b423498fc
D0NM <hyuBYSnONMVWgWDpCeV4>
2018-05-09 01:08:39 +0500

    new unit test: check Rick's combo1,2,3,4 damage

--- test/test2.lua
+++ test/test2.lua
@@ -53,24 +53,155 @@ function calcDistanceForSpeedAndFriction(a)
     --    print("Final speed_x, speed_y:", u.speed_x, u.speed_y)
 end
 
+-- Test 1 damage
+function checkComboDamage(a, b, res)
+    if not a or not b then
+        return
+    end
+    local FPS = 60
+    local time = 1
+    local dt = 1 / FPS
+    --    print("Start x,y:", u.x, u.y, u.name, u.id)
+    --print("FPS:", FPS, " dt:", dt, " time:", time)
+    print("Unit-1 HP:", a.hp, " Unit-2 HP:", b.hp)
+    --print("xy", a.x, a.y, "xy", b.x, b.y)
+    --print(a.horizontal, a.face, b.horizontal, b.face)
+    --print("shape", a.shape, "sh2", b.shape)
+    --    print("Start speed_x, speed_y:", u.speed_x, u.speed_y)
+    local nextAttack = 0
+    for i = 1, time * FPS do
+--        print(a.name, a.state, a.isHittable, " == ", b.name, b.state, b.isHittable)
+        nextAttack = nextAttack + dt
+        if nextAttack > 0.5 and a.state == "stand" then
+            nextAttack = 0
+            a:setState(a.combo)
+        end
+        a:updateAI(dt)
+        a:update(dt)
+        b:updateAI(dt)
+        b:update(dt)
+        a:onHurt()
+        b:onHurt()
+    end
+    print("Final Unit-1 HP:", a.hp, " Unit-2 HP:", b.hp)
+    return b.hp == res
+end
+
+-- Test combo attacks connection
+function checkComboAttackConnection(a, b, timeBetweenAttacks)
+    if not a or not b then
+        return
+    end
+    local FPS = 60
+    local time = 5
+    local dt = 1 / FPS
+    --    print("Start x,y:", u.x, u.y, u.name, u.id)
+    print("FPS:", FPS, " dt:", dt, " time:", time)
+    print("Unit-1 HP:", a.hp, " Unit-2 HP:", b.hp)
+    print("xy", a.x, a.y, "xy", b.x, b.y)
+    print(a.horizontal, a.face, b.horizontal, b.face)
+    --print("shape", a.shape, "sh2", b.shape)
+--    print("Start speed_x, speed_y:", u.speed_x, u.speed_y)
+    local nextAttack = 0
+    for i = 1, time * FPS do
+        print(a.name, a.state, a.isHittable, " == ", b.name, b.state, b.isHittable)
+        nextAttack = nextAttack + dt
+        if nextAttack > (timeBetweenAttacks or 0.5) and a.state == "stand" then
+            print("!!",a.connectHit,a.attacksPerAnimation)
+            nextAttack = 0
+            a:setState(a.combo)
+        end
+
+        a:updateAI(dt)
+        a:update(dt)
+        if a.infoBar then
+            a.infoBar:update(dt)
+        end
+        b:updateAI(dt)
+        b:update(dt)
+        if b.infoBar then
+            b.infoBar:update(dt)
+        end
+        a:onHurt()
+        b:onHurt()
+    end
+--    self.connectHit
+--    self.attacksPerAnimation
+
+    print("Final Unit-1 HP:", a.hp, " Unit-2 HP:", b.hp)
+    return false
+end
+
+
 -- prepare dummy stage
 stage = Stage:new()
 
 cleanRegisteredPlayers()
+local player1, player2, whichPlayer
 -- prepare dummy player
-local whichPlayer = 1
-local player = HEROES[whichPlayer].hero:new("SPRED", getSpriteInstance(HEROES[whichPlayer].spriteInstance), DUMMY_CONTROL, 0, 0 )
-player.id = 1   -- fixed id
-player:setOnStage(stage)
-player:setState(player.stand)
-registerPlayer(player)
+whichPlayer = 1
+player1 = HEROES[whichPlayer].hero:new("PL1", getSpriteInstance(HEROES[whichPlayer].spriteInstance), DUMMY_CONTROL, 0, 0 )
+player1.id = 1   -- fixed id
+player1:setOnStage(stage)
+player1:setState(player1.stand)
+registerPlayer(player1)
 
-local p = getRegisteredPlayer(1)
-calcDistanceForSpeedAndFriction({
-    speed = p.comboSlideSpeed2_x,   -- 1) slide speed x
-    friction = p.repelFriction,     -- 2) repelFriction
-    toSlowDown = false,
-    name = p.name, id = p.id })
+whichPlayer = 2
+player2 = HEROES[whichPlayer].hero:new("PL2", getSpriteInstance(HEROES[whichPlayer].spriteInstance), DUMMY_CONTROL, 0, 0 )
+player2.id = 2   -- fixed id
+player2:setOnStage(stage)
+player2:setState(player2.stand)
+registerPlayer(player2)
+
+-- run tests
+
+test("checkComboDamage()",
+    function()
+        player1.x = 30
+        player1.y = 200
+        player2.x = 80
+        player2.y = 200
+        player2.hp = 100
+        return checkComboDamage(player1, player2, 93)
+    end,
+    function()
+        player1.x = 30
+        player1.comboN = 2
+        return checkComboDamage(player1, player2, 86)
+    end,
+    function()
+        player1.x = 30
+        player1.comboN = 3
+        return checkComboDamage(player1, player2, 79)
+    end,
+    function()
+        player1.x = 30
+        player1.comboN = 4
+        return checkComboDamage(player1, player2, 72)
+    end
+)
+
+--[[test("checkComboAttackConnection()",
+    function()
+--        print(#stage.objects.entities)
+        player1.x = 30
+        player1.y = 200
+        player2.x = 80
+        player2.y = 200
+        return checkComboAttackConnection(player1, player2, 0.9)
+    end
+)]]
+
+test("calcDistanceForSpeedAndFriction()",
+    function()
+        local p = getRegisteredPlayer(1)
+        return calcDistanceForSpeedAndFriction({
+            speed = p.comboSlideSpeed2_x,   -- 1) slide speed x
+            friction = p.repelFriction,     -- 2) repelFriction
+            toSlowDown = false,
+            name = p.name, id = p.id })
+    end
+)
 
 -- clean dummy players & stage
 cleanRegisteredPlayers()
