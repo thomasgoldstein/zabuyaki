5ba465d456c43fc8e984d5684b8ecdfe34e1044d
D0NM <hyuBYSnONMVWgWDpCeV4>
2018-05-10 21:06:21 +0500

    new UT function distance between players test

--- test/test2.lua
+++ test/test2.lua
@@ -62,15 +62,15 @@ local function checkComboAttackConnection(a, b, timeBetweenAttacks)
     local time = 5
     local dt = 1 / FPS
     --    print("Start x,y:", u.x, u.y, u.name, u.id)
---    print("FPS:", FPS, " dt:", dt, " time:", time)
---    print("Unit-1 HP:", a.hp, " Unit-2 HP:", b.hp)
---    print("xy", a.x, a.y, "xy", b.x, b.y)
---    print(a.horizontal, a.face, b.horizontal, b.face)
+    --    print("FPS:", FPS, " dt:", dt, " time:", time)
+    --    print("Unit-1 HP:", a.hp, " Unit-2 HP:", b.hp)
+    --    print("xy", a.x, a.y, "xy", b.x, b.y)
+    --    print(a.horizontal, a.face, b.horizontal, b.face)
     --print("shape", a.shape, "sh2", b.shape)
     --    print("Start speed_x, speed_y:", u.speed_x, u.speed_y)
     local nextAttack = 0
     for i = 1, time * FPS do
---        print(a.name, a.state, a.isHittable, " == ", b.name, b.state, b.isHittable)
+        --        print(a.name, a.state, a.isHittable, " == ", b.name, b.state, b.isHittable)
         nextAttack = nextAttack + dt
         if nextAttack > (timeBetweenAttacks or 0.5) and a.state == "stand" then
             print("!!", a.connectHit, a.attacksPerAnimation)
@@ -94,7 +94,7 @@ local function checkComboAttackConnection(a, b, timeBetweenAttacks)
     --    self.connectHit
     --    self.attacksPerAnimation
 
---    print("Final Unit-1 HP:", a.hp, " Unit-2 HP:", b.hp)
+    --    print("Final Unit-1 HP:", a.hp, " Unit-2 HP:", b.hp)
     return false
 end
 
@@ -128,9 +128,10 @@ describe("Characters moves", function()
         -- mock real lib functions
         --local _SFXplay = SFX.play
         --SFX.play = function() end
+
         -- prepare dummy stage
         stage = Stage:new()
-        --cleanRegisteredPlayers()
+
         local n
         -- prepare dummy player
         n = 1
@@ -138,78 +139,52 @@ describe("Characters moves", function()
         player1.id = 1 -- fixed id
         player1:setOnStage(stage)
         player1:setState(player1.stand)
-        --registerPlayer(player1)
+        player1.x = 30
+        player1.y = 200
         n = 2
         player2 = HEROES[n].hero:new("PL2", getSpriteInstance(HEROES[n].spriteInstance), DUMMY_CONTROL, 0, 0)
         player2.id = 2 -- fixed id
         player2:setOnStage(stage)
         player2:setState(player2.stand)
-        --registerPlayer(player2)
-        player1.x = 30
-        player1.y = 200
         player2.x = 80
         player2.y = 200
+        n = 3
+        player3 = HEROES[n].hero:new("PL3", getSpriteInstance(HEROES[n].spriteInstance), DUMMY_CONTROL, 0, 0)
+        player3.id = 3 -- fixed id
+        player3:setOnStage(stage)
+        player3:setState(player3.stand)
+        player3.x = 130
+        player3.y = 200
     end)
     lust.after(function(txt)
-        player1, player2 = nil, nil
         -- This gets run after every test.
+        player1, player2, player3 = nil, nil, nil
         cleanRegisteredPlayers()
         stage = nil
         -- restore mocked lib functions
         --SFX.play = _SFXplay
     end)
-    it('Combo 1 Damage (wrong P1 facing)', function()
+    it('Distance between players', function()
+        --return min_x + (max_x - min_x) / 2, max_x - min_x, min_x, max_x
+        local a, b, c, d = getDistanceBetweenPlayers()
+        expect(b).to.equal(player3.x - player1.x)
+        expect(a).to.equal(player1.x + (player3.x - player1.x) / 2)
+    end)
+    it('P1 uses Combo1 on P2 (wrong P1 facing)', function()
         player1.face = -1
         local res = checkComboDamage(player1, player2)
         expect(res).to.equal(100)
     end)
-    it('Combo 1 Damage (P2 is too far)', function()
-        player2.x = 120
-        local res = checkComboDamage(player1, player2)
+    it('P1 uses Combo1 on P3 (P3 is too far)', function()
+        local res = checkComboDamage(player1, player3)
         expect(res).to.equal(100)
     end)
-    it('Combo 1 Damage', function()
+    it('P2 loses 7HP after Combo1', function()
         local res = checkComboDamage(player1, player2)
         expect(res).to.equal(93)
     end)
 end)
 
---[[test("checkComboDamage()",
-    function()
-        player1.x = 30
-        player1.y = 200
-        player2.x = 80
-        player2.y = 200
-        player2.hp = 100
-        return checkComboDamage(player1, player2, 93)
-    end,
-    function()
-        player1.x = 30
-        player1.comboN = 2
-        return checkComboDamage(player1, player2, 85)
-    end,
-    function()
-        player1.x = 30
-        player1.comboN = 3
-        return checkComboDamage(player1, player2, 75)
-    end,
-    function()
-        player1.x = 30
-        player1.comboN = 4
-        return checkComboDamage(player1, player2, 72)
-    end
-)]]
-
---[[test("checkComboAttackConnection()",
-    function()
---        print(#stage.objects.entities)
-        player1.x = 30
-        player1.y = 200
-        player2.x = 80
-        player2.y = 200
-        return checkComboAttackConnection(player1, player2, 0.9)
-    end
-)]]
 
 --[[test("calcDistanceForSpeedAndFriction()",
     function()
