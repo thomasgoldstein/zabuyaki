39cb327cce2b959e9b59ef290a3df91ae9cf3e52
Don Miguel <mikhail.bratus@gmail.com>
2018-05-11 12:23:45 +0500

    added Rick's walk-jump,run-jump UT tests

--- test/test2.lua
+++ test/test2.lua
@@ -100,9 +100,9 @@ end
 
 local function setStateAndWait(a, setState, waitSeconds)
     local FPS = 60
-    local time = waitSeconds or 3
+    local time = waitSeconds or 5
     local dt = 1 / FPS
-    local x, y, z, hp  = a.x, a.y, a.z, a.hp
+    local x, y, z, hp = a.x, a.y, a.z, a.hp
     a:setState(setState)
     for i = 1, time * FPS do
         a:updateAI(dt)
@@ -111,9 +111,9 @@ local function setStateAndWait(a, setState, waitSeconds)
             a.infoBar:update(dt)
         end
         a:onHurt()
---        print(a.state)
+        --        print(a.state)
     end
---    print(":", a.x, a.y, a.z, a.hp, x, y, z, hp)
+    --    print(":", a.x, a.y, a.z, a.hp, x, y, z, hp)
     return a.x, a.y, a.z, a.hp, x, y, z, hp
 end
 
@@ -155,21 +155,21 @@ describe("Character Class", function()
         local n
         -- prepare dummy player
         n = 1
-        player1 = HEROES[n].hero:new("PL1-"..HEROES[n][1].name, getSpriteInstance(HEROES[n].spriteInstance), DUMMY_CONTROL, 0, 0)
+        player1 = HEROES[n].hero:new("PL1-" .. HEROES[n][1].name, getSpriteInstance(HEROES[n].spriteInstance), DUMMY_CONTROL, 0, 0)
         player1.id = 1 -- fixed id
         player1:setOnStage(stage)
         player1:setState(player1.stand)
         player1.x = 130
         player1.y = 200
         n = 2
-        player2 = HEROES[n].hero:new("PL2-"..HEROES[n][1].name, getSpriteInstance(HEROES[n].spriteInstance), DUMMY_CONTROL, 0, 0)
+        player2 = HEROES[n].hero:new("PL2-" .. HEROES[n][1].name, getSpriteInstance(HEROES[n].spriteInstance), DUMMY_CONTROL, 0, 0)
         player2.id = 2 -- fixed id
         player2:setOnStage(stage)
         player2:setState(player2.stand)
         player2.x = 150
         player2.y = 200
         n = 3
-        player3 = HEROES[n].hero:new("PL3-"..HEROES[n][1].name, getSpriteInstance(HEROES[n].spriteInstance), DUMMY_CONTROL, 0, 0)
+        player3 = HEROES[n].hero:new("PL3-" .. HEROES[n][1].name, getSpriteInstance(HEROES[n].spriteInstance), DUMMY_CONTROL, 0, 0)
         player3.id = 3 -- fixed id
         player3:setOnStage(stage)
         player3:setState(player3.stand)
@@ -184,43 +184,69 @@ describe("Character Class", function()
         -- restore mocked lib functions
         --SFX.play = _SFXplay
     end)
-    it('Distance between players', function()
+    it('Function getDistanceBetweenPlayers', function()
         --return min_x + (max_x - min_x) / 2, max_x - min_x, min_x, max_x
         local a, b, c, d = getDistanceBetweenPlayers()
         expect(b).to.equal(player3.x - player1.x)
         expect(a).to.equal(player1.x + (player3.x - player1.x) / 2)
     end)
+    describe("Rick Class", function()
+        describe("Jump Method", function()
+            it('P1 jumps on place', function()
+                --print(player1.name, player1.duck2jump.name)
+                --            player1.speed_x = 0
+                local x, y, z, hp, _x, _y, _z, _hp = setStateAndWait(player1, player1.duck2jump)
+                expect(x).to.equal(_x)
+                expect(y).to.equal(_y)
+                expect(z).to.equal(_z)
+                expect(hp).to.equal(_hp)
+            end)
+            it('P1 jumps after walking diagonally', function()
+                --print(player1.name, player1.duck2jump.name)
+                player1.speed_x = player1.walkSpeed_x
+                player1.speed_y = player1.walkSpeed_y
+                local x, y, z, hp, _x, _y, _z, _hp = setStateAndWait(player1, player1.duck2jump)
+                local xd = x - _x
+                local yd = y - _y
+                --            print(xd, yd)
+                expect(math.floor(xd)).to.equal(71)
+                expect(math.floor(yd)).to.equal(34)
+                expect(z).to.equal(_z)
+                expect(hp).to.equal(_hp)
+            end)
+            it('P1 jumps after running diagonally', function()
+                --print(player1.name, player1.duck2jump.name)
+                player1.speed_x = player1.runSpeed_x
+                player1.speed_y = player1.runSpeed_y
+                local x, y, z, hp, _x, _y, _z, _hp = setStateAndWait(player1, player1.duck2jump)
+                local xd = x - _x
+                local yd = y - _y
+                --            print(xd, yd)
+                expect(math.floor(xd)).to.equal(105)
+                expect(math.floor(yd)).to.equal(21)
+                expect(z).to.equal(_z)
+                expect(hp).to.equal(_hp)
+            end)
+        end)
 
-    describe("Jump Method", function()
-        it('P1 jumps on place', function()
-            --print(player1.name, player1.duck2jump.name)
---            player1.speed_x = 0
-            local x, y, z, hp, _x, _y, _z, _hp = setStateAndWait(player1, player1.duck2jump)
-            expect(x).to.equal(_x)
-            expect(y).to.equal(_y)
-            expect(z).to.equal(_z)
-            expect(hp).to.equal(_hp)
-        end)
-    end)
-
-    describe("Combo Method", function()
-        it('P1 implicts 7HP damage to P2', function()
-            local res = checkComboDamage(player1, player2)
-            expect(res).to.equal(93)
-            print(player1.connectHit, player1.attacksPerAnimation)
-        end)
-        it('P1 cannot reach P2 (wrong facing)', function()
-            player1.face = -1
-            local res = checkComboDamage(player1, player2)
-            expect(res).to.equal(100)
-        end)
-        it('P1 cannot reach P3 (too far)', function()
-            local res = checkComboDamage(player1, player3)
-            expect(res).to.equal(100)
+        describe("Combo Method", function()
+            it('P1 implicts 7HP damage to P2', function()
+                local res = checkComboDamage(player1, player2)
+                expect(res).to.equal(93)
+                print(player1.connectHit, player1.attacksPerAnimation)
+            end)
+            it('P1 cannot reach P2 (wrong facing)', function()
+                player1.face = -1
+                local res = checkComboDamage(player1, player2)
+                expect(res).to.equal(100)
+            end)
+            it('P1 cannot reach P3 (too far)', function()
+                local res = checkComboDamage(player1, player3)
+                expect(res).to.equal(100)
+            end)
         end)
     end)
 end)
-
 
 --[[test("calcDistanceForSpeedAndFriction()",
     function()
