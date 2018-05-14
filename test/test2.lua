c63eeb096cd74c46946485115da10f011c2bdb58
D0NM <hyuBYSnONMVWgWDpCeV4>
2018-05-14 16:45:37 +0500

    refactor UT now using 1 function for all units: setStateAndWait

--- test/test2.lua
+++ test/test2.lua
@@ -1,6 +1,5 @@
 -- Copyright (c) .2018 SineDie
 
--- Copyright (c) .2018 SineDie
 local lust = require 'lib.test.lust.lust'
 local describe, it, expect = lust.describe, lust.it, lust.expect
 -- save DEBUG level
@@ -8,7 +7,7 @@ local _debugLevel = getDebugLevel()
 setDebugLevel(0)
 
 -- Calc the distance in pixels the unit can move in 1 second (60 FPS)
-local function calcDistanceForSpeedAndFriction(a)
+--[[local function calcDistanceForSpeedAndFriction(a)
     if not a then
         return
     end
@@ -56,109 +55,33 @@ local function calcDistanceForSpeedAndFriction(a)
     end
     print("Final x,y:", u.x, u.y, " Friction:", u.friction, " Name: ", u.name, u.id)
     --    print("Final speed_x, speed_y:", u.speed_x, u.speed_y)
-end
-
--- Test combo attacks connection
-local function checkComboAttackConnection(a, b, timeBetweenAttacks)
-    if not a or not b then
-        return
-    end
-    local FPS = 60
-    local time = 5
-    local dt = 1 / FPS
-    --    print("Start x,y:", u.x, u.y, u.name, u.id)
-    --    print("FPS:", FPS, " dt:", dt, " time:", time)
-    --    print("Unit-1 HP:", a.hp, " Unit-2 HP:", b.hp)
-    --    print("xy", a.x, a.y, "xy", b.x, b.y)
-    --    print(a.horizontal, a.face, b.horizontal, b.face)
-    --print("shape", a.shape, "sh2", b.shape)
-    --    print("Start speed_x, speed_y:", u.speed_x, u.speed_y)
-    local nextAttack = 0
-    for i = 1, time * FPS do
-        --        print(a.name, a.state, a.isHittable, " == ", b.name, b.state, b.isHittable)
-        nextAttack = nextAttack + dt
-        if nextAttack > (timeBetweenAttacks or 0.5) and a.state == "stand" then
-            print("!!", a.connectHit, a.attacksPerAnimation)
-            nextAttack = 0
-            a:setState(a.combo)
-        end
-
-        a:updateAI(dt)
-        a:update(dt)
-        if a.infoBar then
-            a.infoBar:update(dt)
-        end
-        b:updateAI(dt)
-        b:update(dt)
-        if b.infoBar then
-            b.infoBar:update(dt)
-        end
-        a:onHurt()
-        b:onHurt()
-    end
-    --    self.connectHit
-    --    self.attacksPerAnimation
-
-    --    print("Final Unit-1 HP:", a.hp, " Unit-2 HP:", b.hp)
-    return false
-end
+end]]
 
 local function setStateAndWait(a, setState, waitSeconds)
     local FPS = 60
-    local time = waitSeconds or 5
+    local time = waitSeconds or 3
     local dt = 1 / FPS
-    local x, y, z, hp, maxZ = a.x, a.y, a.z, a.hp, a.z
+    local x, y, z, hp = a.x, a.y, a.z, a.hp
+    a.maxZ = 0
     a:setState(setState)
     for i = 1, time * FPS do
-        if a.z > maxZ then
-            maxZ = a.z
+        stage:update(dt)
+        if a.z > a.maxZ then
+            a.maxZ = a.z
         end
-        a:updateAI(dt)
-        a:update(dt)
-        if a.infoBar then
-            a.infoBar:update(dt)
-        end
-        a:onHurt()
-        --        print(a.state)
     end
-    --    print(":", a.x, a.y, a.z, maxZ, a.hp, x, y, z, hp)
-    return a.x, a.y, a.z, maxZ, a.hp, x, y, z, hp
-end
-
--- Test 1 combo damage
-local function checkComboDamage(a, b)
-    if not a or not b then
-        return
-    end
-    local FPS = 60
-    local dt = 1 / FPS
-    a:updateAI(dt)
-    a:update(dt)
-    b:updateAI(dt)
-    b:update(dt)
-
-    a:setState(a.combo)
-
-    a:updateAI(dt)
-    a:update(dt)
-    b:updateAI(dt)
-    b:update(dt)
-    a:onHurt()
-    b:onHurt()
-    return b.hp
+--    print(":", a.x, a.y, a.z, a.hp, "MaxZ:" .. a.maxZ,  "<==", x, y, z, hp)
+    return a.x, a.y, a.z, a.maxZ, a.hp, x, y, z, hp
 end
 
 -- Start Unit Tests
 describe("Character Class", function()
     lust.before(function()
         -- This gets run before every test.
-        -- mock real lib functions
-        --local _SFXplay = SFX.play
-        --SFX.play = function() end
 
         -- prepare dummy stage
         stage = Stage:new()
-        stage:updateZStoppers(0.01)
+        --stage:updateZStoppers(0.01)
 
         local n
         -- prepare dummy player
@@ -167,8 +90,12 @@ describe("Character Class", function()
         player1.id = 1 -- fixed id
         player1:setOnStage(stage)
         player1:setState(player1.stand)
+        player1.face = 1
+        player1.horizontal = 1
         player1.x = 130
         player1.y = 200
+        player1.maxZ = player1.z
+        player1:checkCollisionAndMove(0.01)
         n = 2
         player2 = HEROES[n].hero:new("PL2-" .. HEROES[n][1].name, getSpriteInstance(HEROES[n].spriteInstance), DUMMY_CONTROL, 0, 0)
         player2.id = 2 -- fixed id
@@ -176,6 +103,8 @@ describe("Character Class", function()
         player2:setState(player2.stand)
         player2.x = 150
         player2.y = 200
+        player2.maxZ = player2.z
+        player2:checkCollisionAndMove(0.01)
         n = 3
         player3 = HEROES[n].hero:new("PL3-" .. HEROES[n][1].name, getSpriteInstance(HEROES[n].spriteInstance), DUMMY_CONTROL, 0, 0)
         player3.id = 3 -- fixed id
@@ -183,6 +112,11 @@ describe("Character Class", function()
         player3:setState(player3.stand)
         player3.x = 230
         player3.y = 200
+        player3.maxZ = player3.z
+        player3:checkCollisionAndMove(0.01)
+        -- mock real lib functions
+        --local _SFXplay = SFX.play
+        --SFX.play = function() end
     end)
     lust.after(function(txt)
         -- This gets run after every test.
@@ -193,7 +127,6 @@ describe("Character Class", function()
         --SFX.play = _SFXplay
     end)
     it('Function getDistanceBetweenPlayers', function()
-        --return min_x + (max_x - min_x) / 2, max_x - min_x, min_x, max_x
         local a, b, c, d = getDistanceBetweenPlayers()
         expect(b).to.equal(player3.x - player1.x)
         expect(a).to.equal(player1.x + (player3.x - player1.x) / 2)
@@ -201,10 +134,7 @@ describe("Character Class", function()
     describe("Rick Class", function()
         describe("Jump Method", function()
             it('Jumps on place', function()
-                --print(player1.name, player1.duck2jump.name)
-                --            player1.speed_x = 0
                 local x, y, z, maxZ, hp, _x, _y, _z, _hp = setStateAndWait(player1, player1.duck2jump)
---                print(xd, yd, maxZ)
                 expect(x).to.equal(_x)
                 expect(y).to.equal(_y)
                 expect(z).to.equal(_z)
@@ -212,13 +142,11 @@ describe("Character Class", function()
                 expect(hp).to.equal(_hp)
             end)
             it('Jumps after walking diagonally', function()
-                --print(player1.name, player1.duck2jump.name)
                 player1.speed_x = player1.walkSpeed_x
                 player1.speed_y = player1.walkSpeed_y
                 local x, y, z, maxZ, hp, _x, _y, _z, _hp = setStateAndWait(player1, player1.duck2jump)
                 local xd = x - _x
                 local yd = y - _y
---                print(xd, yd, maxZ)
                 expect(math.floor(xd)).to.equal(71)
                 expect(math.floor(yd)).to.equal(34)
                 expect(math.floor(maxZ)).to.equal(40)
@@ -226,13 +154,11 @@ describe("Character Class", function()
                 expect(hp).to.equal(_hp)
             end)
             it('Jumps after running diagonally', function()
-                --print(player1.name, player1.duck2jump.name)
                 player1.speed_x = player1.runSpeed_x
                 player1.speed_y = player1.runSpeed_y
                 local x, y, z, maxZ, hp, _x, _y, _z, _hp = setStateAndWait(player1, player1.duck2jump)
                 local xd = x - _x
                 local yd = y - _y
---                print(xd, yd, maxZ)
                 expect(math.floor(xd)).to.equal(105)
                 expect(math.floor(yd)).to.equal(21)
                 expect(math.floor(maxZ)).to.equal(40)
@@ -243,28 +169,26 @@ describe("Character Class", function()
 
         describe("Combo Method", function()
             it('P1 implicts 7HP damage to P2', function()
-                local res = checkComboDamage(player1, player2)
-                expect(res).to.equal(93)
+                setStateAndWait(player1, player1.combo)
+
+                expect(player2.hp).to.equal(93)
 --                print(player1.connectHit, player1.attacksPerAnimation)
             end)
             it('P1 cannot reach P2 (wrong facing)', function()
                 player1.face = -1
-                local res = checkComboDamage(player1, player2)
-                expect(res).to.equal(100)
+                setStateAndWait(player1, player1.combo)
+                expect(player2.hp).to.equal(100)
             end)
             it('P1 cannot reach P3 (too far)', function()
-                local res = checkComboDamage(player1, player3)
-                expect(res).to.equal(100)
+                setStateAndWait(player1, player1.combo)
+                expect(player3.hp).to.equal(100)
             end)
         end)
     end)
     describe("Chai Class", function()
         describe("Jump Method", function()
             it('Jumps on place', function()
-                --print(player1.name, player1.duck2jump.name)
-                --            player1.speed_x = 0
                 local x, y, z, maxZ, hp, _x, _y, _z, _hp = setStateAndWait(player3, player3.duck2jump)
---                print(xd, yd, maxZ)
                 expect(x).to.equal(_x)
                 expect(y).to.equal(_y)
                 expect(z).to.equal(_z)
@@ -272,13 +196,11 @@ describe("Character Class", function()
                 expect(hp).to.equal(_hp)
             end)
             it('Jumps after walking diagonally', function()
-                --print(player1.name, player1.duck2jump.name)
                 player3.speed_x = player3.walkSpeed_x
                 player3.speed_y = player3.walkSpeed_y
                 local x, y, z, maxZ, hp, _x, _y, _z, _hp = setStateAndWait(player3, player3.duck2jump)
                 local xd = x - _x
                 local yd = y - _y
---                print(xd, yd, maxZ)
                 expect(math.floor(xd)).to.equal(78)
                 expect(math.floor(yd)).to.equal(37)
                 expect(math.floor(maxZ)).to.equal(40)
@@ -286,13 +208,11 @@ describe("Character Class", function()
                 expect(hp).to.equal(_hp)
             end)
             it('Jumps after running diagonally', function()
-                --print(player1.name, player1.duck2jump.name)
                 player3.speed_x = player3.runSpeed_x
                 player3.speed_y = player3.runSpeed_y
                 local x, y, z, maxZ, hp, _x, _y, _z, _hp = setStateAndWait(player3, player3.duck2jump)
                 local xd = x - _x
                 local yd = y - _y
---                print(xd, yd, maxZ)
                 expect(math.floor(xd)).to.equal(112)
                 expect(math.floor(yd)).to.equal(22)
                 expect(math.floor(maxZ)).to.equal(40)
