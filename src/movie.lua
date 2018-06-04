fb10a428c129c872107522d2f06206f0e79e5f12
D0NM <hyuBYSnONMVWgWDpCeV4>
2018-06-05 02:55:12 +0500

    tweak movie to fade-in/out pictures only on their change (picture ref, quad, vScroll, hScroll)

--- src/movie.lua
+++ src/movie.lua
@@ -1,4 +1,5 @@
--- Date: 25.10.2016
+-- Copyright (c) .2018 SineDie
+
 local class = require "lib/middleclass"
 local Movie = class('Movie')
 
@@ -56,6 +57,7 @@ function Movie:initialize(frames)
     self.delayAfterFrame = frames.delayAfterFrame or 3
     self.bgColor = frames.bgColor or { 0, 0, 0 }
     self.time = 0 --self.frames[self.frame].delay
+    self.pictureTransparency = 0
     self.transparency = 0
     for i = 1, #frames do -- calc delay and text's x offset
         if not self.frames[i].delay then
@@ -70,21 +72,39 @@ function Movie:initialize(frames)
     end
 end
 
+function Movie:compareToFrame(n)
+    local f = self.frames[self.frame]
+    local f2 = self.frames[self.frame + n]
+    return f2 and f.slide == f2.slide
+        and f.q[1] == f2.q[1]
+        and f.q[2] == f2.q[2]
+        and f.q[3] == f2.q[3]
+        and f.q[4] == f2.q[4]
+        and f.hScroll == f2.hScroll
+        and f.vScroll == f2.vScroll
+end
+
 function Movie:update(dt)
     self.time = self.time + dt
     if self.b.attack:isDown() or love.mouse.isDown(1) then
         self.time = self.time + dt * 3 -- Speed Up
---        if self.b.attack:pressed() then
---            sfx.play("sfx", "menuMove")
---        end
     end
     if self.b.back:pressed() or self.b.jump:pressed() or love.mouse.isDown(2) then
-        --sfx.play("sfx", "menuCancel")
         return true -- Interrupt
     end
     if not self.frames or not self.frames[self.frame] then
         dp("Movie is empty")
         return true
+    end
+    if (self.time >= self.frames[self.frame].delay + self.delayAfterFrame and self.autoSkip)
+        or (self.time >= self.frames[self.frame].delay and self.b.attack:released())
+    then
+        self.frame = self.frame + 1
+        self.hScroll, self.vScroll = 0, 0
+        if not self.frames[self.frame] then
+            return true
+        end
+        self.time = 0
     end
     local timeToFadeout = self.frames[self.frame].delay + self.delayAfterFrame - 1
     if self.time <= 1 then
@@ -94,19 +114,10 @@ function Movie:update(dt)
     else
         self.transparency = 1
     end
-    if (self.time >= self.frames[self.frame].delay + self.delayAfterFrame and self.autoSkip)
-            or (self.time >= self.frames[self.frame].delay and self.b.attack:released())
-    then
---        if self.b.attack:released() or self.b.attack:pressed() then
---            sfx.play("sfx", "menuSelect")
---        end
-        self.frame = self.frame + 1
-        self.hScroll, self.vScroll = 0, 0
-        if not self.frames[self.frame] then
-            --dp("Movie ended on the enmpty frame "..self.frame)
-            return true
-        end
-        self.time = 0
+    if (self.time <= 1 and self:compareToFrame(-1)) or (self.time >= timeToFadeout and self:compareToFrame(1)) then
+        self.pictureTransparency = 1
+    else
+        self.pictureTransparency = self.transparency
     end
     local f = self.frames[self.frame]
     -- h/vScroll
@@ -128,24 +139,22 @@ function Movie:update(dt)
     return false
 end
 
-function Movie:draw(l, t, w, h)
+function Movie:draw(l, t, _w, _h)
     love.graphics.clear(unpack(self.bgColor))
-    -- Flick Perforations
-    love.graphics.setColor(255, 255, 255, 255 * self.transparency)
     local f = self.frames[self.frame]
     -- Show Picture
+    love.graphics.setColor(255, 255, 255, 255 * self.pictureTransparency)
     local w, h = f.q[3], f.q[4]
     local x, y = (screenWidth - w) / 2, (screenHeight - h) / 2
     local q = { r(f.q[1] + self.hScroll), r(f.q[2] + self.vScroll), w, h }
     q[5], q[6] = f.slide:getDimensions()
-
     love.graphics.draw(f.slide,
         love.graphics.newQuad(unpack(q)),
         l + x, t + y - screen_gap)
-    -- Text
+    -- Show Text
+    love.graphics.setColor(255, 255, 255, 255 * self.transparency)
     love.graphics.setFont(self.font)
     love.graphics.print(string.sub(f.text, 1, self.add_chars), l + f.x, r(t + y + h + slide_text_gap - screen_gap))
-
     if self.time >= self.frames[self.frame].delay and not self.autoSkip then
         love.graphics.setColor(255, 255, 255, 200 + 55 * math.sin(self.time * 2))
         love.graphics.print("PRESS ATTACK", r(screenWidth - 12 * 9), r(screenHeight - 12 + math.sin(self.time * 6)))
