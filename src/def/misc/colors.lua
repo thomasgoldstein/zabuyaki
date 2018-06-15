2b8925665493a4cd2e9235864ef25cff4bb91102
Mikhail Bratus <mikhail.bratus@gmail.com>
2018-06-15 22:24:27 +0200

    new D012 initial Colors class helper

---
+++ src/def/misc/colors.lua
@@ -0,0 +1,38 @@
+-- Copyright (c) .2018 SineDie
+
+local class = require "lib/middleclass"
+local Colors = class('Colors')
+
+function Colors:initialize()
+    self.c = {
+        ghostTraceColors = { {125, 150, 255, 175}, {25, 50, 255, 125 } }, -- RGBA, also the number of the ghosts
+        white = {255, 255, 255, 255},
+        black = {0, 0, 0, 255},
+        red = {255, 0, 0, 255},
+        green = {0, 255, 0, 255},
+        blue = {0, 0, 255, 255},
+    }
+end
+
+function Colors:get(name, index)
+    if index then
+        return self.c[name][index]
+    else
+        return self.c[name]
+    end
+end
+
+function Colors:unpack(...)
+    return unpack(self:get(...))
+end
+
+local tempColor
+function Colors:set(name, alpha)
+    tempColor = self.c[name]
+    if alpha then
+        tempColor[4] = alpha
+    end
+    love.graphics.setColor(unpack(tempColor))
+end
+
+return Colors
