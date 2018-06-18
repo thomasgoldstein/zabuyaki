b1875568c42b52814888bb5071eded6e8022cf2c
Mikhail Bratus <mikhail.bratus@gmail.com>
2018-06-18 21:28:34 +0200

    removed comments and dead code

--- src/compoPic.lua
+++ src/compoPic.lua
@@ -3,13 +3,7 @@
 
 local class = require "lib/middleclass"
 
-local function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
---	dp(x1,y1,w1,h1, x2,y2,w2,h2)
-    return x1 < x2+w2 and
-    x2 < x1+w1 and
-    y1 < y2+h2 and
-    y2 < y1+h1
-end
+local CheckCollision = CheckCollision
 
 local CompoundPicture = class('CompoundPicture')
 
@@ -18,17 +12,11 @@ function CompoundPicture:initialize(name, width, height)
     self.width = width
     self.height = height
     self.pics = {}
-    --dp(name..' '..self.width..'x'..self.height..' compoundPicture created')
 end
 
 function CompoundPicture:add(spriteSheet, quad, x, y, px, py, sx, sy, func)
     local _,_,w,h = quad:getViewport()
     table.insert(self.pics, {spriteSheet = spriteSheet, quad = quad, w = w, h = h, x = x or 0, y = y or 0, px = px or 0, py = py or 0, sx = sx or 0, sy = sy or 0, update = func})
-    --[[dp('rect '..self.pics[#self.pics].x ..' '..self.pics[#self.pics].y
-        ..' P:'..self.pics[#self.pics].px ..','..self.pics[#self.pics].py
-        ..' S:'..self.pics[#self.pics].sx ..','..self.pics[#self.pics].sy
-        ..' added to '..self.name)
-    ]]
 end
 
 function CompoundPicture:remove(rect)
@@ -47,7 +35,6 @@ end
 
 function CompoundPicture:getRect(i)
     if i then
-        --local _,_,w,h = self.pics[i].quad:getViewport()
         return self.pics[i].x, self.pics[i].y, self.pics[i].w, self.pics[i].h
     end
     -- Whole Picture rect
@@ -93,9 +80,6 @@ function CompoundPicture:drawAll()
     local l, t = 0, 0
     for i = 1, #self.pics do
         p = self.pics[i]
---		if p.update then
---			p.update(p, self.dt)
---		end
         love.graphics.draw(p.spriteSheet,
             p.quad,
             p.x + p.px * l, --parallax slow down
@@ -104,14 +88,10 @@ function CompoundPicture:drawAll()
 end
 
 function CompoundPicture:draw(l, t, w, h)
-    --love.graphics.setColor(0, 200, 130)
     local p
     for i = 1, #self.pics do
-        --		dp( CheckCollision( l,t,w,h, self:getRect(i) ) )
         p = self.pics[i]
---		if CheckCollision(l - p.px * l, t - p.py * t, w, h, self:getRect(i)) then
         if CheckCollision(l - p.px * l, t - p.py * t, w, h, p.x, p.y, p.w, p.h) then
-            --custom function(p, dt)
             if p.update then
                 p.update(p, self.dt)
             end
