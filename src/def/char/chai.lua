4aafb92fe3641459056b9f46b05846514fc31c2e
Thomas Goldstein <goldstein.thomas@gmail.com>
2018-06-30 13:26:28 +0200

    Tweak the frame delays of Chai's run animation

--- src/def/char/chai.lua
+++ src/def/char/chai.lua
@@ -273,14 +273,14 @@ return {
             delay = 0.117
         },
         run = {
-            { q = q(2,135,37,63), ox = 18, oy = 63 }, --run 1
-            { q = q(41,134,50,63), ox = 26, oy = 63 }, --run 2
+            { q = q(2,135,37,63), ox = 18, oy = 63, delay = 0.117 }, --run 1
+            { q = q(41,134,50,63), ox = 26, oy = 63, delay = 0.133 }, --run 2
             { q = q(93,134,46,64), ox = 25, oy = 63, func = stepFx }, --run 3
-            { q = q(2,201,37,63), ox = 18, oy = 63 }, --run 4
-            { q = q(41,200,49,64), ox = 24, oy = 63 }, --run 5
+            { q = q(2,201,37,63), ox = 18, oy = 63, delay = 0.117 }, --run 4
+            { q = q(41,200,49,64), ox = 24, oy = 63, delay = 0.133 }, --run 5
             { q = q(92,200,48,63), ox = 26, oy = 63, func = stepFx }, --run 6
             loop = true,
-            delay = 0.117
+            delay = 0.1
         },
         jump = {
             { q = q(43,266,39,67), ox = 26, oy = 65, delay = 0.15 }, --jump up
