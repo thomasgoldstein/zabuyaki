46aea59ca6806901a4202aeb8c0e31933a80fd82
Thomas Goldstein <goldstein.thomas@gmail.com>
2018-06-30 13:34:32 +0200

    Tweak the frame delays of Rick's run animation

--- src/def/char/rick.lua
+++ src/def/char/rick.lua
@@ -273,10 +273,10 @@ return {
         },
         run = {
             { q = q(2,136,42,60), ox = 13, oy = 59 }, --run 1
-            { q = q(46,134,48,62), ox = 18, oy = 61 }, --run 2
+            { q = q(46,134,48,62), ox = 18, oy = 61, delay = 0.12 }, --run 2
             { q = q(96,134,47,62), ox = 17, oy = 61, func = stepFx }, --run 3
             { q = q(2,200,41,60), ox = 12, oy = 59 }, --run 4
-            { q = q(45,198,48,61), ox = 18, oy = 61 }, --run 5
+            { q = q(45,198,48,61), ox = 18, oy = 61, delay = 0.12 }, --run 5
             { q = q(95,198,47,62), ox = 17, oy = 61, func = stepFx }, --run 6
             loop = true,
             delay = 0.1
