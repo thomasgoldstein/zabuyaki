61cda5a0faf1081263bfbc4d43174b32e74913e5
D0NM <hyuBYSnONMVWgWDpCeV4>
2018-07-30 01:03:06 +0500

    new D483 fix the jump start frame of Rick's Dragon Punch

--- src/def/char/rick.lua
+++ src/def/char/rick.lua
@@ -227,10 +227,12 @@ local specialOffensive1 = function(slf, cont) slf:checkAndAttack(
 local specialOffensiveFollowUp = function(slf, cont)
     if slf.isAttackConnected then
         slf:setSprite("specialOffensive2")
-        slf.speed_z = slf.jumpSpeed_z * 1.4
-        slf.z = slf:getMinZ() + 0.01
-        slf:showEffect("jumpStart")
     end
+end
+local specialOffensiveJumpStart = function(slf, cont)
+    slf.speed_z = slf.jumpSpeed_z * 1.4
+    slf.z = slf:getMinZ() + 0.01
+    slf:showEffect("jumpStart")
 end
 local specialOffensive2a = function(slf, cont) slf:checkAndAttack(
     { x = 10, y = 18, width = 40, height = 35, damage = 18, type = "knockDown" },
@@ -396,7 +398,7 @@ return {
             { q = q(104,2151,44,56), ox = 16, oy = 56, funcCont = specialOffensive2a }, --offensive special 5
             { q = q(150,2151,45,57), ox = 15, oy = 56, funcCont = specialOffensive2a }, --offensive special 6
             { q = q(2,2228,45,59), ox = 14, oy = 58 , funcCont = specialOffensive2b}, --offensive special 7
-            { q = q(49,2210,50,77), ox = 22, oy = 76, funcCont = specialOffensive2c, delay =  0.13 }, --offensive special 8a
+            { q = q(49,2210,50,77), ox = 22, oy = 76, funcCont = specialOffensive2c, func = specialOffensiveJumpStart, delay =  0.13 }, --offensive special 8a
             { q = q(101,2210,47,77), ox = 22, oy = 76, funcCont = specialOffensive2c, delay =  0.13 }, --offensive special 8b
             { q = q(150,2214,45,73), ox = 26, oy = 72, delay =  0.1 }, --offensive special 9
             { q = q(2,2289,38,70), ox = 21, oy = 69, delay =  0.1 }, --offensive special 10
