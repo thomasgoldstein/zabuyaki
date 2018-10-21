1bf4684408008fd15f430d88a513cf1a27484b19
Thomas Goldstein <stifu@free.fr>
2018-10-21 09:47:43 +0200

    Complete Rick's offensive special animation

--- src/def/char/rick.lua
+++ src/def/char/rick.lua
@@ -384,12 +384,13 @@ return {
             delay = 0.05
         },
         specialOffensive = {
-            { q = q(2,2433,47,52), ox = 13, oy = 51 },
-            { q = q(51,2430,46,55), ox = 6, oy = 54 },
-            { q = q(99,2427,46,58), ox = 7, oy = 57 },
-            { q = q(147,2425,58,60), ox = 12, oy = 59, funcCont = specialOffensive, delay = 0.03 },
-            { q = q(2,2487,50,61), ox = 11, oy = 60, delay = 0.03 },
-            { q = q(54,2490,40,58), ox = 11, oy = 57, delay = 0.3 },
+            { q = q(2,2433,47,52), ox = 14, oy = 51 }, --special offensive 1
+            { q = q(51,2430,46,55), ox = 7, oy = 54 }, --special offensive 2
+            { q = q(99,2427,41,58), ox = 8, oy = 57 }, --special offensive 3
+            { q = q(142,2425,59,60), ox = 14, oy = 59, funcCont = specialOffensive, delay = 0.03 }, --special offensive 4
+            { q = q(2,2487,50,61), ox = 12, oy = 60, delay = 0.03 }, --special offensive 5
+            { q = q(54,2489,41,59), ox = 13, oy = 58 }, --special offensive 6a
+            { q = q(97,2490,41,58), ox = 13, oy = 57, delay = 0.25 }, --special offensive 6b
             { q = q(138,779,46,63), ox = 18, oy = 62 }, --combo 4.7 (shifted right by 4px)
             delay = 0.05
         },
