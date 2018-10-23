e176154e7dcac60216e5f3f0b51236ca1c29524d
Thomas Goldstein <stifu@free.fr>
2018-10-23 07:46:15 +0200

    Tone down the first frame of the blue effect of Rick's offensive special

--- src/def/char/rick.lua
+++ src/def/char/rick.lua
@@ -387,7 +387,7 @@ return {
             { q = q(2,2433,47,52), ox = 14, oy = 51 }, --special offensive 1
             { q = q(51,2430,46,55), ox = 7, oy = 54 }, --special offensive 2
             { q = q(99,2427,41,58), ox = 8, oy = 57 }, --special offensive 3
-            { q = q(142,2425,64,60), ox = 14, oy = 59, funcCont = specialOffensive, delay = 0.03 }, --special offensive 4
+            { q = q(142,2425,61,60), ox = 14, oy = 59, funcCont = specialOffensive, delay = 0.03 }, --special offensive 4
             { q = q(2,2487,56,61), ox = 12, oy = 60, delay = 0.03 }, --special offensive 5
             { q = q(60,2489,45,59), ox = 18, oy = 58 }, --special offensive 6a
             { q = q(107,2490,44,58), ox = 17, oy = 57 }, --special offensive 6b
