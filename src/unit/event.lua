b8ac1f19c5c2e6f43fa1b0f39b371a0f8938a26b
Don Miguel <mikhail.bratus@gmail.com>
2018-11-27 19:31:47 +0500

    refactor renamed options->properties, do not disable event if it failed to apply movement to players

--- src/unit/event.lua
+++ src/unit/event.lua
@@ -4,33 +4,36 @@ local Event = class('Event', Unit)
 function Event:initialize(name, sprite, x, y, f, input)
     Unit.initialize(self, name, sprite, x, y, f, input)
     self.type = "event"
-    self.isDisabled = f.disabled
-    self.options = f
-    self.options.affect = self.options.affect or "first" --apply to the 1st collided player
+    self.properties = f
 end
 
 function Event:setOnStage(stage)
     stage.objects:add(self)
+    self.isDisabled = self.properties.disabled  -- disable Point events
 end
 
 local statesForGo = { walk = true, stand = true, run = true, duck = true, eventMove = true }
 function Event:checkForGo(player)
-    if self.options.go
+    if self.properties.go
         and statesForGo[player.state]
         and player.z <= player:getMinZ()
     then
         player:setState(player.eventMove, {
-            duration = self.options.duration,
-            animation = self.options.animation,
-            x = self.options.go.x,
-            y = self.options.go.y,
-            z = self.options.z
+            duration = self.properties.duration,
+            animation = self.properties.animation,
+            face = tonumber(self.properties.face),
+            x = self.properties.go.x,
+            y = self.properties.go.y,
+            z = self.properties.z
         })
+        return true
     end
+    return false
 end
 
 local collidedPlayer = {}
 function Event:updateAI(dt)
+    local wasApplied = false
     if self.isDisabled then
         return
     end
@@ -38,25 +41,25 @@ function Event:updateAI(dt)
     for i = 1, GLOBAL_SETTING.MAX_PLAYERS do
         local player = getRegisteredPlayer(i)
         if player and player:isAlive() then
-            if player.state == "walk" and self.shape:collidesWith(player.shape) then
+            if statesForGo[player.state] and self.shape:collidesWith(player.shape) then
                 collidedPlayer[#collidedPlayer+1] = player
             end
         end
     end
     if #collidedPlayer > 0 then
-        if self.options.affect == "first" then
-            self:checkForGo(collidedPlayer[1]) --1st detected player
-        elseif self.options.affect == "all" then --all alive players
+        if self.properties.move == "player" then
+            wasApplied = self:checkForGo(collidedPlayer[1]) --1st detected player
+        elseif self.properties.move == "players" then --all alive players
             for i = 1, GLOBAL_SETTING.MAX_PLAYERS do
                 local player = getRegisteredPlayer(i)
                 if player and player:isAlive() then
-                    self:checkForGo(player) --every alive walking player
+                    wasApplied = self:checkForGo(player) or wasApplied --every alive walking player
                 end
             end
         else
-            error("Unknown value of Event affect property")
+            error("Unknown value of Event affect property: "..tostring(self.properties.move))
         end
-        self.isDisabled = true
+        self.isDisabled = wasApplied
     end
 end
 
