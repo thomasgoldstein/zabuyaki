e55978bd4feddbf4581775211bdcd90ecae10d2a
Thomas Goldstein <stifu@free.fr>
2019-11-29 21:24:57 +0100

    Fix the repel_x of Rick's offensive special

--- src/def/char/rick.lua
+++ src/def/char/rick.lua
@@ -175,7 +175,7 @@ local specialDefensive4 = function(slf, cont) slf:checkAndAttack(
     cont
 ) end
 local specialOffensive = function(slf, cont) slf:checkAndAttack(
-    { x = 15, y = 37, width = 65, damage = 34, repel_x = slf.specialOffensiveRepel, type = "twist" },
+    { x = 15, y = 37, width = 65, damage = 34, repel_x = slf.specialOffensiveRepel_x, type = "twist" },
     cont
 ) end
 local specialDash1 = function(slf, cont)
