72c12e5359f64ac5ec46a5ed7ff40dbb211da00d
Thomas Goldstein <stifu@free.fr>
2019-12-03 23:57:58 +0100

    Slightly shorten Rick's legs during twist for consistency

--- src/def/char/rick.lua
+++ src/def/char/rick.lua
@@ -692,9 +692,9 @@ return {
         },
         fallTwist = {
             { q = q(2,2659,65,54), ox = 40, oy = 55 }, --fall twist 1
-            { q = q(129,2653,61,59), ox = 38, oy = 61 }, --fall twist 2
-            { q = q(69,2652,58,63), ox = 37, oy = 62 }, --fall twist 3
-            { q = q(192,2653,57,58), ox = 40, oy = 61 }, --fall twist 4
+            { q = q(69,2652,60,58), ox = 38, oy = 61 }, --fall twist 2
+            { q = q(131,2652,58,62), ox = 37, oy = 62 }, --fall twist 3
+            { q = q(191,2652,56,57), ox = 40, oy = 61 }, --fall twist 4
             loop = true,
             delay = f(5)
         },
