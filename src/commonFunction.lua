c23aec85444b545826980dc8db966a7cd25f84bd
Don Miguel <mikhail.bratus@gmail.com>
2019-12-13 23:24:23 +0500

    new function to check if there is Val in the given Table

--- src/commonFunction.lua
+++ src/commonFunction.lua
@@ -169,3 +169,12 @@ function delayWithSlowMotion(delay)
     end
     return delay
 end
+
+function contains(table, val)
+    for i=1, #table do
+        if table[i] == val then
+            return true
+        end
+    end
+    return false
+end
