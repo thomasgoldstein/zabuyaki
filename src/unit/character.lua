1af834b1849e67eeee973ffa86dbcae757dd6164
Don Miguel <mikhail.bratus@gmail.com>
2020-02-14 16:04:37 +0500

    new #D671 fallTwistStrong makes other units fall

--- src/unit/character.lua
+++ src/unit/character.lua
@@ -1018,7 +1018,7 @@ function Character:fallUpdate(dt)
         end
     end
     if self.speed_z < self.fallSpeed_z / 2 and self.bounced == 0
-        and ( self.condition == "throw" or self.condition == "twist" ) then
+        and ( self.condition == "throw" or ( self.condition == "twist" and self.condition2 >= 20 ) ) then
             self:checkAndAttack(
                 { x = 0, z = self:getHurtBoxHeight() / 2,
                   width = self:getHurtBoxWidth(),
