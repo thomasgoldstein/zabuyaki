6bbe397a502e27645d32d8f32e37e6ff069ab070
Don Miguel <mikhail.bratus@gmail.com>
2020-03-28 02:19:47 +0500

    tweak test looped step up back down forward ai patterns

--- src/unit/ai/experimental_ai.lua
+++ src/unit/ai/experimental_ai.lua
@@ -24,10 +24,30 @@ end
 function eAI:selectNewSchedule(conditions)
     local r
     if not self.currentSchedule or conditions.init then
-        self.currentSchedule = self.SCHEDULE_INTRO
-        --        print("GOPPER INTRO", self.unit.name, self.unit.id )
+        --self.currentSchedule = self.SCHEDULE_INTRO
+        --self.currentSchedule = self.SCHEDULE_STAND
+        self.currentSchedule = self.SCHEDULE_STEP_BACK
         return
     end
+
+    if self.currentSchedule == self.SCHEDULE_STEP_BACK then
+        self.currentSchedule = self.SCHEDULE_STEP_DOWN
+        print(self.unit.id, "change schedule SCHEDULE_STEP_DOWN")
+        return
+    elseif self.currentSchedule == self.SCHEDULE_STEP_DOWN then
+        self.currentSchedule = self.SCHEDULE_STEP_FORWARD
+        print(self.unit.id, "change schedule SCHEDULE_STEP_FORWARD")
+        return
+    elseif self.currentSchedule == self.SCHEDULE_STEP_FORWARD then
+        self.currentSchedule = self.SCHEDULE_STEP_UP
+        print(self.unit.id, "change schedule SCHEDULE_STEP_UP")
+        return
+    elseif self.currentSchedule == self.SCHEDULE_STEP_UP then
+        self.currentSchedule = self.SCHEDULE_STEP_BACK
+        print(self.unit.id, "change schedule SCHEDULE_STEP_BACK")
+        return
+    end
+
     if self.currentSchedule == self.SCHEDULE_ESCAPE_BACK then
         print(self.unit.id, "change schedule SCHEDULE_WAIT_LONG")
         self.currentSchedule = self.SCHEDULE_WAIT_LONG
@@ -36,69 +56,6 @@ function eAI:selectNewSchedule(conditions)
         self.currentSchedule = self.SCHEDULE_ESCAPE_BACK
         print(self.unit.id, "change schedule SCHEDULE_ESCAPE_BACK")
         return
-    end
-    if conditions.noPlayers then
-        self.currentSchedule = self.SCHEDULE_WALK_OFF_THE_SCREEN
-        return
-    end
-    if not conditions.cannotAct then
-        if self.currentSchedule ~= self.SCHEDULE_RUN_DASH
-            and conditions.canMove and conditions.tooFarToTarget
-            and love.math.random() < 0.25
-        then
-            self.currentSchedule = self.SCHEDULE_RUN_DASH
-            return
-        end
-        if conditions.canCombo then
-            --if conditions.canMove and conditions.tooCloseToPlayer then
-            --and love.math.random() < 0.5 then --and love.math.random() < 0.5
-            --self.currentSchedule = self.SCHEDULE_ESCAPE_BACK
-            --return
-            --end
-            self.currentSchedule = self.SCHEDULE_COMBO
-            return
-        end
-        --if conditions.canMove and conditions.tooCloseToPlayer then --and love.math.random() < 0.5
-        --    self.currentSchedule = self.SCHEDULE_ESCAPE_BACK
-        --    return
-        --end
-        if conditions.faceNotToPlayer then
-            self.currentSchedule = self.SCHEDULE_FACE_TO_PLAYER
-            return
-        end
-        --if self.currentSchedule ~= self.SCHEDULE_MEDIUM_WAIT and love.math.random() < self.waitChance then
-        --    self.currentSchedule = self.SCHEDULE_MEDIUM_WAIT
-        --    return
-        --end
-        if conditions.canMove and conditions.wokeUp or not conditions.noTarget then
-            r = love.math.random()
-            if r < 0.25 then
-                self.currentSchedule = self.SCHEDULE_WALK_AROUND
-            elseif r < 0.5 then
-                self.currentSchedule = self.SCHEDULE_GET_TO_BACK
-            elseif r < 0.75 then
-                self.currentSchedule = self.SCHEDULE_ATTACK_FROM_BACK
-            else
-                self.currentSchedule = self.SCHEDULE_WALK_CLOSE_TO_ATTACK
-            end
-            return
-        end
-        if not conditions.dead and not conditions.cannotAct and conditions.wokeUp then
-            if self.currentSchedule ~= self.SCHEDULE_STAND then
-                self.currentSchedule = self.SCHEDULE_STAND
-            else
-                self.currentSchedule = self.SCHEDULE_MEDIUM_WAIT
-            end
-            return
-        end
-    else
-        -- cannot control body
-        self.currentSchedule = self.SCHEDULE_RECOVER
-        return
-    end
-
-    if not self.currentSchedule then
-        self.currentSchedule = self.SCHEDULE_STAND
     end
 end
 
