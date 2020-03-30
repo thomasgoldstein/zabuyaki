59fa164248a6c4d22185069bcc73ffb94c5c3b2c
Don Miguel <mikhail.bratus@gmail.com>
2020-03-31 00:50:29 +0500

    refactor #D687 refactored prev code and implemented AI tactics lists of AI schedules

--- src/unit/ai/experimental_ai.lua
+++ src/unit/ai/experimental_ai.lua
@@ -12,70 +12,29 @@ local _settings = {
 function eAI:initialize(unit, settings)
     AI.initialize(self, unit, settings or _settings)
     -- new or overridden AI schedules
-    self.tactics = "passive" -- "passive", "aggressive", "cowardly"
+    -- new or overridden AI tactics lists of AI schedules
+    self.tacticsPassive = { self.SCHEDULE_STEP_BACK, self.SCHEDULE_STEP_DOWN, self.SCHEDULE_STEP_FORWARD, self.SCHEDULE_STEP_UP, self.SCHEDULE_MEDIUM_WAIT }
+    self.tacticsPassive.name = "passive"
+    self.tacticsAggressive = { self.SCHEDULE_WALK_CLOSE_TO_ATTACK, self.SCHEDULE_WAIT_LONG  }
+    self.tacticsAggressive.name = "aggressive"
+    self.tacticsCowardly = { self.SCHEDULE_WALK_OFF_THE_SCREEN, self.SCHEDULE_WAIT_SHORT }
+    self.tacticsCowardly.name = "cowardly"
+    self.tactics = self.tacticsPassive
 end
 
 function eAI:_update(dt)
-    --    if self.thinkInterval - dt <= 0 then
-    --        print(inspect(self.conditions, {depth = 1, newline ="", ident=""}))
-    --    end
     AI.update(self, dt)
 end
 
 function eAI:selectNewSchedule(conditions)
-    if self.tactics == "aggressive" and self.unit.hp < self.unit.maxHp / 3 then
-        self.tactics = "cowardly"
-    elseif self.tactics == "passive" and self.unit.hp < self.unit.maxHp then
-        self.tactics = "aggressive"
+    if self.tactics == self.tacticsPassive and self.unit.hp < self.unit.maxHp then
+        self.tactics = self.tacticsAggressive
+    elseif  self.tactics == self.tacticsAggressive and self.unit.hp < self.unit.maxHp / 3 then
+        self.tactics = self.tacticsCowardly
+    elseif not self.tactics then
+        self.tactics = self.tacticsPassive
     end
-
-    if self.tactics == "aggressive" then
-        print(self.unit.id, "change schedule SCHEDULE_WALK_CLOSE_TO_ATTACK")
-        self.currentSchedule = self.SCHEDULE_WALK_CLOSE_TO_ATTACK
-        return
-
-    elseif self.tactics == "passive"  then
-        if self.currentSchedule == self.SCHEDULE_STEP_BACK then
-            self.currentSchedule = self.SCHEDULE_STEP_DOWN
-            print(self.unit.id, "change schedule SCHEDULE_STEP_DOWN")
-            return
-        elseif self.currentSchedule == self.SCHEDULE_STEP_DOWN then
-            self.currentSchedule = self.SCHEDULE_STEP_FORWARD
-            print(self.unit.id, "change schedule SCHEDULE_STEP_FORWARD")
-            return
-        elseif self.currentSchedule == self.SCHEDULE_STEP_FORWARD then
-            self.currentSchedule = self.SCHEDULE_STEP_UP
-            print(self.unit.id, "change schedule SCHEDULE_STEP_UP")
-            return
-        elseif self.currentSchedule == self.SCHEDULE_STEP_UP then
-            self.currentSchedule = self.SCHEDULE_STEP_BACK
-            print(self.unit.id, "change schedule SCHEDULE_STEP_BACK")
-            return
-        end
-
-    else -- "cowardly"
-        print(self.unit.id, "change schedule SCHEDULE_WALK_OFF_THE_SCREEN")
-        self.currentSchedule = self.SCHEDULE_WALK_OFF_THE_SCREEN
-        return
-    end
-
-    if not self.currentSchedule or conditions.init then
-        --self.currentSchedule = self.SCHEDULE_INTRO
-        --self.currentSchedule = self.SCHEDULE_STAND
-        print(self.unit.id, "change schedule SCHEDULE_STEP_BACK")
-        self.currentSchedule = self.SCHEDULE_STEP_BACK
-        return
-    end
-
-    if self.currentSchedule == self.SCHEDULE_ESCAPE_BACK then
-        print(self.unit.id, "change schedule SCHEDULE_WAIT_LONG")
-        self.currentSchedule = self.SCHEDULE_WAIT_LONG
-        return
-    else
-        self.currentSchedule = self.SCHEDULE_ESCAPE_BACK
-        print(self.unit.id, "change schedule SCHEDULE_ESCAPE_BACK")
-        return
-    end
+    self.currentSchedule = self.tactics[ love.math.random(1, #self.tactics ) ]
 end
 
 return eAI
