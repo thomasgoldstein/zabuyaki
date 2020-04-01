f7fbcf63943015dd313c2712509790cb11a29251
Don Miguel <mikhail.bratus@gmail.com>
2020-04-01 22:00:23 +0500

    refactor speed up

--- src/ai/schedule.lua
+++ src/ai/schedule.lua
@@ -6,11 +6,9 @@ function Schedule:initialize(tasks, interrupts)
     self.interrupts = {}
     self.currentTask = 1
     self.done = false
-    for _, task in ipairs(tasks) do
-        self:addTask(task)
-    end
-    for _, interrupt in pairs(interrupts) do
-        self:addInterrupt(interrupt)
+    for i = 1, math.max(#tasks, #interrupts) do
+        self:addTask(tasks[i])
+        self:addInterrupt(interrupts[i])
     end
     return self
 end
@@ -26,11 +24,13 @@ function Schedule:stop()
 end
 
 function Schedule:addTask(taskFunc)
+    if not taskFunc then return end
     assert(taskFunc and type(taskFunc) == "function", "argument should be a function")
     self.tasks[#self.tasks + 1] = taskFunc
 end
 
 function Schedule:addInterrupt(interruptStr)
+    if not interruptStr then return end
     assert(interruptStr and type(interruptStr) == "string", "argument should be a string with interrupt condition name")
     self.interrupts[interruptStr] = true
 end
@@ -53,16 +53,12 @@ function Schedule:isDone(conditions)
 end
 
 function Schedule:update(env, dt)
-    if self.done then
-        return false
-    end
-    if #self.tasks < 1 then
+    if self.done or #self.tasks < 1 then
         return false
     end
     if self.tasks[self.currentTask](env, dt) then --if func returns true, delete this from the que
         self.currentTask = self.currentTask + 1
-
-        if self.currentTask > #self.tasks then -- -1
+        if self.currentTask > #self.tasks then
             self:stop()
         end
         return true
