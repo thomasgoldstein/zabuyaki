5f9b4fff00604d622295717133c973719205553b
Thomas Goldstein <stifu@free.fr>
2020-04-03 23:01:42 +0200

    Add new Zveta palette

--- src/def/misc/shaders.lua
+++ src/def/misc/shaders.lua
@@ -173,12 +173,19 @@ local svetaColors_original = {
 local svetaColors_1 = {
     { 134, 146, 169, 255 }, { 51, 69, 122, 255 }, { 20, 34, 62, 255 }, -- blue-gray vest
     { 62, 52, 56, 255 }, { 28, 28, 28, 255 }, { 8, 8, 8, 255 } } -- black pants
+local svetaColors_2 = svetaColors_1 -- TODO
+local svetaColors_3 = svetaColors_1 -- TODO
 
 local zeenaColors_original = svetaColors_original
 local zeenaColors_1 = {
     { 247, 182, 193, 255 }, { 189, 96, 151, 255 }, { 77, 24, 101, 255 }, -- pink vest
     { 62, 52, 56, 255 }, { 28, 28, 28, 255 }, { 8, 8, 8, 255 }, -- black pants
     { 38, 61, 93, 255 }, { 7, 12, 72, 255 }, { 7, 5, 44, 255 } } -- dark blue hair
+local zeenaColors_2 = {
+    { 62, 52, 56, 255 }, { 28, 28, 28, 255 }, { 8, 8, 8, 255 }, -- black vest
+    { 62, 52, 56, 255 }, { 28, 28, 28, 255 }, { 8, 8, 8, 255 }, -- black pants
+    { 160, 89, 32, 255 }, { 107, 41, 21, 255 }, { 40, 19, 7, 255 } } -- brown hair
+local zeenaColors_3 = zeenaColors_2 -- TODO
 
 local satoffColors_original = {
     { 228, 33, 35, 255 }, { 135, 12, 30, 255 }, { 51, 8, 9, 255 }, -- red suit
@@ -310,13 +317,13 @@ function reloadShaders()
     }
     shaders.sveta = {
         swapColors(svetaColors_original, svetaColors_1),
-        swapColors(svetaColors_original, svetaColors_1),
-        swapColors(svetaColors_original, svetaColors_1),
+        swapColors(svetaColors_original, svetaColors_2),
+        swapColors(svetaColors_original, svetaColors_3),
     }
     shaders.zeena = {
         swapColors(zeenaColors_original, zeenaColors_1),
-        swapColors(zeenaColors_original, zeenaColors_1),
-        swapColors(zeenaColors_original, zeenaColors_1),
+        swapColors(zeenaColors_original, zeenaColors_2),
+        swapColors(zeenaColors_original, zeenaColors_3),
     }
     shaders.beatnik = {
         nil,
