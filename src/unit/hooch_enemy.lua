c36bf9357940b3ff02155d5ab1bf1e94404a9f69
Don Miguel <mikhail.bratus@gmail.com>
2020-04-27 12:48:52 +0500

    fix #699 crash on enemy Hooch combo

--- src/unit/hooch_enemy.lua
+++ src/unit/hooch_enemy.lua
@@ -24,6 +24,13 @@ function Hooch:initAttributes()
     self.dashSpeed_x = 150 --speed of the character during dash attack
     self.dashRepel_x = 180 --how much the dash attack repels other units
     self.dashFriction = self.dashSpeed_x
+
+    self.comboSlideSpeed3_x = 130 --horizontal speed of combo3Forward attacks
+    self.comboSlideDiagonalSpeed3_x = 100 --diagonal horizontal speed of combo3Forward attacks
+    self.comboSlideDiagonalSpeed3_y = 13 --diagonal vertical speed of combo3Forward attacks
+    self.comboSlideSpeed3_z = 30 --jump speed of combo3Forward attacks
+    self.comboSlideRepel3_x = 246 --how much combo3Forward pushes units back (high value to make up for the jump that ignores friction)
+
     --    self.throwSpeed_x = 220 --my throwing speed
     --    self.throwSpeed_z = 200 --my throwing speed
     self.myThrownBodyDamage = 10  --DMG (weight) of my thrown body that makes DMG to others
