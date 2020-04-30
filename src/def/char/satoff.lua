83d445687e3dcb325961b39ad2c09055e510727e
Don Miguel <mikhail.bratus@gmail.com>
2020-04-30 23:07:29 +0500

    tweak Staoff's sideStep anim frames order (rolling upward)

--- src/def/char/satoff.lua
+++ src/def/char/satoff.lua
@@ -94,10 +94,10 @@ return {
         },
         sideStepUp = {
             { q = q(121,424,58,58), ox = 31, oy = 59, delay = f(3) }, --jump attack forward 2 (lowered)
-            { q = q(2,809,56,43), ox = 33, oy = 40, funcCont = rollAttack }, --run 1
-            { q = q(60,806,50,46), ox = 32, oy = 43, funcCont = rollAttack }, --run 2
-            { q = q(112,809,53,43), ox = 32, oy = 40, funcCont = rollAttack }, --run 3
             { q = q(167,799,52,53), ox = 31, oy = 50, funcCont = rollAttack }, --run 4
+            { q = q(112,809,53,43), ox = 32, oy = 40, funcCont = rollAttack }, --run 3
+            { q = q(60,806,50,46), ox = 32, oy = 43, funcCont = rollAttack }, --run 2
+            { q = q(2,809,56,43), ox = 33, oy = 40, funcCont = rollAttack }, --run 1
             loop = true,
             loopFrom = 2,
             delay = f(6)
