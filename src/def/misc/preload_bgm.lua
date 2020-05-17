aee529b08772dd854ba3d7ed9d562fbd14cb99ce
Don Miguel <mikhail.bratus@gmail.com>
2020-05-17 17:29:35 +0500

    new music control functions

--- src/def/misc/preload_bgm.lua
+++ src/def/misc/preload_bgm.lua
@@ -1,9 +1,29 @@
 local BGM = {}
 
+local musicVolume = GLOBAL_SETTING.BGM_VOLUME
+local currentMusic = nil
+
 BGM.load = function(alias, filePath, copyright)
     BGM[alias] = filePath
     local _, fileName, _ = string.match(filePath, "(.-)([^\\/]-%.?([^%.\\/]*))$")
     BGM[#BGM + 1] = {filePath = filePath, alias = alias, fileName = fileName, copyright = copyright or "SubspaceAudio" }
+end
+
+BGM.setVolume = function(_musicVolume)
+    musicVolume = _musicVolume or GLOBAL_SETTING.BGM_VOLUME
+    if currentMusic then
+        currentMusic:setVolume(musicVolume)
+    end
+end
+
+BGM.play = function(_music)
+    currentMusic = nil
+end
+
+BGM.stop = function()
+    if currentMusic and currentMusic:isPlaying() then
+        currentMusic:stop()
+    end
 end
 
 --BGM.level01 = BGM.test
