b8318e8562c233af3499b10e40699cc1096d49f9
Mikhail Bratus <mikhail.bratus@gmail.com>
2020-08-20 15:05:29 +0500

    new units damage tracking methods

---
+++ src/unit/ui_fx/unit_damage.lua
@@ -0,0 +1,18 @@
+local Unit = Unit
+
+local clamp = clamp
+local CheckCollision = CheckCollision
+
+function Unit:initDamage()
+    self.isHurt = nil
+end
+
+function Unit:trackDamage(damage)
+
+end
+
+function Unit:getDamage(damage)
+
+end
+
+
