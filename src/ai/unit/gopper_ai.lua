cb0652a0d5aeb542ef4451f959fb258f2cb2d1b7
Don Miguel <mikhail.bratus@gmail.com>
2020-11-01 14:49:26 +0500

    twaek Gopper AI

--- src/ai/unit/gopper_ai.lua
+++ src/ai/unit/gopper_ai.lua
@@ -2,28 +2,45 @@ local class = require "lib/middleclass"
 local eAI = class('eAI', AI)
 
 local _settings = {
+    maxSwitchingSchedulesCounter = 4,
     thinkIntervalMin = 0.2,
     thinkIntervalMax = 0.35,
-    hesitateMin = 0.1,
-    hesitateMax = 0.3,
+    hesitateMin = 0.2,
+    hesitateMax = 0.5,
     waitChance = 0.34
 }
 
 function eAI:initialize(unit, settings)
     AI.initialize(self, unit, settings or _settings)
     -- new or overridden AI schedules
+    self.tacticsWalkAround = {
+        --self.SCHEDULE_ATTACK_FROM_BACK,
+        --self.SCHEDULE_WALK_CLOSE_TO_ATTACK,
+        self.SCHEDULE_WALK_RANDOM,
+        self.SCHEDULE_WALK_AROUND,
+        self.SCHEDULE_GET_TO_BACK,
+        self.SCHEDULE_WALK_BY_TARGET_V,
+        self.SCHEDULE_WALK_BY_TARGET_H,
+        self.SCHEDULE_WAIT_SHORT,
+        --self.SCHEDULE_ESCAPE_BACK,    --
+        self.SCHEDULE_WALK_TO_MEDIUM_DISTANCE,
+        self.SCHEDULE_WALK_TO_SHORT_DISTANCE,
+        self.SCHEDULE_WALKING_SPEED_UP,
+        self.SCHEDULE_WALKING_SPEED_DOWN,
+        --self.SCHEDULE_WALK_OVER_TO_MEDIUM_DISTANCE,   --
+        --self.SCHEDULE_WALK_OVER_TO_LONG_DISTANCE,  ---
+    }
+    self.tacticsWalkToAttack = {
+        self.SCHEDULE_ATTACK_FROM_BACK,
+        self.SCHEDULE_WALK_CLOSE_TO_ATTACK,
+        self.SCHEDULE_WALKING_SPEED_UP,
+    }
 end
 
 function eAI:selectNewSchedule(conditions)
-    local r
     self.unit.b.reset()
     if not self.currentSchedule or conditions.init then
         self:setSchedule( self.SCHEDULE_INTRO )
---        print("GOPPER INTRO", self.unit.name, self.unit.id )
-        return
-    end
-    if conditions.noPlayers then
-        self:setSchedule( self.SCHEDULE_WALK_OFF_THE_SCREEN )
         return
     end
     if not conditions.cannotAct then
@@ -34,39 +51,21 @@ function eAI:selectNewSchedule(conditions)
             self:setSchedule( self.SCHEDULE_RUN_DASH_ATTACK )
             return
         end
-        if conditions.canCombo then
-            --if conditions.canMove and conditions.tooCloseToPlayer then
-                    --and love.math.random() < 0.5 then --and love.math.random() < 0.5
-                --self:setSchedule( self.SCHEDULE_STEP_BACK )
-                --return
-            --end
-            self:setSchedule( self.SCHEDULE_COMBO )
-            return
-        end
-        --if conditions.canMove and conditions.tooCloseToPlayer then --and love.math.random() < 0.5
-        --    self:setSchedule( self.SCHEDULE_STEP_BACK )
-        --    return
-        --end
         if conditions.faceNotToPlayer then
             self:setSchedule( self.SCHEDULE_FACE_TO_PLAYER )
             return
         end
-        --if self.currentSchedule ~= self.SCHEDULE_WAIT_MEDIUM and love.math.random() < self.waitChance then
-        --    self:setSchedule( self.SCHEDULE_WAIT_MEDIUM )
-        --    return
-        --end
         if conditions.canMove and conditions.wokeUp or not conditions.noTarget then
-            r = love.math.random()
-            if r < 0.25 then
-                self:setSchedule( self.SCHEDULE_WALK_AROUND )
-            elseif r < 0.5 then
-                self:setSchedule( self.SCHEDULE_GET_TO_BACK )
-            elseif r < 0.75 then
-                self:setSchedule( self.SCHEDULE_ATTACK_FROM_BACK )
+            self.switchingSchedulesCounter = self.switchingSchedulesCounter + 1
+            if self.switchingSchedulesCounter < self.maxSwitchingSchedulesCounter then
+                self:setSchedule( self.tacticsWalkAround[ love.math.random(1, #self.tacticsWalkAround ) ])
+                return
             else
-                self:setSchedule( self.SCHEDULE_WALK_CLOSE_TO_ATTACK )
+                self.switchingSchedulesCounter = love.math.random( math.floor(0, self.maxSwitchingSchedulesCounter / 3) )
+                self:setSchedule( self.tacticsWalkToAttack[ love.math.random(1, #self.tacticsWalkToAttack ) ])
+                return
             end
-            return
+
         end
         if not conditions.dead and not conditions.cannotAct and conditions.wokeUp then
             if self.currentSchedule ~= self.SCHEDULE_STAND then
