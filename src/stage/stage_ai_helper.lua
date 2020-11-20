b0dbf44dd6b412c72a1b137c59d50d8894c77667
Don Miguel <mikhail.bratus@gmail.com>
2020-11-20 23:17:19 +0500

    refactor replace safePos with walkableArea data methods

--- src/stage/stage_ai_helper.lua
+++ src/stage/stage_ai_helper.lua
@@ -1,24 +1,44 @@
 -- collect data to help AI choose right patterns and places
-
 local Stage = Stage
 
-local safePlace = {}
-local safePlacePos = 1
-local safePlacePosMax = 200
-local safePlacePosReset = false
-local logEveryFrame = 60
+local logEveryFrame = 15
+-- Walkable area
+local walkableGridSize = 8
+local walkableAreaTop = {}
+local walkableAreaBottom = {}
 
 function Stage:initLog()
-    safePlace = {}
-    safePlacePos = 1
-    safePlacePosReset = false
-    for i = 1, safePlacePosMax do
-        safePlace[ i ] = { x = nil, y = nil }
+    walkableAreaTop = {}
+    walkableAreaBottom = {}
+    -- Calc initial walkable area
+    local bottom_y = self.bottomStopper.y -  self.bottomStopper.depth / 2 - 20
+    local top_y = bottom_y - 240/3 + 40
+    --print(top_y, bottom_y)
+    for x = 1, self.worldWidth, walkableGridSize do
+        local _x = math.floor( x / walkableGridSize )
+        rawset(walkableAreaTop, _x, top_y)
+        rawset(walkableAreaBottom, _x, bottom_y)
+        --print(_x, top_y, bottom_y)
     end
 end
 
+function Stage:getWalkableAreaTopAndBottomY( x, unit )
+    local _x = math.floor( x / walkableGridSize )
+    local maxTop = rawget(walkableAreaTop, _x)
+    local maxBottom = rawget(walkableAreaBottom, _x)
+    return maxTop or unit.y, maxBottom or unit.y
+end
+
+function Stage:clampWalkableAreaY( x, y )
+    local _x = math.floor( x / walkableGridSize )
+    local maxTop = rawget(walkableAreaTop, _x)
+    local maxBottom = rawget(walkableAreaBottom, _x)
+    return clamp( y, maxTop, maxBottom )
+end
+
 function Stage:logUnit( unit )
-    if unit.isDisabled or unit.z > 1 then --r unit.x <= this.leftStopper:getX() then
+    local x
+    if unit.isDisabled or unit.z > 1 or unit.x < walkableGridSize or unit.x > self.worldWidth then --r unit.x <= this.leftStopper:getX() then
         -- keep z > 1 to include start of jumps
         return
     end
@@ -31,24 +51,15 @@ function Stage:logUnit( unit )
         unit.logged = unit.logged - 1
         return
     end
-    safePlacePos = safePlacePos + 1
-    if safePlacePos > safePlacePosMax then
-        safePlacePos = 1
-        safePlacePosReset = true
+    local _x = math.floor( unit.x / walkableGridSize )
+    maxTop = rawget(walkableAreaTop, _x)
+    local maxBottom = rawget(walkableAreaBottom, _x)
+    if maxTop > unit.y then
+        rawset(walkableAreaTop, _x, unit.y)
+        --print(_x, "set TOP", unit.y)
     end
-    local s = safePlace[ safePlacePos ]
-    s.x, s.y = unit.x, unit.y
-end
-
-function Stage:getRandomSafePoint()
-    local s
-    if safePlacePosReset then
-        s = safePlace[ love.math.random(1, safePlacePosMax ) ]  -- the buffer is full
-    else
-        s = safePlace[ love.math.random(1, safePlacePos ) ]
+    if maxBottom < unit.y then
+        rawset(walkableAreaBottom, _x, unit.y)
+        --print(_x, "set BOT", unit.y)
     end
-    if not s then
-        return nil
-    end
-    return s.x, s.y
 end
