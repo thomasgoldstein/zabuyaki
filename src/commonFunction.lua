5a1ab29354eca862f691df30ab459d07fbefb2a3
Don Miguel <mikhail.bratus@gmail.com>
2020-11-24 21:19:19 +0500

    add pure Lua 5.1 bitwise operations blogged by RiciLake

--- src/commonFunction.lua
+++ src/commonFunction.lua
@@ -187,3 +187,17 @@ function splitString(str)
     end
     return t
 end
+-- bitwise operations (pure Lua 5.1 bitwise operations blogged by RiciLake)
+function bit(p)
+    return 2 ^ (p - 1)  -- 1-based indexing
+end
+-- Typical call:  if hasbit(x, bit(3)) then ...
+function hasbit(x, p)
+    return x % (p + p) >= p
+end
+function setbit(x, p)
+    return hasbit(x, p) and x or x + p
+end
+function clearbit(x, p)
+    return hasbit(x, p) and x - p or x
+end
