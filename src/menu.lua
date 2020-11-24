251faa2eb2c106607def5e09de8666d1c074eeee
Don Miguel <mikhail.bratus@gmail.com>
2020-11-24 20:00:19 +0500

    refactor allow import extra menu params

--- src/menu.lua
+++ src/menu.lua
@@ -1,14 +1,16 @@
-local screenWidth = 640
-local screenHeight = 480
-local menuItem_h = 40
-local menuOffset_y = 200 - menuItem_h
-local menuOffset_x = 0
-local hintOffset_y = 80
-local leftItemOffset  = 6
-local topItemOffset  = 6
-
-function fillMenu(txtItems, hintsText)
-    local m = {}
+function fillMenu(txtItems, hintsText, optionalParams)
+    local m = {
+        params = optionalParams or {
+            screenWidth = 640,
+            screenHeight = 480,
+            menuItem_h = 40,
+            menuOffset_y = 200 - 40, -- - menuItem_h
+            menuOffset_x = 0,
+            hintOffset_y = 80,
+            leftItemOffset = 6,
+            topItemOffset = 6,
+        }
+    }
     local maxItemWidth, maxItem_x = 8, 0
     if not hintsText then
         hintsText = {}
@@ -16,7 +18,7 @@ function fillMenu(txtItems, hintsText)
     for i = 1, #txtItems do
         local w = gfx.font.arcade4:getWidth(txtItems[i])
         if w > maxItemWidth then
-            maxItem_x = menuOffset_x + screenWidth / 2 - w / 2
+            maxItem_x = m.params.menuOffset_x + m.params.screenWidth / 2 - w / 2
             maxItemWidth = w
         end
     end
@@ -25,13 +27,13 @@ function fillMenu(txtItems, hintsText)
         m[#m + 1] = {
             item = txtItems[i],
             hint = hintsText[i] or "",
-            x = menuOffset_x + screenWidth / 2 - w / 2,
-            y = menuOffset_y + i * menuItem_h,
+            x = m.params.menuOffset_x + m.params.screenWidth / 2 - w / 2,
+            y = m.params.menuOffset_y + i * m.params.menuItem_h,
             rect_x = maxItem_x,
             w = maxItemWidth,
             h = gfx.font.arcade4:getHeight(txtItems[i]),
-            wx = (screenWidth - gfx.font.arcade4:getWidth(hintsText[i] or "")) / 2,
-            wy = screenHeight - hintOffset_y,
+            wx = (m.params.screenWidth - gfx.font.arcade4:getWidth(hintsText[i] or "")) / 2,
+            wy = m.params.screenHeight - m.params.hintOffset_y,
             n = 1
         }
     end
@@ -39,13 +41,12 @@ function fillMenu(txtItems, hintsText)
 end
 
 function calcMenuItem(menu, i)
-    assert(menu and menu[i], "menu item error")
     local m = menu[i]
     m.w = gfx.font.arcade4:getWidth(m.item)
     m.h = gfx.font.arcade4:getHeight(m.item)
-    m.wy = screenHeight - hintOffset_y
-    m.x = menuOffset_x + screenWidth / 2 - m.w / 2
-    m.y = menuOffset_y + i * menuItem_h
-    m.rect_x = menuOffset_x + screenWidth / 2 - m.w / 2
-    m.wx = (screenWidth - gfx.font.arcade4:getWidth(m.hint)) / 2
+    m.wy = menu.params.screenHeight - menu.params.hintOffset_y
+    m.x = menu.params.menuOffset_x + menu.params.screenWidth / 2 - m.w / 2
+    m.y = menu.params.menuOffset_y + i * menu.params.menuItem_h
+    m.rect_x = menu.params.menuOffset_x + menu.params.screenWidth / 2 - m.w / 2
+    m.wx = (menu.params.screenWidth - gfx.font.arcade4:getWidth(m.hint)) / 2
 end
