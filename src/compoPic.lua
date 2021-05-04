e4d9cf18635f4c81135eaa4a74d591c8d4822708
Mikhail Bratus <Mikhail.Bratus@GMail.com>
2021-05-04 22:04:52 +0500

    refactor to be able to operate of single images (not the whole batch)

--- src/compoPic.lua
+++ src/compoPic.lua
@@ -108,66 +108,30 @@ function CompoundPicture:getRect(i)
 end
 
 function CompoundPicture:update(dt)
-    local p, a
     for i=1, #self.pics do
-        p = self.pics[i]
-        a = p.animate
-        if a then
-            a.elapsedTime = a.elapsedTime + dt
-            if a.elapsedTime >= a.delay[a.curFrame] then
-                a.elapsedTime = 0
-                a.curFrame = a.curFrame + 1
-                if a.curFrame > a.maxFrame then
-                    a.curFrame = 1
-                end
-            end
-        end
-        -- scroll horizontally e.g. clouds
-        if p.scrollSpeedX and p.scrollSpeedX ~= 0 then
-            p.x = p.x + (p.scrollSpeedX * dt)
-            if p.scrollSpeedX > 0 then
-                if p.x > self.width then
-                    p.x = -p.w
-                end
-            else
-                if p.x + p.w < 0 then
-                    p.x = self.width
-                end
-            end
-        end
-        -- scroll vertically
-        if p.scrollSpeedY and p.scrollSpeedY ~= 0 then
-            p.y = p.y + (p.scrollSpeedY * dt)
-            if p.scrollSpeedY > 0 then
-                if p.y > self.height then
-                    p.y = -p.h
-                end
-            else
-                if p.y + p.h < 0 then
-                    p.y = self.height
-                end
-            end
+        self:updateOne(self.pics[i], dt)
+    end
+end
+
+function CompoundPicture:drawOne(p, l, t, w, h, drawReflections)
+    if p.reflect == drawReflections and CheckCollision(l - p.relativeX * l, t - p.relativeY * t, w, h, p.x, p.y, p.w, p.h) then
+        if p.animate then
+            love.graphics.draw(p.image,
+                p.animate.quads[ p.animate.frame[p.animate.curFrame] ],
+                round(p.x + p.relativeX * l), -- slow down parallax
+                round(p.y + p.relativeY * t))
+        else
+            love.graphics.draw(p.image,
+                p.quad,
+                round(p.x + p.relativeX * l), -- slow down parallax
+                round(p.y + p.relativeY * t))
         end
     end
 end
 
 function CompoundPicture:draw(l, t, w, h, drawReflections)
-    local p
     for i = 1, #self.pics do
-        p = self.pics[i]
-        if p.reflect == drawReflections and CheckCollision(l - p.relativeX * l, t - p.relativeY * t, w, h, p.x, p.y, p.w, p.h) then
-            if p.animate then
-                love.graphics.draw(p.image,
-                    p.animate.quads[ p.animate.frame[p.animate.curFrame] ],
-                    round(p.x + p.relativeX * l), -- slow down parallax
-                    round(p.y + p.relativeY * t))
-            else
-                love.graphics.draw(p.image,
-                    p.quad,
-                    round(p.x + p.relativeX * l), -- slow down parallax
-                    round(p.y + p.relativeY * t))
-            end
-        end
+        self:drawOne(self.pics[i], l, t, w, h, drawReflections)
     end
 end
 
