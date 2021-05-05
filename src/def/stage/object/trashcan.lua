35593455ad04e9c95f9755ed709bc2354b1d1ad4
Mikhail Bratus <Mikhail.Bratus@GMail.com>
2021-05-05 22:52:06 +0500

    fix #D742 Trashcans graphics are cropped (only integer values are allowed!)

--- src/def/stage/object/trashcan.lua
+++ src/def/stage/object/trashcan.lua
@@ -21,10 +21,10 @@ return {
             { q = q(89, 9, 27, 17) }
         },
         stand = {
-            { q = q(2,2,27,42), ox = 13.5, oy = 41 }, --100% hp
-            { q = q(31,2,27,42), ox = 13.5, oy = 41, hurtBox = { width = 15, height = 33, depth = 1 } },
-            { q = q(60,5,27,39), ox = 13.5, oy = 38, hurtBox = { width = 15, height = 28, depth = 1 } },
-            { q = q(89,9,27,35), ox = 13.5, oy = 34, hurtBox = { width = 15, height = 24, depth = 1 } } -- 0 HP
+            { q = q(2,2,27,42), ox = 13, oy = 41 }, --100% hp
+            { q = q(31,2,27,42), ox = 13, oy = 41, hurtBox = { width = 15, height = 33, depth = 1 } },
+            { q = q(60,5,27,39), ox = 13, oy = 38, hurtBox = { width = 15, height = 28, depth = 1 } },
+            { q = q(89,9,27,35), ox = 13, oy = 34, hurtBox = { width = 15, height = 24, depth = 1 } } -- 0 HP
         },
     } --offsets
 
