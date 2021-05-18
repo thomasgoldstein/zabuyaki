b4f471dc5429d8433e1bf8fd65e32122540cbd5e
Mikhail Bratus <Mikhail.Bratus@GMail.com>
2021-05-18 22:31:56 +0500

    tweak #D747	the hurt anim itself is longer for enemies. Now the last hurt animation frame is shown instead of 'stand' animation during the after hurt stun time.

--- src/unit/character.lua
+++ src/unit/character.lua
@@ -1020,15 +1020,10 @@ function Character:hurtStart()
 end
 function Character:hurtUpdate(dt)
     self.comboTimer = self.comboTimer + dt -- freeze comboTimer
-    if self.canActTimer >= 0 then -- on canActTimer over after the animation end
-        if self.sprite.isFinished and self.sprite.curAnim ~= "stand" then
-            -- prevents stop on the last frame of hurt anim for enemies
-            self:setSprite("stand")
-        end
-        if self.sprite.curAnim == "stand" then
-            self.canActTimer = self.canActTimer - dt
-        end
-    else
+    if self.sprite.isFinished then
+        self.canActTimer = self.canActTimer - dt
+    end
+    if self.canActTimer < 0 then
         if self.hp <= 0 then
             self:setState(self.getUp)
         elseif self.isGrabbed then
